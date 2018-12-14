f6a435e8685b76eaa54cafc853c04a0b73520d68
proje <proje@192.168.0.7>
2018-12-14 12:42:18 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,93 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="widget">
+                    <h5 class="widgetheading">Our Contact</h5>
+                    <address>
+                        <strong>Golden Avenue Trading</strong><br>
+                        Al-Helo Bldg,<br>
+                        Eyal Nasser, Shop # 4, After<br>
+                        London Hotel,  Al Sabkha Rd,<br>
+                        Naif, Deira, Dubai, U.A.E.</address>
+                    <p>
+                        <i class="fa fa-phone"></i> &nbsp;+971 4 266 8272 / +971 4 271 3404 <br>
+                        <i class="fa fa-envelope-o"></i>  &nbsp;enquiry@goldenavenue.ae
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="widget">
+                    <h5 class="widgetheading">Quick Links</h5>
+                    <ul class="link-list">
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Terms and conditions</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                        <li><a href="#">Career</a></li>
+                        <li><a href="#">Contact us</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="widget">
+                    <h5 class="widgetheading">Our Services</h5>
+                    <ul class="link-list">
+                        <li><a href="#">IT Health Checks:</a></li>
+                        <li><a href="#">Technical Solutions</a></li>
+                        <li><a href="#">Hardware & Software</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="widget">
+                    <h5 class="widgetheading">Recent News</h5>
+                    <ul class="link-list">
+                        <li><a href="https://www.arabianbusiness.com/technology/403750-274-cyber-attacks-target-the-uae-so-far-in-2018-down-39">274 cyber attacks target the UAE so far in 2018,</a></li>
+                        <li><a href="https://blog.barkly.com/biggest-data-breaches-2018-so-far">The 10 Biggest Data Breaches of 2018... So Far</a></li>
+                        <li><a href="https://www.wired.com/story/2018-worst-hacks-so-far/">THE WORST CYBERSECURITY BREACHES OF 2018 SO FAR</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="sub-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="copyright">
+                        <p>
+                            <span>&copy; Golden Avenue 2018 All right reserved. Designed By </span><a href="http://yellowandgray.com" target="_blank">YG Studio</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <ul class="social-network">
+                        <li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+                        <li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script>  
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<!-- Vendor Scripts -->
+<script src="js/modernizr.custom.js"></script>
+<script src="js/jquery.isotope.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script> 
+<script src="js/common.js" type="text/javascript"></script>
---
+++ menu.php
@@ -0,0 +1,55 @@
+<div class="topbar">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="pull-left hidden-xs"><span>Welcome to Golden Avenue</span> 
+                <p class="pull-right"><a href=""><i class="fa fa-facebook"></i></a><a href=""><i class="fa fa-twitter"></i></a><a href=""><i class="fa fa-linkedin"></i></a><a href=""><i class="fa fa-pinterest"></i></a><a href=""><i class="fa fa-google-plus"></i></a>&nbsp;<i class="fa fa-phone"></i> 04 2668272 &nbsp;&nbsp;<i class="fa fa-envelope-o"></i> enquiry@goldenavenue.ae</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- start header -->
+<header>
+    <div class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php"><img src="img/Logo-Golden-Avenue.png" alt="logo"/></a>
+            </div>
+            <div class="navbar-collapse collapse ">
+                <ul class="nav navbar-nav">
+                    <!--                    <li class="active"><a href="index.html">Home</a></li> -->
+                    <!--                    <li class="dropdown">
+                                            <a href="#" data-toggle="dropdown" class="dropdown-toggle">About Us <b class="caret"></b></a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="about.html">Company</a></li>
+                                                <li><a href="#">Our Team</a></li>
+                                                <li><a href="#">News</a></li> 
+                                                <li><a href="#">Investors</a></li>
+                                            </ul>
+                                        </li> -->
+<!--                    <li><a href="#"></a></li>-->
+                    <li class="dropdown">
+                        <a href="#" data-toggle="dropdown" class="dropdown-toggle">Services <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">IT HEALTH CHECKS</a></li>
+                            <li><a href="#">NETWORK HEALTH CHECKS</a></li>
+                            <li><a href="#">BUSINESS CONTINUITY</a></li> 
+                            <li><a href="#">IT SECURITY</a></li>
+                            <li><a href="#">APPLICATION PERFROMANCE</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">AMC Contracts</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Why Us</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
