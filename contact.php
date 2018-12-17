4e145b86ce03a77d5afb679c33da73ec427adcf2
proje <proje@192.168.0.7>
2018-12-17 21:32:35 +0530

    contact page changed

--- contact.php
+++ contact.php
@@ -1,11 +1,12 @@
 <!doctype html>
 <html lang="en">
-    <?php $page = 'contact'; include 'head.php'; ?>
+    <?php $page = 'contact';
+    include 'head.php'; ?>
 
     <body>
         <div class="wrapper">
             <!--Header Start-->
-            <?php include 'menu.php'; ?>
+<?php include 'menu.php'; ?>
             <div id="search">
                 <button type="button" class="close">×</button>
                 <form class="search-overlay-form">
@@ -55,15 +56,32 @@
                                             </select>
                                         </li>
                                         <li class="half pr-15">
-                                            <select name="preffred_classes" class="form-control">
-                                                <option value="0">Programs Preffred</option>
-                                                <option value="L.K.G">L.K.G</option>
-                                                <option value="I STD">I STD</option>
-                                                <option value="II STD">II STD</option>
-                                                <option value="III STD ">III STD </option>
-                                                <option value="IV STD">IV STD</option>
-                                                <option value="V STD">V STD</option>
-                                            </select>
+                                            <div class="multiselect">
+                                                <div class="selectBox" onclick="showCheckboxes()">
+                                                    <select>
+                                                        <option>Choose Programs</option>
+                                                    </select>
+                                                    <div class="overSelect"></div>
+                                                </div>
+                                                <div id="checkboxes">
+                                                    <label for="one">
+                                                        <input type="checkbox" id="one" /> Pre-KG</label>
+                                                    <label for="two">
+                                                        <input type="checkbox" id="two" /> L-KG</label>
+                                                    <label for="three">
+                                                        <input type="checkbox" id="three" /> U-KG</label>
+                                                    <label for="four">
+                                                        <input type="checkbox" id="four" /> I</label>
+                                                    <label for="five">
+                                                        <input type="checkbox" id="five" /> II</label>
+                                                    <label for="six">
+                                                        <input type="checkbox" id="six" /> III</label>
+                                                    <label for="seven">
+                                                        <input type="checkbox" id="seven" /> IV</label>
+                                                    <label for="eight">
+                                                        <input type="checkbox" id="eight" /> V</label>
+                                                </div>
+                                            </div>
                                         </li>
                                         <li class="half pl-15">
                                             <input type="text" class="form-control" name="subject" placeholder="Subject">
@@ -121,112 +139,8 @@
                 </div>
             </section>
             <!--Contact End--> 
-            <!--Footer Start-->
-            <footer class="footer">
-                <div class="footer-top wf100">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-3 col-md-6 col-sm-6">
-                                <!--Footer Widget Start-->
-                                <div class="footer-widget">
-                                    <h4>About Ecova</h4>
-                                    <p> If anything’s hot in today’s economy, it’s saving money, including a broad range of green businesses helping people save energy, water, and other resources. Definitely, you can go with this business as it is a nothing but the future. </p>
-                                    <a href="#" class="lm">About us</a> 
-                                </div>
-                                <!--Footer Widget End--> 
-                            </div>
-                            <div class="col-lg-3 col-md-6 col-sm-6">
-                                <!--Footer Widget Start-->
-                                <div class="footer-widget">
-                                    <h4>Our Promises</h4>
-                                    <ul class="quick-links">
-                                        <li><a href="#">Solar Energy</a></li>
-                                        <li><a href="#">Waste Management</a></li>
-                                        <li><a href="#">Eco Ideas</a></li>
-                                        <li><a href="#">Recycling Materials</a></li>
-                                        <li><a href="#">Plant Ecology</a></li>
-                                        <li><a href="#">Saving Wildlife </a></li>
-                                    </ul>
-                                </div>
-                                <!--Footer Widget End--> 
-                            </div>
-                            <div class="col-lg-3 col-md-6 col-sm-6">
-                                <!--Footer Widget Start-->
-                                <div class="footer-widget">
-                                    <h4>Latest Posts</h4>
-                                    <ul class="lastest-products">
-                                        <li> <img src="images/flp1.jpg" alt=""> <strong><a href="#">How you can keep alive wild animals for...</a></strong> <span class="pdate"><i>Posted:</i> 29 September, 2018</span> </li>
-                                        <li> <img src="images/flp2.jpg" alt=""> <strong><a href="#">Eliminate your plastic bottle pollution & keep...</a></strong> <span class="pdate"><i>Posted:</i> 29 September, 2018</span> </li>
-                                        <li> <img src="images/flp3.jpg" alt=""> <strong><a href="#">How you can keep alive wild animals for...</a></strong> <span class="pdate"><i>Posted:</i> 29 September, 2018</span> </li>
-                                    </ul>
-                                </div>
-                                <!--Footer Widget End--> 
-                            </div>
-                            <div class="col-lg-3 col-md-6 col-sm-6">
-                                <!--Footer Widget Start-->
-                                <div class="footer-widget">
-                                    <div id="fpro-slider" class="owl-carousel owl-theme">
-                                        <!--Footer Product Start-->
-                                        <div class="item">
-                                            <div class="f-product">
-                                                <img src="images/fpro1.jpg" alt="">
-                                                <div class="fp-text">
-                                                    <h6><a href="#">Buy T-Shirts</a></h6>
-                                                    <strong>$19.00</strong> 
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!--Footer Product End--> 
-                                        <!--Footer Product Start-->
-                                        <div class="item">
-                                            <div class="f-product">
-                                                <img src="images/fpro1.jpg" alt="">
-                                                <div class="fp-text">
-                                                    <h6><a href="#">Buy T-Shirts</a></h6>
-                                                    <strong>$19.00</strong> 
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!--Footer Product End--> 
-                                        <!--Footer Product Start-->
-                                        <div class="item">
-                                            <div class="f-product">
-                                                <img src="images/fpro1.jpg" alt="">
-                                                <div class="fp-text">
-                                                    <h6><a href="#">Buy T-Shirts</a></h6>
-                                                    <strong>$19.00</strong> 
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!--Footer Product End--> 
-                                    </div>
-                                </div>
-                                <!--Footer Widget End--> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="footer-copyr wf100">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-4 col-sm-4"> <img src="images/logo.png" alt=""> </div>
-                            <div class="col-md-8 col-sm-8">
-                                <p> All Rights Reserved of Ecova © 2018, Design & Developed By: GramoTech </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-            <!--Footer End--> 
+            <?php             include 'footer.php'; ?>
         </div>
-        <!--   JS Files Start  --> 
-        <script src="js/jquery-3.3.1.min.js"></script> 
-        <script src="js/jquery-migrate-1.4.1.min.js"></script> 
-        <script src="js/popper.min.js"></script> 
-        <script src="js/bootstrap.min.js"></script> 
-        <script src="js/owl.carousel.min.js"></script> 
-        <script src="js/jquery.prettyPhoto.js"></script> 
-        <script src="js/isotope.min.js"></script> 
-        <script src="js/custom.js"></script>
+       
     </body>
 </html>
