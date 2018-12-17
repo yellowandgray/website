0fb2e8d7b195acabae0965fbf9cfbe765d551453
proje <proje@192.168.0.7>
2018-12-17 21:42:33 +0530

    contact page changed

--- about.php
+++ about.php
@@ -14,7 +14,7 @@
             </div>
             <!--Header End-->
             <!--Inner Header Start-->
-            <section class="wf100 p100 inner-header">
+            <section class="wf100 p100 inner-header" style="background: url(images/sub-banner/banner-02.jpg) no-repeat;">
                 <div class="container">
                     <h1>About Us</h1>
                     <ul>
