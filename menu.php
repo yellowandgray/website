3542fba37f622d2d9e7ac2927dfdd41cb15cd191
proje <proje@192.168.0.7>
2018-12-24 13:09:14 +0530

    social icons hide in header and footer

--- menu.php
+++ menu.php
@@ -8,14 +8,14 @@
                         <i class="fa fa-envelope"></i>&nbsp;<a href="mailto:info@enpeekkl.com">info@enpeekkl.com</a>
                     </p>
                 </div>
-                <div class="col-xs-6 col-md-push-2 text-right">
-                    <ul class="colorlib-social-icons">
+                <div class="col-xs-6 text-right">
+<!--                    <ul class="colorlib-social-icons">
                         <li><a href="https://www.facebook.com/ENPEE-International-School-1943441245950809/?modal=admin_todo_tour" target="blank"><i class="fa fa-facebook-official" style="color:#1c61ab"></i></a></li>&nbsp;
                         <li><a href="https://twitter.com/SchoolEnpee" target="blank"><i class="fa fa-twitter-square" style="color:#429cd6"></i></a></li>&nbsp;
                         <li><a href="https://www.linkedin.com/in/enpee-international-school-262124178/" target="blank"><i class="fa fa-linkedin-square" style="color:#0077B5"></i></a></li>&nbsp;
                         <li><a href="https://www.instagram.com/enpeeschool/" target="blank"><i class="fa fa-instagram" style="color:#125688"></i></a></li>&nbsp;
                         <li><a href="#"><i class="fa fa-youtube-square" style="color:#cd201f"></i></a></li>&nbsp;
-                    </ul>
+                    </ul>-->
                     <p class="btn-apply"><a href="index.php#enquiry">Enquire Now</a></p>
                 </div>
             </div>
