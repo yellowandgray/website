2f641fa24f47bccd1268ea0c4c163933949a1706
proje <proje@192.168.0.7>
2019-01-10 14:44:02 +0530

    initial commit

---
+++ head.php
@@ -0,0 +1,30 @@
+<head>
+    <title>Technical Solutions a Corporate Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+    <!-- custom-theme -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Technical Solutions a Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+          Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+    <!-- //custom-theme -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css" media="all">
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+
+    <!-- js -->
+    <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+    <!-- //js -->
+
+    <!-- font-awesome-icons -->
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <!-- //font-awesome-icons -->
+
+    <!-- google fonts -->
+    <link href="//fonts.googleapis.com/css?family=Muli:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+    <!-- //google fonts -->
+
+</head>
---
+++ menu.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-default">
+    <div class="navbar-header navbar-left">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <h1><a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="Alias Logo" title="Alias" style="width: 100%" />
+<!--                Technical <span>SOlutions</span>-->
+            </a>
+        </h1>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+        <nav class="link-effect-8" id="link-effect-8">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.html">Home</a></li>
+                <li><a href="#about" class="scroll">About</a></li>
+                <li><a href="#services" class="scroll">Services</a></li>
+                <li><a href="#team" class="scroll">Team</a></li>
+                <li><a href="#testimonials" class="scroll">Testimonials</a></li>
+                <li><a href="#contact" class="scroll">Contact</a></li>
+            </ul>
+            <ul class="footericons">
+                <li><a href="#"><span class="fa fa-twitter" aria-hidden="true"></span></a></li>
+                <li><a href="#"><span class="fa fa-linkedin" aria-hidden="true"></span></a></li>
+                <li><a href="#"><span class="fa fa-google-plus" aria-hidden="true"></span></a></li>
+                <li><a href="#"><span class="fa fa-youtube" aria-hidden="true"></span></a></li>
+            </ul>
+        </nav>
+    </div>
+</nav>
