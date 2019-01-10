ffc7bfc5669bb26938302271872287351dffa6f1
proje <proje@192.168.0.7>
2019-01-10 15:36:18 +0530

    head page updated

--- head.php
+++ head.php
@@ -1,10 +1,9 @@
 <head>
-    <title>Technical Solutions a Corporate Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+    <title>ALIAS INNOVATION SDN BHD | FUTURIZING INNOVATION</title>
     <!-- custom-theme -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="keywords" content="Technical Solutions a Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
-          Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <meta name="keywords" content="" />
     <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
         function hideURLbar(){ window.scrollTo(0,1); } </script>
 
@@ -27,6 +26,7 @@
     <!-- google fonts -->
     <link href="//fonts.googleapis.com/css?family=Muli:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext,vietnamese" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
     <!-- //google fonts -->
 
 </head>
