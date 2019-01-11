b0b07a89ddf905d42295e2d223c2dcb209549c75
proje <proje@192.168.0.7>
2019-01-11 20:04:24 +0530

    common css updated

--- side-content.php
+++ side-content.php
@@ -1,3 +1,4 @@
 <button class="open-button">
-    Admissions (2019-2020) for<br/> Pre-KG to Fifth Standard Starts From February 10th
+    <a href="contact.php" style="color: white">Admissions (2019-2020) for<br/> Pre-KG to Fifth Standard Starts From February 10th
+        <img src="images/web-admission.jpg" alt="Web-Admissions" title="ENPEE International School - Admission" /></a>
 </button>
