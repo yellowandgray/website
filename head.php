b7c2f2d44f4498fda30b5f8dcf9f62ad75b6c956
proje <proje@192.168.0.2>
2019-01-12 19:49:27 +0530

    pages updated

--- head.php
+++ head.php
@@ -21,6 +21,7 @@
     <!-- font-awesome-icons -->
     <link rel="stylesheet" href="css/font-awesome.min.css" />
     <link href="css/common.css" rel="stylesheet" type="text/css"/>
+    <link href="css/responsive.css" rel="stylesheet" type="text/css"/>
     <!-- //font-awesome-icons -->
 
     <!-- google fonts -->
