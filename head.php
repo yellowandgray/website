e18ad5804c2b9c0263d54d3d41714c9786380628
proje <proje@192.168.0.2>
2019-01-12 11:48:29 +0530

    index and common css pages updated

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
     <!-- custom-theme -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="icon" href="images/favicon.png" type="image/gif" />
     <meta name="keywords" content="" />
     <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
         function hideURLbar(){ window.scrollTo(0,1); } </script>
