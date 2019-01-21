40f7def9981923c38fb596368d2cb36d8f194282
proje <proje@192.168.0.7>
2019-01-21 15:31:11 +0530

    download page updated

--- downloads.php
+++ downloads.php
@@ -34,7 +34,7 @@
                 <div class="container contact-dropdown-from">
                     <div class="row">
                         <div class="col-md-6 text-center">
-                            <a href="images/calendar/ENPEE-School-Calendar-2019.pdf" download>
+                            <a href="images/calendar/ENPEE-School-Calendar-2019.pdf">
                                 <h5 title="ENPEE-School-Calendar-2019">ENPEE International School Calendar - 2019</h5>
                                 <img src="images/ENPEE-School-Calendar-2019-.jpg" class="img-width download-box" alt="ENPEE-School-Calendar-2019" title="ENPEE-School-Calendar-2019">
                             </a>
