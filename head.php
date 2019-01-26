53c3b04b0be45d5d9a741c476a35827a17abbf02
proje <proje@192.168.0.6>
2019-01-26 15:46:55 +0530

    index and style pages updated

--- head.php
+++ head.php
@@ -23,6 +23,7 @@
     <link href="css/style.css" type="text/css" rel="stylesheet" media="all">
     <link href="css/common.css" rel="stylesheet" type="text/css"/>
     <link href="css/infinite-slider.css" rel="stylesheet" type="text/css"/>
+    <link href="css/custom-responsive.css" rel="stylesheet" type="text/css"/>
     <link href="css/font-awesome.css" rel="stylesheet">
     <!-- font-awesome icons -->
     <!-- //Custom Theme files -->
