86d3b00dd9c36012f8b8ccd95d71bf7cfa3dc95a
Y&G <Y&G@192.168.0.11>
2019-03-08 19:23:45 +0530

    Merge origin/aaluv into aaluv
    Conflicts:
    	aaluv-math.php

--- franchise-requisites.php
+++ franchise-requisites.php
@@ -13,7 +13,7 @@
         <div class="site-breadcrumb breadcrumb-bg">
             <div class="container">
                 <a href="index.php"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-right"></i>
-                <span>Business Requisites Franchise</span>
+                <span>Franchise Requisites</span>
             </div>
         </div>
         <!-- Breadcrumb section end -->
@@ -25,43 +25,47 @@
                         <h3 class="color-blue text-center">AALUV CLASSROOM SITE SPECIFICATIONS</h3>
                         <div class="row">
                             <div class="col-md-6">
-                                <h3 class="color-blue text-center">Option A</h3>
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <img src="img/course/class-001.png" alt=""/>
+                                <div class="box-gray">
+                                    <h3 class="color-blue text-center">Option A</h3>
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <img src="img/course/class-001.png" alt=""/>
 
+                                        </div>
+                                        <div class="col-md-8">
+                                            <p><b>Area requirement for Stand Alone Centre:</b></p>
+                                            <p>Min Area required1000 Sq. Ft, in a Semi Commercial Area.</p>
+                                        </div>
                                     </div>
-                                    <div class="col-md-8">
-                                        <p><b>Area requirement for Stand Alone Centre:</b></p>
-                                        <p>Min Area required1000 Sq. Ft, in a Semi Commercial Area.</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <img src="img/course/class-003.png" alt=""/>
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <img src="img/course/class-003.png" alt=""/>
 
+                                        </div>
+                                        <div class="col-md-8">
+                                            <p><b>Class Room:</b> A well-illuminated and ventilated classroom of about 250 Sq. ft each and minimum of 2-3 class rooms with basic amenities such as Air condition, Fans, drinking water and washroom facilities.  </p>
+                                        </div>
                                     </div>
-                                    <div class="col-md-8">
-                                        <p><b>Class Room:</b> A well-illuminated and ventilated classroom of about 250 Sq. ft each and minimum of 2-3 class rooms with basic amenities such as Air condition, Fans, drinking water and washroom facilities.  </p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <img src="img/course/class-002.png" alt=""/>
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <img src="img/course/class-002.png" alt=""/>
 
-                                    </div>
-                                    <div class="col-md-8">
-                                        <p class="padding-tb-20"><b>Seating:</b> A comfortable seating arrangement.</p>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <p class="padding-tb-20"><b>Seating:</b> A comfortable seating arrangement.</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-6">
-                                <h3 class="color-blue text-center">Option B</h3>
-                                <h4>Area requirement for Hiring of Kinder Garden or Regular School classroom </h4>
-                                <p>investment to cover the franchise expenses and Licensee Fee will be required:</p>
-                                <p>Well-illuminated and ventilated classroom</p>
-                                <p>Comfortable Table and Chairs.</p>
-                                <p>Separate Rooms for Counselling and Admin activity such as fee collection and parents meeting.</p>
+                                <div class="box-gray">
+                                    <h3 class="color-blue text-center">Option B</h3>
+                                    <h4>Area requirement for Hiring of Kinder Garden or Regular School classroom </h4>
+                                    <p>investment to cover the franchise expenses and Licensee Fee will be required:</p>
+                                    <p>Well-illuminated and ventilated classroom</p>
+                                    <p>Comfortable Table and Chairs.</p>
+                                    <p>Separate Rooms for Counselling and Admin activity such as fee collection and parents meeting.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -113,7 +117,9 @@
                         <h3 class="color-blue">AALUV CLASSROOM INSTRUCTOR</h3>
                         <p>AALUV aims at nurturing the joy of learning mathematics in children, building self-confidence and imparting knowledge that will complement what they have learned in school. It helps children develop their logical thinking skill through learning numbers and acquiring knowledge and also develop independence in solving problems.</p>
                         <p><b>Training Details:</b>  Instructor has to complete the worksheets provided before they start the classes for students of their batch and answered works sheets will be kept only in the center under lock and key.</p>
-                        <h3 class="color-blue">DUTIES OF AALUV CLASSROOM INSTRUCTOR</h3>
+                    </div>    
+                    <div class="col-md-12">
+                        <h3 class="color-blue">DUTIES OF AALUV CLASSROOM INSTRUCTOR</h3>
                         <ul class="icon-position">
                             <i class="fas fa-check"></i><li>Make a difference every day!</li>
                             <i class="fas fa-check"></i><li>Assist the Center Director with the Centre's Operations, ensuring that the centre functions in accordance with company Norms & Standards</li>
@@ -122,55 +128,56 @@
                             <i class="fas fa-check"></i><li>Teach, educate and monitor children as needed</li>
                             <i class="fas fa-check"></i><li>Promote the positive image of the company and play a major role in Prtryaing the company as a provider of choice in educational programs for the communities that we serve.</li>
                         </ul>
-                    </div>
+                    </div>    
                 </div>
             </div>
-        </section>
-        <section class="padding-30">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3 class="color-blue">Desired Qualification</h3>
-                        <p>A young and dynamic <b>MATHS GRADUATE</b> preferably between 25-45 years of age. The Course Instructor has to sign an agreement with the Corporate Office, which is valid for 3 years.</p>
-                        <p>To prepare, guide and conduct Aaluv Classroom.</p>
-                        <ul class="icon-position">
-                            <i class="fas fa-check"></i><li>Dedication and commitment to Aaluv Math Classroom</li>
-                            <i class="fas fa-check"></i><li>Competency in Mathematics</li>
-                            <i class="fas fa-check"></i><li>Passionate in children  Education</li>
-                            <i class="fas fa-check"></i><li>Good communication skills.</li>
-                            <i class="fas fa-check"></i><li>Enjoy a young and vibrant work environment.</li>
-                            <i class="fas fa-check"></i><li>Pro-active go getter who make things to happen</li>
-                            <i class="fas fa-check"></i><li>Passion for a career with belief that “change is only the constant”</li>
-                            <i class="fas fa-check"></i><li>Able to work on weekends</li>
-                        </ul>
-                    </div>
+        </div>
+    </section>
+    <section class="padding-30">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="color-blue">Desired Qualification</h3>
+                    <p>A young and dynamic <b>MATHS GRADUATE</b> preferably between 25-45 years of age. The Course Instructor has to sign an agreement with the Corporate Office, which is valid for 3 years.</p>
+                    <p>To prepare, guide and conduct Aaluv Classroom.</p>
+                    <ul class="icon-position">
+                        <i class="fas fa-check"></i><li>Dedication and commitment to Aaluv Math Classroom</li>
+                        <i class="fas fa-check"></i><li>Competency in Mathematics</li>
+                        <i class="fas fa-check"></i><li>Passionate in children  Education</li>
+                        <i class="fas fa-check"></i><li>Good communication skills.</li>
+                        <i class="fas fa-check"></i><li>Enjoy a young and vibrant work environment.</li>
+                        <i class="fas fa-check"></i><li>Pro-active go getter who make things to happen</li>
+                        <i class="fas fa-check"></i><li>Passion for a career with belief that “change is only the constant”</li>
+                        <i class="fas fa-check"></i><li>Able to work on weekends</li>
+                    </ul>
                 </div>
             </div>
-        </section>
-        <section class="padding-30">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3 class="color-blue">AALUV MATHS SUGGESTED MARKETING ACTIVITIES</h3>
-                        <ul class="icon-position">
-                            <i class="fas fa-check"></i><li>Release periodic advertisements inbNational Newspapers, Satellite Channels, Magazines, etc.</li>
-                            <i class="fas fa-check"></i><li>Press Note.</li>
-                            <i class="fas fa-check"></i><li>Participating in Educational Exhibitions.</li>
-                            <i class="fas fa-check"></i><li>Road shows and conducting demo in Public place like Malls, Schools and gathering.</li>
-                            <i class="fas fa-check"></i><li>Glow Sign Board.</li>
-                            <i class="fas fa-check"></i><li>Referral Scheme.</li>
-                            <i class="fas fa-check"></i><li>Advertisement in social media.</li>
-                            <i class="fas fa-check"></i><li>Door to Door activity</li>
-                            <i class="fas fa-check"></i><li>Auto, Bus Panel display.</li>
-                            <i class="fas fa-check"></i><li>Flier Distribution.</li>
-                            <i class="fas fa-check"></i><li>SMS / WhatsApp / Email Campaign.</li>
-                        </ul>
-                        <p>The Franchisee is solely responsible for local promotional activity.</p>
-                    </div>
+        </div>
+    </section>
+    <section class="padding-30">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="color-blue">AALUV MATHS SUGGESTED MARKETING ACTIVITIES</h3>
+                    <ul class="icon-position">
+                        <i class="fas fa-check"></i><li>Release periodic advertisements inbNational Newspapers, Satellite Channels, Magazines, etc.</li>
+                        <i class="fas fa-check"></i><li>Press Note.</li>
+                        <i class="fas fa-check"></i><li>Participating in Educational Exhibitions.</li>
+                        <i class="fas fa-check"></i><li>Road shows and conducting demo in Public place like Malls, Schools and gathering.</li>
+                        <i class="fas fa-check"></i><li>Glow Sign Board.</li>
+                        <i class="fas fa-check"></i><li>Referral Scheme.</li>
+                        <i class="fas fa-check"></i><li>Advertisement in social media.</li>
+                        <i class="fas fa-check"></i><li>Door to Door activity</li>
+                        <i class="fas fa-check"></i><li>Auto, Bus Panel display.</li>
+                        <i class="fas fa-check"></i><li>Flier Distribution.</li>
+                        <i class="fas fa-check"></i><li>SMS / WhatsApp / Email Campaign.</li>
+                    </ul>
+                    <p>The Franchisee is solely responsible for local promotional activity.</p>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
 
-        <?php include 'footer.php'; ?>
-    </body>
+    <?php include 'footer.php'; ?>
+</body>
 </html>
