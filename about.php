4e6733379dbd0fb20329799ab30899fd5e1ba3e1
proje <proje@192.168.0.7>
2019-03-08 17:59:43 +0530

    index and about page updated

--- about.php
+++ about.php
@@ -32,9 +32,6 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-7 about-text">
-                        <!--                        <h3 class="color-blue">AALUV Maths</h3>
-                                                <p class="text-justify">Aaluv Maths is a unique Comprehensive Mathematic Learning Solution helping millions of students world-wide developing a self-motivated learning and attitude which is the greatest challenge of contemporary parents, teachers and the students themselves.</p>
-                                                <p class="text-justify">Aaluv Maths prioritizes and gives shape to the “desire to learn” and develops in students, invisible abilities like concentration.</p>-->
                         <p class="text-justify">AALUV is a Holistic Brain Development company, developing children's mind to better equip them for the challenges of Life, this training program transforms and empowers Children to perform extensive calculations at stunning speed and accuracy with nothing but their own sharp mind to aid them.</p>
                         <p class="text-justify">The curriculum is specifically designed to enhance a child's skill set for the 21st Century’s robust Challenges.</p>
                         <p class="text-justify">AALUV endows the child with creativity, concentration, memory, speed writing, lateral thinking, listening capacity, analytical skills and over all self confidence making them an “Enhanced Global Citizens”.</p>
@@ -43,43 +40,9 @@
                         <h4>The Global business is expanding</h4>
                         <p class="text-justify">The group company of Aaluv is a Global Leader in educational field and conducts school activity program, which is specially designed for children in the age group of 4-14 years. Since their existence in the last 17 years, they have trained more than 5 million students with 1300 centres across the globe. Essentially, in these programs, the infinite potential of the human brain and its effective use is unveiled through the Scientific Brain Management Techniques.</p>
                         <p  class="text-justify">Being a vibrant organization, it stands for a mission committed to create a new generation, which will be mentally equipped to face the challenges in a competitive environment. Steered by visionary leadership, the Company has set new benchmarks for the industry and is currently expanding its operations in Australia. Kindly check with the author,if it is1300 students or countries??? Because,1300 what is the question?? And it looks incomplete.</p>
-
-<!--                        <table>
-<tbody>
-<tr>
-<th>Company:</th>
-<td>Educhild Australia Pty Ltd.</td>
-</tr>
-<tr>
-<th>Address:</th>
-<td> 40, Kershaw Drive, Narre Warren South, Melbourne, Victoria 3805, Australia.</td>
-</tr>
-<tr>
-<th>Establishment:</th>
-<td></td>
-</tr>
-<tr>
-<th>Capital:</th>
-<td></td>
-</tr>
-<tr>
-<th>Annual sales:</th>
-<td></td>
-</tr>
-<tr>
-<th>CEO:</th>
-<td></td>
-</tr>
-</tbody>
-</table>-->
-
-
                     </div>
                     <div class="col-lg-5 pt-5 pt-lg-0 text-center">
                         <img src="img/course/class-01.jpg" alt="" class="margin-top-20">
-
-                        <!--                        <h6>Founder Name</h6>
-                                                <img src="img/persons.png">-->
                     </div>
                 </div>
                 <div class="row">
@@ -88,154 +51,12 @@
                         <p class="text-justify">‘To endow the new generation with intelligent and expert cognizance enabling them to scale and conquer all heights’.</p>
                         <h3 class="color-blue">MISSION</h3>
                         <p class="text-justify">‘To be recognized as Experts in the field of Innovative Education and as the leading authority in establishing Benchmarks and Standards. Furthermore, to unlock the innate aptitude of the human mind and cultivate knowledge through Math Classroom tutoring’.</p>
-
-                        <!--                        <h3 class="color-blue">WHY AALUV Maths:</h3>
-                                                <p class="text-justify">Aaluv Maths program emphasizes on</p>
-                                                <p><i class="fas fa-check color-blue"></i> problem solving skills among the young students. </p>
-                                                <p><i class="fas fa-check color-blue"></i> mathematical questions presented through various figures, charts, and scenarios closely related to real-life situations </p>
-                                                <p><i class="fas fa-check color-blue"></i> challenge and improve their problem-solving approach and capability. </p>
-                                                <p><i class="fas fa-check color-blue"></i> Significantly helps the overall development of each student.</p>-->
                     </div>
                 </div>
            
             </div>
         </section>
         <!-- About section end-->
-
-
-        <!-- Testimonial section  -->
-<!--        <section class="testimonial-section spad">
-            <div class="container">
-                <div class="testimonial-slider owl-carousel">
-                    <div class="ts-item">
-                        <div class="row">
-                            <div class="col-md-3">
-                                <div class="ts-author-pic set-bg" data-setbg="img/member/5.jpg"></div>
-                            </div>
-                            <div class="col-md-9 ts-text">
-                                <p>“ I want to let you know that I am very absolutely delighted with the course in every respect. It has been particularly pleasing for me, with an interest in seeing the application and good educational practice to school education. Learn a different approach that leads to better outcomes. ”</p>
-                                <h5>Mr. Peter Crough</h5>
-                                <span>Student’s Parent</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="ts-item">
-                        <div class="row">
-                            <div class="col-md-3">
-                                <div class="ts-author-pic set-bg" data-setbg="img/member/5.jpg"></div>
-                            </div>
-                            <div class="col-md-9 ts-text">
-                                <p>“ I want to let you know that I am very absolutely delighted with the course in every respect. It has been particularly pleasing for me, with an interest in seeing the application and good educational practice to school education. Learn a different approach that leads to better outcomes. ”</p>
-                                <h5>Mr. Peter Crough</h5>
-                                <span>Student’s Parent</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="ts-item">
-                        <div class="row">
-                            <div class="col-md-3">
-                                <div class="ts-author-pic set-bg" data-setbg="img/member/5.jpg"></div>
-                            </div>
-                            <div class="col-md-9 ts-text">
-                                <p>“ I want to let you know that I am very absolutely delighted with the course in every respect. It has been particularly pleasing for me, with an interest in seeing the application and good educational practice to school education. Learn a different approach that leads to better outcomes. ”</p>
-                                <h5>Mr. Peter Crough</h5>
-                                <span>Student’s Parent</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>-->
-        <!-- Testimonial section end -->
-
-
-        <!-- Team section  -->
-<!--        <section class="team-section spad">
-            <div class="container">
-                <div class="section-title text-center">
-                    <h3>OUR TEAM</h3>
-                    <p>The professional standards and expectations</p>
-                </div>
-                <div class="row">
-                    <div class="col-md-6 col-lg-3">
-                        <div class="member">
-                            <div class="member-pic set-bg" data-setbg="img/member/1.jpg">
-                                <div class="member-social">
-                                    <a href=""><i class="fa fa-facebook"></i></a>
-                                    <a href=""><i class="fa fa-twitter"></i></a>
-                                    <a href=""><i class="fa fa-pinterest"></i></a>
-                                </div>
-                            </div>
-                            <h5>Sasha Johnson</h5>
-                            <p>Literature Teacher</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-3">
-                        <div class="member">
-                            <div class="member-pic set-bg" data-setbg="img/member/2.jpg">
-                                <div class="member-social">
-                                    <a href=""><i class="fa fa-facebook"></i></a>
-                                    <a href=""><i class="fa fa-twitter"></i></a>
-                                    <a href=""><i class="fa fa-pinterest"></i></a>
-                                </div>
-                            </div>
-                            <h5>Darmian Shaw</h5>
-                            <p>Physics Teacher</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-3">
-                        <div class="member">
-                            <div class="member-pic set-bg" data-setbg="img/member/3.jpg">
-                                <div class="member-social">
-                                    <a href=""><i class="fa fa-facebook"></i></a>
-                                    <a href=""><i class="fa fa-twitter"></i></a>
-                                    <a href=""><i class="fa fa-pinterest"></i></a>
-                                </div>
-                            </div>
-                            <h5>Joshua Matt</h5>
-                            <p>Matt Teacher</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-3">
-                        <div class="member">
-                            <div class="member-pic set-bg" data-setbg="img/member/4.jpg">
-                                <div class="member-social">
-                                    <a href=""><i class="fa fa-facebook"></i></a>
-                                    <a href=""><i class="fa fa-twitter"></i></a>
-                                    <a href=""><i class="fa fa-pinterest"></i></a>
-                                </div>
-                            </div>
-                            <h5>Taylor Launer</h5>
-                            <p>Music Teacher</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>-->
-        <!-- Team section end -->
-
-
-        <!-- Newsletter section -->
-<!--        <section class="newsletter-section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-5 col-lg-7">
-                        <div class="section-title mb-md-0">
-                            <h3>NEWSLETTER</h3>
-                            <p>Subscribe and get the latest news and useful tips, advice and best offer.</p>
-                        </div>
-                    </div>
-                    <div class="col-md-7 col-lg-5">
-                        <form class="newsletter">
-                            <input type="text" placeholder="Enter your email">
-                            <button class="site-btn">SUBSCRIBE</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>-->
-        <!-- Newsletter section end -->	
-
 
         <!-- Footer section -->
         <?php include 'footer.php'; ?>
