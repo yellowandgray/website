16b1903a926247176e0b4ec9aaefb225115d9977
Y&G <Y&G@192.168.0.11>
2019-03-08 14:48:38 +0530

    add to one main page

---
+++ careers.php
@@ -0,0 +1,152 @@
+<html>
+    <?php
+    include 'head.php';
+    $page = 'career';
+    ?>
+    <body>
+        <?php include 'menu.php'; ?>
+        <!-- Banner Section -->
+        <div class="sub-banner-section" style="background: url(img/sub-banner/career.jpg)no-repeat; height: 250px;"></div>
+        <!-- End Banner Section -->
+
+        <!-- Breadcrumb section -->
+        <div class="site-breadcrumb breadcrumb-bg">
+            <div class="container">
+                <a href="index.php"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-right"></i>
+                <span>Franchise</span>
+            </div>
+        </div>
+        <!-- Breadcrumb section end -->
+        <section class="padding-b-70 section-background">
+            <div class="container">
+                <h3>What's it like to be a Aaluv instructor?</h3>
+                <div class="row padding-b-20">
+                    <div class="col-md-6">
+                        <img src="img/career-1.jpg" />
+                    </div>
+                    <div class="col-md-6">
+                        <h4>Learn and grow with Children</h4>
+                        <p>Working as a Aaluv instructor is very rewarding. There’s nothing more precious than the smiles of children after they “get” something new they’ve worked hard at. Through such experience, you’ll notice not only children’s growth but your own as well.</p>
+                    </div>
+                </div>
+                <h6>You 'll find a new you.</h6>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4>The best part of being an instructor is seeing children grow and bloom</h4>
+                        <p>The instructor’s task is to watch over each child and give them the support they need. Being able to watch children grow and bloom is what makes teaching such a fulfilling job.</p>
+                        <h4>APPLY TO BE A AALUV CENTRE HEAD</h4>
+                        <p>A Career with Aaluv offers you a fantastic opportunity to develop your career with International education.</p>
+                        <p>You will be a part of dynamic innovative organisation focused on providing the complete education solution to our learning centres and schools in India as well as our overseas franchise partners.</p>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="img/career-2.jpg" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4>WE BELIEVE IN YOU</h4>
+                        <ul class="list-style-none">
+                            <i class="fas fa-check" aria-hidden="true"></i><li>Unlike the typical tuition centre jobs</li>
+                            <i class="fas fa-check" aria-hidden="true"></i><li>We are passionate about our people</li>
+                            <i class="fas fa-check" aria-hidden="true"></i><li>We encourage our talents to pursue their professional development by providing support and training to enable them to achieve their individual goals.</li>
+                            <i class="fas fa-check" aria-hidden="true"></i><li>Our talents enjoy a wide range of benefits in addition to those expected as Country Standard</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <h4>REQUIREMENTS</h4>
+                        <ul class="list-style-none">
+                            <i class="fas fa-check" aria-hidden="true"></i><li>Strong organisational and office skills.</li>
+                            <i class="fas fa-check" aria-hidden="true"></i><li>Self Motivated and Passionate about Building business.</li>
+                            <i class="fas fa-check" aria-hidden="true"></i><li>Excellent communication skills and able to connect with parents effortlessly.</li>
+                            <i class="fas fa-check" aria-hidden="true"></i><li>Excellent interpersonal skills and thrives on teamwork.</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <h4>WE ARE LOOKING FOR CANDIDATES</h4>
+                        <strong>WHO</strong>
+                        <ul class="list-style-none">
+                            <i class="fas fa-check" aria-hidden="true"></i><li>Enjoy a young and vibrant work environment</li>
+                            <i class="fas fa-check" aria-hidden="true"></i><li>Are pro-active go getter who make things to happen</li>
+                            <i class="fas fa-check" aria-hidden="true"></i><li>Want a career where learning and change is the only constant</li>
+                            <i class="fas fa-check" aria-hidden="true"></i><li>Are able to work on weekends</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="padding-b-70">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4>Apply to Us</h4>
+                        <h6>(*) Indicates Required Fields.</h6>
+                        <form class="comment-form contact">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label>First Name*</label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="text" name="fname" placeholder="First Name" >
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label>Last Name*</label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="text" name="lname" placeholder="Last Name" >
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label>Email*</label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="email" name="email" placeholder="Email" >
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label>Mobile*</label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="text" name="mobile" placeholder="Mobile" >
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label>Applying For*</label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="text" name="applyfor" placeholder="Applying For" >
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label>Resume*<br/><span style="font-size: 12px">(Pdf or Doc Only)</span></label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="file" name="resume" accept="pdf/*">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label>Captcha</label>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="g-recaptcha" data-sitekey="6LdcMI8UAAAAAPt3UZa7vt8OiUTOI37LTsYBf0Vg"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="text-center">
+                                    <button class="site-btn">Send</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php include 'footer.php'; ?>
+    </body>
+</html>
---
+++ franchise-requisites.php
@@ -0,0 +1,176 @@
+<html>
+    <?php
+    include 'head.php';
+    $page = 'franchise';
+    ?>
+    <body>
+        <?php include 'menu.php'; ?>
+        <!-- Banner Section -->
+        <div class="sub-banner-section" style="background: url(img/sub-banner/franchise.jpg)no-repeat; height: 250px;"></div>
+        <!-- End Banner Section -->
+
+        <!-- Breadcrumb section -->
+        <div class="site-breadcrumb breadcrumb-bg">
+            <div class="container">
+                <a href="index.php"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-right"></i>
+                <span>Business Requisites Franchise</span>
+            </div>
+        </div>
+        <!-- Breadcrumb section end -->
+
+        <section class="section-background">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="color-blue">AALUV CLASSROOM SITE SPECIFICATIONS</h3>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h3 class="color-blue">Option A</h3>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <img src="img/course/class-001.png" alt=""/>
+
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><b>Area requirement for Stand Alone Centre:</b></p>
+                                        <p>Min Area required1000 Sq. Ft, in a Semi Commercial Area.</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <img src="img/course/class-003.png" alt=""/>
+
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><b>Class Room:</b> A well-illuminated and ventilated classroom of about 250 Sq. ft each and minimum of 2-3 class rooms with basic amenities such as Air condition, Fans, drinking water and washroom facilities.  </p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <img src="img/course/class-002.png" alt=""/>
+
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><b>Seating:</b> A comfortable seating arrangement.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <h3 class="color-blue">Option B</h3>
+                                <h4>Area requirement for Hiring of Kinder Garden or Regular School classroom </h4>
+                                <p>investment to cover the franchise expenses and Licensee Fee will be required:</p>
+                                <p>Well-illuminated and ventilated classroom</p>
+                                <p>Comfortable Table and Chairs.</p>
+                                <p>Separate Rooms for Counselling and Admin activity such as fee collection and parents meeting.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section-background">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="color-blue">WHO CAN BECOME OUR FRANCHISEE?</h3>
+                        <ul class="icon-position">
+                            <i class="fas fa-check color-blue"></i><li>Business People with an entrepreneurial spirit.</li>
+                            <i class="fas fa-check color-blue"></i><li>Home makers and single mothers looking to break into the business world.</li>
+                            <i class="fas fa-check color-blue"></i><li>Retired people looking for a new source of income.</li>
+                            <i class="fas fa-check color-blue"></i><li>Married couples wanting more financial independence.</li>
+                            <i class="fas fa-check color-blue"></i><li>Working people looking to make a career change.</li>
+                            <i class="fas fa-check color-blue"></i><li>Recent graduates looking to launch their careers but also be self employee.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-background">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="color-blue">AALUV  CLASSROOM FRANCHISEE DESIRED QUALITIES</h3>
+                        <h4>The prospective franchisee must have:</h4>
+                        <ul class="icon-position">
+                            <i class="fas fa-check color-blue"></i><li>An interest in being an entrepreneur and have a positive attitude to work while maintaining quality. Professional ethics are of paramount importance.</li>
+                            <i class="fas fa-check color-blue"></i><li>Passionate about education and working with children</li>
+                            <i class="fas fa-check color-blue"></i><li>A genuine commitment to strive for growth and expansion that will translate into profits for yourself.</li>
+                            <i class="fas fa-check color-blue"></i><li>The aptitude to head your enterprise and manage the  Center.</li>
+                            <i class="fas fa-check color-blue"></i><li>The ability to devote your time and make this as their main activity.</li>
+                            <i class="fas fa-check color-blue"></i><li>Interested in child enrichment</li>
+                            <i class="fas fa-check color-blue"></i><li>Totally agree with Aaluv's philosophy</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-background">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="color-blue">AALUV CLASSROOM INSTRUCTOR</h3>
+                        <p>AALUV aims at nurturing the joy of learning mathematics in children, building self-confidence and imparting knowledge that will complement what they have learned in school. It helps children develop their logical thinking skill through learning numbers and acquiring knowledge and also develop independence in solving problems.</p>
+                        <p><b>Training Details:</b>  Instructor has to complete the worksheets provided before they start the classes for students of their batch and answered works sheets will be kept only in the center under lock and key.</p>
+                        <h3 class="color-blue">DUTIES OF AALUV CLASSROOM INSTRUCTOR</h3>
+                        <ul class="icon-position">
+                            <i class="fas fa-check color-blue"></i><li>Make a difference every day!</li>
+                            <i class="fas fa-check color-blue"></i><li>Assist the Center Director with the Centre's Operations, ensuring that the centre functions in accordance with company Norms & Standards</li>
+                            <i class="fas fa-check color-blue"></i><li>Responsibility to Ensure Utmost Care, Safety, Cleanliness and Cordial environment for both Children & Parents.</li>
+                            <i class="fas fa-check color-blue"></i><li>Inspire the Children each day, in building  Self-Esteem while helping them learn the “NEW MATH CONCEPT” Each day.</li>
+                            <i class="fas fa-check color-blue"></i><li>Teach, educate and monitor children as needed</li>
+                            <i class="fas fa-check color-blue"></i><li>Promote the positive image of the company and play a major role in Prtryaing the company as a provider of choice in educational programs for the communities that we serve.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-background">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="color-blue">Desired Qualification</h3>
+                        <p>A young and dynamic <b>MATHS GRADUATE</b> preferably between 25-45 years of age. The Course Instructor has to sign an agreement with the Corporate Office, which is valid for 3 years.</p>
+                        <p>To prepare, guide and conduct Aaluv Classroom.</p>
+                        <ul class="icon-position">
+                            <i class="fas fa-check color-blue"></i><li>Dedication and commitment to Aaluv Math Classroom</li>
+                            <i class="fas fa-check color-blue"></i><li>Competency in Mathematics</li>
+                            <i class="fas fa-check color-blue"></i><li>Passionate in children  Education</li>
+                            <i class="fas fa-check color-blue"></i><li>Good communication skills.</li>
+                            <i class="fas fa-check color-blue"></i><li>Enjoy a young and vibrant work environment.</li>
+                            <i class="fas fa-check color-blue"></i><li>Pro-active go getter who make things to happen</li>
+                            <i class="fas fa-check color-blue"></i><li>Passion for a career with belief that “change is only the constant”</li>
+                            <i class="fas fa-check color-blue"></i><li>Able to work on weekends</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-background">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="color-blue">AALUV MATHS SUGGESTED MARKETING ACTIVITIES</h3>
+                        <ul class="icon-position">
+                            <i class="fas fa-check color-blue"></i><li>Release periodic advertisements inbNational Newspapers, Satellite Channels, Magazines, etc.</li>
+                            <i class="fas fa-check color-blue"></i><li>Press Note.</li>
+                            <i class="fas fa-check color-blue"></i><li>Participating in Educational Exhibitions.</li>
+                            <i class="fas fa-check color-blue"></i><li>Road shows and conducting demo in Public place like Malls, Schools and gathering.</li>
+                            <i class="fas fa-check color-blue"></i><li>Glow Sign Board.</li>
+                            <i class="fas fa-check color-blue"></i><li>Referral Scheme.</li>
+                            <i class="fas fa-check color-blue"></i><li>Advertisement in social media.</li>
+                            <i class="fas fa-check color-blue"></i><li>Door to Door activity</li>
+                            <i class="fas fa-check color-blue"></i><li>Auto, Bus Panel display.</li>
+                            <i class="fas fa-check color-blue"></i><li>Flier Distribution.</li>
+                            <i class="fas fa-check color-blue"></i><li>SMS / WhatsApp / Email Campaign.</li>
+                        </ul>
+                        <p>The Franchisee is solely responsible for local promotional activity.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php include 'footer.php'; ?>
+    </body>
+</html>
