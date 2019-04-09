e50ed63657d3bdecb6c132924dd44b801d2cb105
Y&G <Y&G@192.168.0.4>
2019-04-09 17:05:59 +0530

    template edit

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
                 <div class="section-header">
                     <h2 class="section-title text-center wow fadeInDown">VIVA CITY GLOBAL</h2>
                     <p class="wow fadeInDown text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <h3 class="read">Read more</h3>
+                    <center><h3 class="read">Read more</h3></center>
                 </div>
             </div><!--/.container-->
         </section>
