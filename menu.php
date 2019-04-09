8e659bf3a8982484f2c4f0c224ed68de3c81d74c
Y&G <Y&G@192.168.0.4>
2019-04-09 17:01:53 +0530

    template edit

---
+++ menu.php
@@ -0,0 +1,28 @@
+<header id="header">
+        <nav id="main-nav" class="navbar navbar-default navbar-fixed-top" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><img src="images/logo-001.png" alt="logo"></a>
+                </div>
+				
+                <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="scroll active"><a href="#home">Home</a></li> 
+                        <li class="scroll"><a href="#services">Group</a></li>
+                        <li class="scroll"><a href="#about">Initiatives</a></li>
+                        <li class="scroll"><a href="#portfolio">Our Clients</a></li>
+                        <li class="scroll"><a href="#our-team">Our Team</a></li>
+                        <li class="scroll"><a href="#pricing">Awards</a></li>
+                        <li class="scroll"><a href="#contact-us">News</a></li>                        
+                        <li class="scroll"><a href="#contact-us">Contact Us</a></li>                        
+                    </ul>
+                </div>
+            </div><!--/.container-->
+        </nav><!--/nav-->
+    </header><!--/header-->
