e96908a4b08e99153250f9cf4f1733b3ffb206db
Y&G <Y&G@192.168.0.5>
2019-04-11 18:47:50 +0530

    Merge origin/viva-starfish into viva-starfish
    Conflicts:
    	Our-Passion.php

---
+++ orang-asli.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!--head -->
+    <?php
+    include 'head.php';
+    $page = 'passion';
+    ?> 
+    <!--head -->
+    <body>
+
+        <!-- LOADER -->
+        <div id="preloader">
+            <div class="loader">
+                <div class="loader__bar"></div>
+                <div class="loader__bar"></div>
+                <div class="loader__bar"></div>
+                <div class="loader__bar"></div>
+                <div class="loader__bar"></div>
+                <div class="loader__ball"></div>
+            </div>
+        </div><!-- end loader -->
+        <!-- END LOADER -->
+
+        <!-- menu-->
+        <?php include 'menu.php'; ?>
+        <!-- menu-->
+
+        <div class="banner-area banner-bg-1" style="background: url(images/sub-banner/passion-sub-banner.jpg)">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="banner">
+                            <h2><span>Our Passion</span></h2>
+                            <ul class="page-title-link">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Our Services</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="services" class="padding-tb-30">
+            <div class="container">
+                <div class="row">
+                    <div class="section-title message-box">
+                        <h2 class="text-center">ORANG ASLI</h2>
+                        <div class="col-md-12 bg-gray">
+                            <div class="row padding-20">
+                                <div class="col-md-3 text-center border-r">
+                                    <h4>Impact project 2 </h4>
+                                    <p>Providing sustainable livelihood to Orang Asli community </p>
+                                </div>
+                                <div class="col-md-3 text-center border-r">
+                                    <h4>Impact area </h4>
+                                    <p>Community Empowerment - Indigenous (Orang Asli) </p>
+                                </div>
+                                <div class="col-md-3 text-center border-r">
+                                    <h4>Host Organisation </h4>
+                                    <p>Viva Odyssey Sdn Bhd </p>
+
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <h4>Location</h4>
+                                    <p>Kg. Chempian, Pahang</p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="row padding-20">
+                            <div class="col-md-6"></div>
+                            <p class="text-center">"This project aims to create sustainable impact by educating real life skills and vocational training to the Orang Asli community with the goal to make them employable and entrepeneurial material"</p>
+                        </div>
+                    </div><!-- end title -->
+                </div><!-- end container -->
+            </div><!-- end container -->
+            <div id="services" class="padding-tb-30">
+                <div class="container">
+                    <div class="row">
+                        <div class="section-title message-box">
+                            <h2 class="text-center">General Informations about Orang Asli</h2>
+
+                            <h4>Problem Statement</h4>
+                            <p>The poverty rate among Orang Asli is 76,9% and the Statistics Department of Malaysia has classified 35,2% of the population as being "very poor". </p>
+                            <p>The young people in this community (Kg Chempian) have less opportunity due to being, illiterate mainly because the rate of dropping out of school is very high. </p>
+                            <p>There are many young adults who are jobless and have no means to earn a living to sustain their family expenditures.</p>
+                            <h4>Effort by the Host Organisation</h4>
+                            <p>Has access, experience and working relationship with the Orang Asli community. </p>
+                            <p>They have a program under their organisation called "VIVA's rehabilitation program" where the companies under that program are looking for employees. </p>
+                            <p>Had done training for orang asli on a smaller scale (5-10 people) but it wasn't proper planned and the lack of ressource is the main concern.</p>
+
+                        </div><!-- end title -->
+                    </div><!-- end row -->
+                </div><!-- end container -->
+
+            </div>
+            <div id="services" class="padding-tb-30">
+                <div class="container">
+                    <div class="row">
+                        <div class="section-title message-box">
+                            <h2 class="text-center">General Informations about Orang Asli</h2>
+
+                            <h4>Suggest Impact Framework</h4>
+                            <p>To provide a holistic program for orang asli youths to be skilled welders and safety officiers that they will be employable in Oil & Gas/Construction industry and to develop other economic activities for the villagers such as home-stays, aquaphonic farming, etc.</p>
+                            <h4>Expected Outputs & Outcomes</h4>
+                            <p>25 Orang asli youths (18-25 years olds) will be trained in welding & saferty to be placed in Oil & Gas and construction industry. </p>
+                            <p>25 Orang asli youths will be trained in Entrepreneur Development Program.</p>
+                            <p>All 50 beneficiaires will have a job placement with the eployers under the VIVA's rehabillitation program. </p>
+                            <p>The other villagers (approximatif 100 people) will be involved a community owned aquaphonic project that will create livelihood and be an interesting tourism attraction.</p>
+
+                        </div>
+                    </div><!-- row end -->
+                </div><!-- end title -->
+            </div><!-- end container -->
+        </div>
+        <div id="services" class="padding-tb-30">
+            <div class="container">
+                <div class="row">
+                    <div class="section-title message-box">
+                        <h2 class="text-center">The F2O Empowerment Project</h2>
+                        <p><strong>We have successfully turned around an Entire Orang Village which faced the similar predicament as the Indian Estate Communities In 2017 and replicated this effort in 3 other villages thereafter</strong></p>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-6 list-align">
+                            <ul>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Goals Achieved </li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Empowering Orang Asli community from Raub & Lipis District </li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Tribe of Semai </li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Total of 93 families (560 individuals)</li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Key Deliverables </li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Improved water quality - Water 4Life Project</li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Improved medical & hygiene</li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Emergency response – logistic support & ERP </li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Education Centre – Mothers became teachers</li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>New livelihood activities – Farming Activities (Increasehousehold income by 300%)</li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Tourism, Handicraft, fish farm, Aquaponic </li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Increase literacy education </li>
+                                <i class="fa fa-circle" aria-hidden="true"></i><li>Create Trade link with distributors in KL</li>
+                            </ul>
+                        </div>
+
+                        <div class="col-md-6 box-4">
+                            <img src="images/gallery/our-passion-002.jpg" alt="" class="width-100">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+
+
+            <?php include 'footer.php'; ?>
+
+    </body>
+</html>
