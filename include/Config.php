c904f131ecb9de0366b4f34a8145633e3d841721
proje <proje@192.168.0.2>
2019-04-17 12:25:58 +0530

    pages updated

--- include/Config.php
+++ include/Config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_USERNAME', 'mypetcab_curricu');
-define('DB_PASSWORD', 'Admin@curriculum');
-define('DB_NAME', 'mypetcab_curriculum_management');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'curriculum-management');
 define('DB_HOST', 'localhost');
-define('IMG_BASE_URL', 'http://mypetcabin.com/curriculum_management/v1/');
-define('FILES_PATH', 'http://mypetcabin.com/curriculum_management/v1');
+define('IMG_BASE_URL', 'http://localhost/curriculum_management/v1/');
+define('FILES_PATH', 'http://localhost/curriculum_management/v1');
 define('PDF_PATH', 'D:/programs/xampp/htdocs/mushak/eshine/v1/');
