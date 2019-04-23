635ddb9776800ffc7ec5e74e558d2ae294fde28c
proje <proje@192.168.0.5>
2019-04-23 18:37:52 +0530

    index page updated

---
+++ menu.php
@@ -0,0 +1,48 @@
+<div class="py-2 bg-primary">
+    <div class="container">
+        <div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
+            <div class="col-lg-12 d-block">
+                <div class="row d-flex">
+                    <div class="col-md-5 pr-4 d-flex topper align-items-center">
+<!--                        <div class="icon bg-tertiary mr-2 d-flex justify-content-center align-items-center"><span class="icon-map"></span></div>
+                        <span class="text">198 West 21th Street, Suite 721 New York NY 10016</span>-->
+                    </div>
+                    <div class="col-md pr-4 d-flex topper align-items-center">
+                        <div class="icon bg-tertiary mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+                        <span class="text">info@alohaindia.com</span>
+                    </div>
+                    <div class="col-md pr-4 d-flex topper align-items-center">
+                        <div class="icon bg-tertiary mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+                        <span class="text">+91 8 144 944 944</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
+    <div class="container d-flex align-items-center">
+        <a class="navbar-brand" href="index.php"><img src="images/Aloha-Home.png" alt="Aloha Logo"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Menu
+        </button>
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active"><a href="index.php" class="nav-link pl-0">Home</a></li>
+                <li class="nav-item dropdown">
+                    <a href="about.html" class="nav-link" data-toggle="dropdown">About Us</a>
+                    <ul class="dropdown-content">
+                        <li><a href="">Hello World!</a></li>
+                        <li><a href="">Hello World!</a></li>
+                        <li><a href="">Hello World!</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a href="teacher.html" class="nav-link">Aloha Programs</a></li>
+                <li class="nav-item"><a href="courses.html" class="nav-link">Virtual Classroom</a></li>
+                <li class="nav-item"><a href="pricing.html" class="nav-link">News & Events</a></li>
+                <li class="nav-item"><a href="blog.html" class="nav-link">Apply</a></li>
+                <li class="nav-item"><a href="contact.html" class="nav-link">Resource</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
