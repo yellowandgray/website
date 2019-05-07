c9f4cd500902b42177b6222a8cfa1c61db5b399a
Y&G <Y&G@192.168.0.12>
2019-05-07 11:49:20 +0530

    Merge origin/cherylppinto into cherylppinto
    Conflicts:
    	styles/common.css

--- onload-popup.php
+++ onload-popup.php
@@ -1,12 +1,12 @@
-<div id="myModal" class="modal fade" role="dialog">
+<div id="ac-wrapper" style='display:none' >
     <div class="modal-dialog" style="background:url(images/pop-up.jpg)no-repeat;background-size: cover;border: 4px solid #fff;">
         <!-- Modal content-->
         <div class="modal-onload-content">
             <div class="modal-header-1">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <span class="clo" onClick="PopUp('hide')" >&times;</span>
             </div>
             <div class="modal-body-1">
-                <h2 class="text-center margin-top-50">FOUR BREAKTHROUGHS</h2>
+                <h2 class="text-center">FOUR BREAKTHROUGHS</h2>
                 <h3>Free Access to Brendon's "Four Breakthroughs":</h3>
                 <p>Join 2 million subscribers! Get 4 new strategies every week on how to be more productive, confident, and happy.</p>
                 <form>
