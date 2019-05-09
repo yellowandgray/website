06ef8d47f92aab96b44ad5a9718e2b79832ddb1e
Y&G <Y&G@192.168.0.12>
2019-05-09 17:22:51 +0530

    Merge origin/cherylppinto into cherylppinto
    Conflicts:
    	menu.php

--- about.php
+++ about.php
@@ -65,13 +65,37 @@
                             <p class="text-justify">Hi there, my friend! <i class="far fa-smile"></i> Great to have you here. Glad you stopped by.</p>
                             <p class="text-justify">I was born a few years ago (don’t ask when <i class="far fa-smile"></i>) in this wonderful city called <strong>Dubai</strong>. When I was born, it was a quiet, quaint, dusty little city.The city grew with me, rising <strong>from sands to skyscrapers.</strong> The city <strong>bloomed and boomed</strong> around me, turning into an oasis of wealth, 7-star hotels, healthcare, education and technology. I saw, first-hand, what a <strong>powerful vision, belief and perseverance</strong> can do. This city and its leaders taught me that <strong>truly nothing is impossible.</strong></p>
                             <p class="text-justify">My parents moved to the Dubai over 40 years ago and were of humble means. I had to work to make enough money to buy a mobile phone and so <strong>started working when I was 16 years old</strong> (yes, there weren’t strict rules around minimum age back then ). I worked to buy my first car, pay for my University tuition. I worked hard & earned every Dirham I spent. I worked for over <strong>17 years in the banking industry.</strong> Then finally in 2017, I took the <strong>leap of faith</strong> and let go of what most people called a <strong>‘stable-high-paying job’ to pursue what really mattered to me – helping people find their greatness</strong> and <strong>trading</strong> the financial markets.</p>
-                            <p class="text-justify">A Coach, be it for a professional athlete or a CEO, performs the same role. A life coach is someone professionally trained to help you maximize your full potential. To be world-class in anything you do, you need the accountability, guidance, enthusiastic support and objective perspective of a coach.</p>
+                            <p class="text-justify">I am an active Toastmaster, have <strong>travelled to 35 countries, scuba-dived</strong> when I was a hydrophobic, <strong>sky-dived,</strong> run a <strong>half-marathon</strong> (goal is to complete the full marathon in 2018), learnt <strong>kick-boxing, horse-riding</strong> (currently working on my motor-bike license) scaled and <strong>summited Mount Kilimanjaro</strong> (5,895m/ 19,341 ft) in July 2017, among various other ‘crazy’ things. Doing what I previously considered impossible has given me the <strong>irrefutable knowledge that our limitations are self-imposed.</strong> And well, doing the impossible is lots of fun!<i class="far fa-smile"></i></p>
+                            <p class="text-justify">II have always had an <strong>unquenchable thirst for education</strong> and learning – I am a <strong>certified NLP Master Practitioner</strong> and an <strong>Advanced Life Coach.</strong> I have two Masters degrees (yes, one wasn’t good enough for me! LOL), am an <strong>alumni of the Harvard Kennedy School</strong> and have tens of certifications in a varied plethora of fields (Arabic language, Design Thinking, Reiki Healing, Health &Fitness, <strong>Avian Biology</strong> to name a few).</p>
+                            <p class="text-justify">Today, I <strong>coach</strong> my wonderful clients, <strong>trade</strong> the financial markets, spend time with my amazing <strong>pets</strong> (I have a cat and an African grey parrot), <strong>read </strong>and <strong>travel</strong>. I would love to get to know you too and look forward to catching up with you over a cup of coffee soon<i class="far fa-smile"></i> </p>
+                        </div>
+                    </div>
+                    <div class="row margin-t-22">
+                        <div class="col-lg-12">
+                            <div class="text-size-1">
+                                <h3>WOULD THIS BLOG/ FOLLOWING ME BE OF VALUE TO YOU?</h3>
+                            </div>
+                            <h4><strong>Only if you have any of the following thoughts/ questions:</strong></h4>
+                            <ul class="list-1">
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li>I have no idea where my money goes. I always have nothing left over at the end of the month.</li>
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li>I am beginning my career and wish I had someone to guide me on the dos & don’ts.</li>
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li>How do I progress in my career</li>
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li>I wish I had more time to (insert your hobby/ interest you wish to pursue).</li>
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li>Is there somewhere I could go to gain some reliable information on traveling to (insert country you wish to travel to)?</li>
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li>I wish I could communicate more effectively.</li>
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li>How can I overcome my fears and follow my dreams?</li>
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li>How do I stop existing and start living?</li>
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li>I want to leave the world a better place because I came this way. How do I do this?</li>
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li>Is there a purpose to my life? How do I find it?</li>
+                            </ul>
                         </div>
                     </div>
 
                 </div>
+
             </div>
             <section class="pad-50 bg-b">
+
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-3">
@@ -97,60 +121,18 @@
 
                 </div>
             </section>
-            <!-- About -->
-            <!--<div class="about prlx_parent" style="background-image:url(images/main_slider_03.jpg);background-repeat: no-repeat;background-size: cover;background-position: right;">-->
-            <div class="about prlx_parent pad-50" >
+            <div class="pad-20 bg-p">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-8">
-                            <h3 class="pad-20"><strong>HOW DOES COACHING HELP?</strong></h3>
-                            <ul class="list-1">
-                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li class="text-justify">Coaching can help with virtually any aspect of someone’s life, whether it is in their personal or professional life.</li>
-                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li class="text-justify">The coaching process takes on specific projects or personal goals by:</li>
-                                <ul class="sub-list">
-                                    <i class="fa fa-circle" aria-hidden="true"></i><li>analyzing the current situation</li>
-                                    <i class="fa fa-circle" aria-hidden="true"></i><li>identifying existing and potential challenges and</li>
-                                    <i class="fa fa-circle" aria-hidden="true"></i><li>devising a plan of action designed to achieve the desired outcome.</li>
-                                </ul>
-                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li class="text-justify">Certified coaches, through their specific strategies and skills, help their clients think clearly and hence arrive at solutions that they are otherwise unable to see on their own. With the careful guidance of a coach, you can see the situation from a new perspective, which brings new answers and solutions. This is the key value that a coach brings.</li>
-                            </ul>
-
+                        <div class="col-lg-6 text-center">
+                            <button class="button-1" id="myBtn-1"><span>Join The Community</span></button>
                         </div>
-                        <div class="col-lg-4">
-                            <div>
-                                <img src="images/slider/001.png" alt="" class="img-b">
-                            </div>
-                            <div class="features_item  d-flex flex-column align-items-center ">
-                                <div class="icon_container box-4 d-flex flex-column">
-                                    <h3 class="text-center">Financial Freedom</h3>
-                                    <video class=" video-res features_item box-3 d-flex flex-column align-items-center justify-content-end text-center" style="width: 100%" height="315" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen controls>
-                                        <source src="vedio/WhatsApp Video 2018-12-18 at 11.14.49 AM (1).mp4" type="video/mp4">
-                                        Your browser does not support the <code>video</code> tag.
-                                    </video>
-                                    <p>If you often get to the end of the month and wonder where your money has gone, if your expenses exceed your income...</p>
-                                    <div class="text-center arrow-1">
-                                        <a href="#"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h3 class="pad-20"><strong>WHY WORK WITH A LIFE COACH?</strong></h3>
-                            <ul class="list-1">
-                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li class="text-justify">People hire a life coach because they want to do even more tomorrow than they can do today.</li>
-                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li class="text-justify">They want to improve their output and see more growth, and they want to do those things quickly and to the best of their ability.</li>
-                                <i class="fa fa-arrow-right" aria-hidden="true"></i><li class="text-justify">These people all identify a gap between where they are and where they want to be, and want help reaching their goals.</li>
-                            </ul>
-                            <h3 class="pad-20"><strong>WHY WOULD YOU WORK WITH A LIFE COACH?</strong></h3>
-                            <p class="text-justify">People hire a life coach because they want to do even more tomorrow than they can do today. They want to improve their output and see more growth, and they want to do those things quickly and to the best of their ability.</p>
-                            <p class="text-justify">All kinds of people use life coaches,including actors, business leaders, creatives, entrepreneurs, executives, home-makers, managers, professionals, small business owners and start-up owners. These people all identify a gap between where they are and where they want to be, and want help reaching their goals.</p>
+                        <div class="col-lg-6 text-center">
+                            <button class="button-1" id="myBtn"><span>Book a 30-minute FREE coaching session</span></button>
                         </div>
                     </div>
                 </div>
             </div>
-
             <div class="prlx_parent pad-50" style="background-image:url(images/bg/cheryl-06.jpg);background-repeat: no-repeat;background-size: cover;background-position: right;">
                 <!-- <div class="contact_background parallax-window" data-parallax="scroll" data-speed="0.7" data-image-src="images/contact_background.jpg"></div> -->
                 <div class=" prlx bg-enquiry"></div>
@@ -177,5 +159,7 @@
             </div>
             <!-- Footer -->
             <?php include 'footer.php'; ?>
+            <?php include 'join-page.php'; ?>
+            <?php include '30-minute-free.php'; ?>
     </body>
 </html>
