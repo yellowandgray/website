518c8e838dba0e82a54a148fbd5861dc1cd4a4be
Y&G <Y&G@192.168.0.12>
2019-05-09 16:22:56 +0530

    Merge origin/cherylppinto into cherylppinto
    Conflicts:
    	menu.php

--- resources.php
+++ resources.php
@@ -64,46 +64,48 @@
                         <p class="text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
                     <div class="row">
-                        <div class="col-lg-4  audio">
-                            <div class="row">
-                                <div class="col-lg-12 text-center">
-                                    <h2>Audio</h2>
+                        <div class="col-lg-4  audio audio-bg">
+                            <div class="pad-f-15">
+                                <div class="row">
+                                    <div class="col-lg-12 text-center">
+                                        <h2>Audio</h2>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row audio-1 margin-t-22">
-                                <h3>simply dummy text</h3>
-                                <audio controls>
-                                    <source src="audio/test-audio.mp3" type="audio/mpeg">
-                                </audio>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                            </div>
-                            <div class="row audio-1">
-                                <h3>simply dummy text</h3>
-                                <audio controls>
-                                    <source src="audio/test-audio.mp3" type="audio/mpeg">
-                                </audio>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                            </div>
-                            <div class="row audio-1">
-                                <h3>simply dummy text</h3>
-                                <audio controls>
-                                    <source src="audio/test-audio.mp3" type="audio/mpeg">
-                                </audio>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                            </div>
-                            <div class="row audio-1">
-                                <h3>simply dummy text</h3>
-                                <audio controls>
-                                    <source src="audio/test-audio.mp3" type="audio/mpeg">
-                                </audio>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                            </div>
-                            <div class="row audio-1">
-                                <h3>simply dummy text</h3>
-                                <audio controls>
-                                    <source src="audio/test-audio.mp3" type="audio/mpeg">
-                                </audio>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                <div class="row audio-1 margin-t-22">
+                                    <h3>simply dummy text</h3>
+                                    <audio controls>
+                                        <source src="audio/test-audio.mp3" type="audio/mpeg">
+                                    </audio>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                </div>
+                                <div class="row audio-1">
+                                    <h3>simply dummy text</h3>
+                                    <audio controls>
+                                        <source src="audio/test-audio.mp3" type="audio/mpeg">
+                                    </audio>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                </div>
+                                <div class="row audio-1">
+                                    <h3>simply dummy text</h3>
+                                    <audio controls>
+                                        <source src="audio/test-audio.mp3" type="audio/mpeg">
+                                    </audio>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                </div>
+                                <div class="row audio-1">
+                                    <h3>simply dummy text</h3>
+                                    <audio controls>
+                                        <source src="audio/test-audio.mp3" type="audio/mpeg">
+                                    </audio>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                </div>
+                                <div class="row audio-1">
+                                    <h3>simply dummy text</h3>
+                                    <audio controls>
+                                        <source src="audio/test-audio.mp3" type="audio/mpeg">
+                                    </audio>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                </div>
                             </div>
                         </div>
 
@@ -185,76 +187,74 @@
                             </div>
                         </div>
                     </div>
-
                 </div>
-            </div>
-            <section class="pad-50 bg-b">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-3">
-                            <img src="images/bg/cheryl-05.jpg" alt="" class="sub-img">
-                        </div>
-                        <div class="col-lg-9 subscribe">
-                            <h2 class="color-w">JOIN OUR COMMUNITY OF DREAMERS, ACHIEVERS & CREATORS</h2>
-                            <h4>Stay updated with articles, tools, updates from me.</h4>
-                            <div class="row">
-                                <div class="col-lg-4">
-                                    <input type='text' name='name' placeholder="Your Name..." class="input-box">
-                                </div>
-                                <div class="col-lg-4">
-                                    <input type='email' name='email' placeholder="Your Email..." class="input-box">
-                                </div>
+                <section class="pad-50 bg-b">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <img src="images/bg/cheryl-05.jpg" alt="" class="sub-img">
+                            </div>
+                            <div class="col-lg-9 subscribe">
+                                <h2 class="color-w">JOIN OUR COMMUNITY OF DREAMERS, ACHIEVERS & CREATORS</h2>
+                                <h4>Stay updated with articles, tools, updates from me.</h4>
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <input type='text' name='name' placeholder="Your Name..." class="input-box">
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <input type='email' name='email' placeholder="Your Email..." class="input-box">
+                                    </div>
 
-                                <div class="col-lg-4">
-                                    <button type="submit" class="button-2"><span>Submit</span></button>
+                                    <div class="col-lg-4">
+                                        <button type="submit" class="button-2"><span>Submit</span></button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
                     </div>
+                </section>
 
-                </div>
-            </section>
-
-            <div class="prlx_parent pad-50" style="background-image:url(images/bg/cheryl-06.jpg);background-repeat: no-repeat;background-size: cover;background-position: right;">
-                <!-- <div class="contact_background parallax-window" data-parallax="scroll" data-speed="0.7" data-image-src="images/contact_background.jpg"></div> -->
-                <div class=" prlx bg-enquiry"></div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6 offset-lg-3 text-center section_title contact_title">
-                            <h2 class="color-w">let's work together</h2>
+                <div class="prlx_parent pad-50" style="background-image:url(images/bg/cheryl-06.jpg);background-repeat: no-repeat;background-size: cover;background-position: right;">
+                    <!-- <div class="contact_background parallax-window" data-parallax="scroll" data-speed="0.7" data-image-src="images/contact_background.jpg"></div> -->
+                    <div class=" prlx bg-enquiry"></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6 offset-lg-3 text-center section_title contact_title">
+                                <h2 class="color-w">let's work together</h2>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <form class="text-bg">
+                                    <h2>Contact us</h2>
+                                    <input type="text" name="fname" placeholder="Name" required > 
+                                    <input type="email" name="email" placeholder="Email Address" required>
+                                    <input type="text" name="phone" placeholder="Phone Number" required> 
+                                    <input type="text" name="subject" placeholder="subject" required>
+                                    <textarea type="text" name="requirement" placeholder="Requirement" class="req" required></textarea>
+                                    <button type="submit" class="button-1"><span class="color-w">Submit</span></button>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <form class="text-bg">
-                                <h2>Contact us</h2>
-                                <input type="text" name="fname" placeholder="Name" required > 
-                                <input type="email" name="email" placeholder="Email Address" required>
-                                <input type="text" name="phone" placeholder="Phone Number" required> 
-                                <input type="text" name="subject" placeholder="subject" required>
-                                <textarea type="text" name="requirement" placeholder="Requirement" class="req" required></textarea>
-                                <button type="submit" class="button-1"><span class="color-w">Submit</span></button>
-                            </form>
-                        </div>
-                    </div>
                 </div>
-            </div>
-            <!-- Footer -->
-            <?php include 'footer.php'; ?>
-            <script>
-                $(function () {
-                    var selectedClass = "";
-                    $(".fil-cat").click(function () {
-                        selectedClass = $(this).attr("data-rel");
-                        $("#portfolio").fadeTo(100, 0.1);
-                        $("#portfolio div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
-                        setTimeout(function () {
-                            $("." + selectedClass).fadeIn().addClass('scale-anm');
-                            $("#portfolio").fadeTo(300, 1);
-                        }, 300);
+                <!-- Footer -->
+                <?php include 'footer.php'; ?>
+                <script>
+                    $(function () {
+                        var selectedClass = "";
+                        $(".fil-cat").click(function () {
+                            selectedClass = $(this).attr("data-rel");
+                            $("#portfolio").fadeTo(100, 0.1);
+                            $("#portfolio div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+                            setTimeout(function () {
+                                $("." + selectedClass).fadeIn().addClass('scale-anm');
+                                $("#portfolio").fadeTo(300, 1);
+                            }, 300);
 
+                        });
                     });
-                });
-            </script>
-    </body>
-</html>
+                </script>
+                </body>
+                </html>
