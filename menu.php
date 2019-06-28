d14213ab9ee50eea1b8ec98e37e15419c5fd4777
Yellowandgray <Yellowandgray@192.168.1.15>
2019-06-28 10:00:44 +0530

    initial commit

---
+++ menu.php
@@ -0,0 +1,26 @@
+<header class="header">
+    <div class="container">
+        <div class="row flexbox-center">
+            <div class="col-lg-2 col-md-3 col-6">
+                <div class="logo">
+                    <a href="#home"><img src="assets/img/logo.png" alt="logo" /></a>
+                </div>
+            </div>
+            <div class="col-lg-10 col-md-9 col-6">
+                <div class="responsive-menu"></div>
+                <div class="mainmenu">
+                    <ul id="primary-menu">
+                        <li><a class="nav-link active" href="#home">Home</a></li>
+                        <li><a class="nav-link" href="#feature">Feature</a></li>
+                        <li><a class="nav-link" href="#screenshot">Screenshot</a></li>
+                        <li><a class="nav-link" href="#mobile">Resources</a></li>
+                        <li><a class="nav-link" href="#team">Team</a></li>
+                        <li><a class="nav-link" href="#blog">Blog</a></li>
+                        <li><a class="nav-link" href="#contact">Contact Us</a></li>
+                        <li><a class="appao-btn" href="#">Download</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
