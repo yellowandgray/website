50dcdcda8b8060a53162f91b55a0175652b92f64
proje <proje@192.168.0.7>
2019-06-29 19:13:28 +0530

    testimonial uupdated

--- beta/what-makes-different.php
+++ beta/what-makes-different.php
@@ -68,53 +68,53 @@
                     </div>
                 </div>
             </section>
-            <section class="testimonials-section wf100 p80">
-                <h2 class="text-center">Testimonials</h2>
+<!--            <section class="testimonials-section wf100 p80">
+                <h2 class="text-center">Testimonials Coming Soon..</h2>
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
                             <div id="h3testimonials" class="owl-carousel owl-theme">
-                                <!--testimonials box start-->
+                                testimonials box start
                                 <div class="item">
                                     <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, making it look like readable. </p>
                                     <div class="tuser"> <img src="images/tuser1.jpg" alt=""> <strong>Danny Scotten</strong> Florida Partner </div>
                                 </div>
-                                <!--testimonials box End--> 
-                                <!--testimonials box start-->
+                                testimonials box End 
+                                testimonials box start
                                 <div class="item">
                                     <p> As opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text. </p>
                                     <div class="tuser"> <img src="images/tuser1.jpg" alt=""> <strong>Richardson</strong> Florida Partner </div>
                                 </div>
-                                <!--testimonials box End--> 
-                                <!--testimonials box start-->
+                                testimonials box End 
+                                testimonials box start
                                 <div class="item">
                                     <p> It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour. </p>
                                     <div class="tuser"> <img src="images/tuser1.jpg" alt=""> <strong>Anna Lillian</strong> Florida Partner </div>
                                 </div>
-                                <!--testimonials box End--> 
-                                <!--testimonials box start-->
+                                testimonials box End 
+                                testimonials box start
                                 <div class="item">
                                     <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, making it look like readable. </p>
                                     <div class="tuser"> <img src="images/tuser1.jpg" alt=""> <strong>Danny Scotten</strong> Florida Partner </div>
                                 </div>
-                                <!--testimonials box End--> 
-                                <!--testimonials box start-->
+                                testimonials box End 
+                                testimonials box start
                                 <div class="item">
                                     <p> As opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text. </p>
                                     <div class="tuser"> <img src="images/tuser1.jpg" alt=""> <strong>Richardson</strong> Florida Partner </div>
                                 </div>
-                                <!--testimonials box End--> 
-                                <!--testimonials box start-->
+                                testimonials box End 
+                                testimonials box start
                                 <div class="item">
                                     <p> It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour. </p>
                                     <div class="tuser"> <img src="images/tuser1.jpg" alt=""> <strong>Anna Lillian</strong> Florida Partner </div>
                                 </div>
-                                <!--testimonials box End--> 
+                                testimonials box End 
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
+            </section>-->
             <!--Footer Start-->
             <?php include 'footer.php'; ?>
         </div>
