5f834739a682ca19957fab7f97f21d747c5c1384
Yellowandgray <Yellowandgray@YG00D7>
2019-07-04 19:49:00 +0530

    features page updated

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
                             <img src="assets/img/feature.png" alt="feature">
                         </div>
                         <div class="single-showcase-box-1">
-                            <a href="#" class="appao-btn appao-btn2">Read More</a>
+                            <a href="features.php" class="appao-btn appao-btn2">Read More</a>
                         </div>
                     </div>
                     <div class="col-lg-4">
