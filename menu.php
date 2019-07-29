4ac68a3f7e5e70756d2ee45c3b8eb26090befcba
Yellowandgray <Yellowandgray@192.168.1.9>
2019-07-29 16:56:17 +0530

    menu and style pages updated

--- menu.php
+++ menu.php
@@ -1,4 +1,4 @@
-<div class="top-bar py-3 bg-light" id="home-section">
+<div class="top-bar py-2 bg-light" id="home-section">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-6 text-left">
@@ -10,7 +10,7 @@
                 </ul>
             </div>
             <div class="col-6">
-                <p class="mb-0 float-right">
+                <p class="mb-0 float-right social-icon">
                     <span class="mr-3"><a href="tel://#"> <span class="icon-phone mr-2" style="position: relative; top: 2px;"></span><span class="d-none d-lg-inline-block text-black">(+1) 234 5678 9101</span></a></span>
                     <span class="mr-3"><a href="#"><span class="icon-envelope mr-2" style="position: relative; top: 2px;"></span><span class="d-none d-lg-inline-block text-black"><span class="__cf_email__" data-cfemail="d7a4bfb8a797aeb8a2a5b3b8bab6beb9f9b4b8ba">mail@gmail.com</span></span></a></span>
                     <span class="mr-3"><a href="#" onclick="document.getElementById('id01').style.display = 'block'" style="width:auto;"> <span class="icon-sign-in mr-2" style="position: relative; top: 2px;"></span><span class="d-none d-lg-inline-block text-black">login</span></a></span>
