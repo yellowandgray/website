d98125cca584d711c5a99ef790bafea49a491d8e
Yellowandgray <Yellowandgray@YG00D7>
2019-07-29 10:51:29 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,95 @@
+<footer class="footer">
+    <div class="footer-top wf100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <!--Footer Widget Start-->
+                    <div class="footer-widget">
+                        <h4>About Ecova</h4>
+                        <p> If anything’s hot in today’s economy, it’s saving money, including a broad range of green businesses helping people save energy, water, and other resources. Definitely, you can go with this business as it is a nothing but the future. </p>
+                        <a href="#" class="lm">About us</a> 
+                    </div>
+                    <!--Footer Widget End--> 
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <!--Footer Widget Start-->
+                    <div class="footer-widget">
+                        <h4>Our Promises</h4>
+                        <ul class="quick-links">
+                            <li><a href="#">Solar Energy</a></li>
+                            <li><a href="#">Waste Management</a></li>
+                            <li><a href="#">Eco Ideas</a></li>
+                            <li><a href="#">Recycling Materials</a></li>
+                            <li><a href="#">Plant Ecology</a></li>
+                            <li><a href="#">Saving Wildlife </a></li>
+                        </ul>
+                    </div>
+                    <!--Footer Widget End--> 
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <!--Footer Widget Start-->
+                    <div class="footer-widget">
+                        <h4>Latest Posts</h4>
+                        <ul class="lastest-products">
+                            <li> <img src="images/flp1.jpg" alt=""> <strong><a href="#">How you can keep alive wild animals for...</a></strong> <span class="pdate"><i>Posted:</i> 29 September, 2018</span> </li>
+                            <li> <img src="images/flp2.jpg" alt=""> <strong><a href="#">Eliminate your plastic bottle pollution & keep...</a></strong> <span class="pdate"><i>Posted:</i> 29 September, 2018</span> </li>
+                            <li> <img src="images/flp3.jpg" alt=""> <strong><a href="#">How you can keep alive wild animals for...</a></strong> <span class="pdate"><i>Posted:</i> 29 September, 2018</span> </li>
+                        </ul>
+                    </div>
+                    <!--Footer Widget End--> 
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <!--Footer Widget Start-->
+                    <div class="footer-widget">
+                        <div id="fpro-slider" class="owl-carousel owl-theme">
+                            <!--Footer Product Start-->
+                            <div class="item">
+                                <div class="f-product">
+                                    <img src="images/fpro1.jpg" alt="">
+                                    <div class="fp-text">
+                                        <h6><a href="#">Buy T-Shirts</a></h6>
+                                        <strong>$19.00</strong> 
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Footer Product End--> 
+                            <!--Footer Product Start-->
+                            <div class="item">
+                                <div class="f-product">
+                                    <img src="images/fpro1.jpg" alt="">
+                                    <div class="fp-text">
+                                        <h6><a href="#">Buy T-Shirts</a></h6>
+                                        <strong>$19.00</strong> 
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Footer Product End--> 
+                            <!--Footer Product Start-->
+                            <div class="item">
+                                <div class="f-product">
+                                    <img src="images/fpro1.jpg" alt="">
+                                    <div class="fp-text">
+                                        <h6><a href="#">Buy T-Shirts</a></h6>
+                                        <strong>$19.00</strong> 
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Footer Product End--> 
+                        </div>
+                    </div>
+                    <!--Footer Widget End--> 
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyr wf100">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-4"> <img src="images/logo.png" alt=""> </div>
+                <div class="col-md-8 col-sm-8">
+                    <p> All Rights Reserved of Ecova © 2018, Design & Developed By: GramoTech </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ head.php
@@ -0,0 +1,40 @@
+<header class="header-style-2">
+    <div class="row">
+        <div class="col-md-10">
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="index.html"><img src="images/h2logo.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-bars"></i> </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"> <a class="nav-link" href="#">Bunker</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="#">Attention!</a> </li>
+                        <!--<li class="nav-item"> <a class="nav-link" href="about.html">Attention!</a> </li>-->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Weapons </a>
+                            <ul class="dropdown-menu" >
+                                <li><a href="blog.php">Red</a></li>
+                                <li><a href="blog.php">Green</a></li>
+                                <li><a href="blog.php">Blue</a></li>
+                            </ul>
+                            <!--                                        <ul class="dropdown-menu" >
+                                                                        <li><a href="events-grid.html">Red</a></li>
+                                                                        <li><a href="events-grid-2.html">Green</a></li>
+                                                                        <li><a href="events-grid-3.html">Blue</a></li>
+                                                                    </ul>-->
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="#">Reports</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="#">Join up!</a> </li>
+                        <!--                                    <li class="nav-item"> <a class="nav-link" href="gallery-grid.html">Reports</a> </li>
+                                                            <li class="nav-item"> <a class="nav-link" href="contact-one.html">Join up!</a> </li>-->
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="col-md-2 col-sm-2">
+            <form class="search-form">
+                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                <button class="sbtn" type="submit"><i class="fas fa-search"></i></button>
+            </form>
+        </div>
+    </div>
+</header>
