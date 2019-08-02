9ff0d6704acf2fd2178077d4df3f3eff15a68e02
Yellowandgray <Yellowandgray@YG00D7>
2019-08-02 17:00:54 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Yellow And Gray | Simple and Effective</title>
+        <link rel="icon" href="img/fav-icon.png" type="image/gif">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div class="overlay"></div>
+<!--            <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+                <source src="img/bg.gif" type="image/ogg">
+            </video>-->
+            <div class="header-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 logo">
+                            <img src="img/logo.png" alt="Yellow And Gray Logo" />
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                            <div class="email-text">
+                                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                                <a href="mailto: info@yellowandgray.sg"> info@yellowandgray.sg</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section">
+                <div class="container h-100">
+                    <div class="row text-center align-items-center">
+                        <div class="col-md-xl-12 col-lg-12 col-md-12 col-sm-12 text-white main-text-content">
+                            <h1 class="display-3">CREATIVE & TECHNICAL</h1>
+                            <h3>Simple & Effective</h3>
+                            <p>Web Apps, Mobile Apps, Marketing Designs, Animation, SEO, SMM and Content Development </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <footer class="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="text-center">
+                                <p>&copy; 2019 All Rights Reserved. | YG Studio.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </header>
+    </body>
+    <script src="js/jquery.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+</html>
---
+++ test.php
@@ -0,0 +1,54 @@
+<div class="section">
+    <!--            <video autoplay muted loop>
+                    <source src="img/Bite_Rain_R.mp4" type="video/mp4">
+                    Your browser does not support HTML5 video.
+                </video>-->
+    <header class="header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-2">
+                    <div class="logo">
+                        <img src="img/logo.png" alt="Yellow And Gray Logo" />
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <div class="main-text text-center">
+                        <h1>Simple & Effective</h1>
+                        <p>Web, Mobile, Concept Designing,  Animation, SEO and Social Media Marketing</p>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="mail padding-top-40">
+                        <span><i class="fa fa-envelope-o" aria-hidden="true"></i><a href="#" target="blank"> info@yellowandgray.sg</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="padding-top-12">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="main-text text-center">
+                        <div class="box">
+                            <h1></h1>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="text-center">
+                        <p>&copy; 2019 All Rights Reserved. | YG Studio.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
