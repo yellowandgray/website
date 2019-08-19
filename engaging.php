6a9c9993f0a465afb470fd8b772435567a82e5f1
Y&G <Y&G@YG00D6>
2019-08-19 11:35:41 +0530

    message commited

---
+++ engaging.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html lang="en">
+
+
+    <!-- Mirrored from jumpstart.mediumra.re/company-about-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 12 Aug 2019 11:11:15 GMT -->
+    <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+    <?php include 'head.php'; ?>
+
+    <body class="subpage">
+        <div class="loader">
+            <div class="loading-animation"></div>
+        </div>
+
+
+        <?php include 'menu.php'; ?>
+
+        <div class="bg-primary-3 o-hidden" data-overlay>
+            <section class="text-white pb-0">
+                <div class="container pb-5">
+                    <div class="row justify-content-center text-center">
+                        <div class="col-md-9 col-lg-8 col-xl-7">
+                            <h1 class="display-3">ENGAGING ACTIVITIES <mark data-aos="highlight-text" data-aos-delay="250"></mark></h1>
+                            <p class="lead mb-0">WORK,LEARN,REST,PLAY.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="position-absolute w-50 h-100 top left" data-jarallax-element="100 50">
+                    <div class="blob blob-4 bg-gradient w-100 h-100 top left"></div>
+                </div>
+                <div class="divider divider-bottom bg-white"></div>
+            </section>
+        </div>
+        <section>
+            <div class="container">
+                <div class="row align-items-center justify-content-around text-center text-lg-left">
+                    <div class="col-md-9 col-lg-6 col-xl-5 mb-4 mb-md-5 mb-lg-0" data-aos="fade-in">
+                        <h2>What we provide and offer at the Project Next Dooe are something that allows its guest to stretch their imagination while enjoying location has to offer.</h2>
+                    </div>
+                    <div class="col-md-9 col-lg-6 col-xl-5" data-aos="fade-in">
+                        <img src="assets/img/Untitled-2.jpg" alt="Image" class="img-fluid rounded shadow">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="pt-0 o-hidden">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="h1">SERVICES</h2>
+                        <ul>
+                            <li>Bite Sized Training</li>
+                            <li>Full Sized Training</li>
+                            <li>Customized Training</li>
+                            <li>Ideas@Pondok</li>
+                            <li>Idenators</li>
+                            <li>S.U.M.O</li>
+                            <li>Business Management & English.</li>
+                            <li>Content Delivery</li>
+                        </ul>
+                    </div> 
+                    <div class="col-md-6">
+                        <h2 class="h1">ENTREPRENEURSHIP</h2>
+                        <ul>
+                            <li>Management</li>
+                            <li>Strategic Planning</li>
+                            <li>Technical Skills</li>
+                            <li>Soft Skills</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg-primary-3 text-white pt-0">
+            <div class="divider divider-top transform-flip-x bg-white"></div>
+            <div class="container">
+                <div class="row section-title justify-content-center text-center">
+                    <div class="col-md-9 col-lg-8 col-xl-7">
+                        <h3 class="display-4">UPCOMING TRAININGS</h3>
+                        <div class="col-md-6 mb-3 mb-md-4 l-25">
+                                <div id="datepicker" class="calendar"></div> 
+                        </div>  
+                        <div class="empower">
+                            <i class="fab fa-instagram"></i><i class="fab fa-youtube"></i><i class="fab fa-facebook"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="row section-title justify-content-center text-center">
+                    <div class="col-md-9 col-lg-8 col-xl-7">
+                        <h3 class="display-4">Contact Us</h3>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-xl-12 col-lg-12">
+                        <div class="row justify-content-center">
+                            <div class="col-md-6 mb-3 mb-md-4" data-aos="fade-up" data-aos-delay="20">
+                                <div class="card card-body contact-bg min-vh-md-30">
+                                    <div class="flex-fill contact">
+                                        <!--                                        <h4 class="h3">Address</h4>-->
+                                        <i class="fas fa-map-marker-alt icon icon-xs bg-white" data-inject-svg></i><p> Project Next Door, No. 16-05, Penthouse,<br/> Menara Infiniti, Jalan SS6/3, 47301<br/> Petaling Jaya, Selangor, Malaysia.</p>
+                                        <i class="far fa-envelope icon icon-xs bg-white" data-inject-svg></i><p> <a href="#" style="color: #fff;"> email@mywebsite.com</a></p>
+                                        <i class="fas fa-phone-alt icon icon-xs bg-white" data-inject-svg></i><p><a href="#" style="color: #fff;"> +630 7662 7601</a></p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.9544992786723!2d101.59787331464979!3d3.1067379977377474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4d6f2ebba175%3A0x8b633e7b44f03dc6!2sInfinity+Tower!5e0!3m2!1sen!2sin!4v1564835922110!5m2!1sen!2sin" style="width: 100%; height: 300px;" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+        <section class="bg-light">
+            <div class="container">
+                <div class="row section-title justify-content-center text-center">
+                    <div class="col-md-9 col-lg-8 col-xl-7">
+                        <h3 class="display-4">Less work, <mark data-aos="highlight-text" data-aos-delay="250">more flow.</mark></h3>
+                        <div class="lead">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.</div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col">
+                        <ul class="avatar-group avatar-group-lg flex-wrap justify-content-center">
+                            <li>
+                                <img src="assets/img/avatars/male-1.jpg" alt="Joshua Lapinsky profile image" class="avatar avatar-xl">
+                            </li>
+                            <li>
+                                <img src="assets/img/avatars/female-1.jpg" alt="Glenda Spence profile image" class="avatar avatar-xl">
+                            </li>
+                            <li>
+                                <img src="assets/img/avatars/male-2.jpg" alt="Michael Trossino profile image" class="avatar avatar-xl">
+                            </li>
+                            <li>
+                                <img src="assets/img/avatars/male-3.jpg" alt="Patrik Janssen profile image" class="avatar avatar-xl">
+                            </li>
+                            <li>
+                                <img src="assets/img/avatars/female-2.jpg" alt="Mirembe Nkrumah profile image" class="avatar avatar-xl">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row justify-content-center mt-4 mt-md-5">
+                    <div class="col-auto">
+                        <div class="alert bg-secondary">Interested in joining the team? <a href="#">View Careers</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="pb-0">
+            <div class="container">
+                <div class="row section-title justify-content-center text-center">
+                    <div class="col-md-9 col-lg-8 col-xl-7">
+                        <h3 class="display-4">As seen in...</h3>
+                        <div class="lead">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.</div>
+                    </div>
+                </div>
+                <div class="row justify-content-center text-center">
+                    <div class="col-sm-8 col-md-4 mb-5 mb-lg-0">
+                        <div class="card card-body bg-secondary mb-4">
+                            <img src="assets/img/logos/brand/kyan.svg" alt="Kyan company logo" class="opacity-50 my-4 my-lg-5">
+                        </div>
+                        <div class="px-xl-4">
+                            &ldquo;A polished product from a solid performer in the brutal and ever-changing SaaS landscape.&rdquo;
+                        </div>
+                    </div>
+                    <div class="col-sm-8 col-md-4 mb-5 mb-lg-0">
+                        <div class="card card-body bg-secondary mb-4">
+                            <img src="assets/img/logos/brand/goldline.svg" alt="Goldline company logo" class="opacity-50 my-4 my-lg-5">
+                        </div>
+                        <div class="px-xl-4">
+                            &ldquo;It's clear that the team at Jumpstart have been listening to their customers.&rdquo;
+                        </div>
+                    </div>
+                    <div class="col-sm-8 col-md-4 mb-5 mb-lg-0">
+                        <div class="card card-body bg-secondary mb-4">
+                            <img src="assets/img/logos/brand/aven.svg" alt="Aven company logo" class="opacity-50 my-4 my-lg-5">
+                        </div>
+                        <div class="px-xl-4">
+                            &ldquo;The future is looking bright for this punky young startup. One to watch for sure.&rdquo;
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="divider divider-bottom bg-primary-3"></div>
+        </section>
+
+
+        <a href="#top" class="btn btn-primary rounded-circle btn-back-to-top" data-smooth-scroll data-aos="fade-up" data-aos-offset="2000" data-aos-mirror="true" data-aos-once="false">
+            <img src="assets/img/icons/interface/icon-arrow-up.svg" alt="Icon" class="icon bg-white" data-inject-svg>
+        </a>
+        <!-- Required vendor scripts (Do not remove) -->
+        <?php include'footer.php'; ?>
+
+    </body>
+
+
+    <!-- Mirrored from jumpstart.mediumra.re/company-about-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 12 Aug 2019 11:11:16 GMT -->
+</html>
