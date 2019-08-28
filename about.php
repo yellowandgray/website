f626c92a43d24630691e6bd58f6408f1c66f6527
Y&G <Y&G@YG00D6>
2019-08-28 13:28:34 +0530

    initial commit

--- about.php
+++ about.php
@@ -33,17 +33,17 @@
                 <div class="about-video-section wf100">
                     <div class="container">
                         <div class="row">
-                            <div class="col-lg-6 wow bounceInLeft">
+                            <div class="col-lg-6 wow fadeInLeft">
                                 <div class="about-text">
                                     <h5>About</h5>
                                     <h2>VPN COMMODITIES</h2>
                                     <p class="text-justify">VPN COMMODITIES is a reputed global service provider in the field of Copper and Fiber Products in the Telecom Sector and in the field of Low Voltage Electrical products. Having its base in India, Germany and USA, the firm has carved a niche through the support of its valuable clients. Backed by a team of experts from engineering manufacturing, trading and sourcing, the firm offers cost effective solutions and caters the needs of the client in mentioned domains.</p>
-                                <h5>Our Moto</h5>
-                                <p>“To exceed customer expectations through Innovation, Continuous Improvement in People & Products, Quality, Cost and Delivery”.</p>
+                                    <h5>Our Moto</h5>
+                                    <p class="text-justify">“To exceed customer expectations through Innovation, Continuous Improvement in People & Products, Quality, Cost and Delivery”.</p>
                                 </div>
                             </div>
-                            <div class="col-lg-6 wow bounceInLeft">
-                                <div class="about-video-img"><img src="images/aboutimg.jpg" alt=""> </div>
+                            <div class="col-lg-6 wow fadeInRight">
+                                <div class="about-img"><img src="images/aboutimg.jpg" alt=""> </div>
                             </div>
                         </div>
                     </div>
@@ -53,47 +53,45 @@
                 <div class="our-success-story wf100">
                     <div class="container">
                         <div class="row">
-                            <div class="col-lg-4">
-                                <div class="story-title-bg"> <strong class="t1">VPN</strong> <strong class="t2">20+</strong> <strong class="t3">Years of
-                                        Services</strong> 
+                            <div class="col-lg-4 wow fadeInLeft">
+                                <div class="story-title-bg"> 
+                                    <img src="images/storybg.jpg" alt="">
+<!--                                    <strong class="t1">VPN</strong> <strong class="t2">20+</strong> <strong class="t3">Years of
+                                        Services</strong> -->
                                 </div>
                             </div>
                             <div class="col-lg-8">
-                                <div class="section-title-2">
-                                    <h5>About our History</h5>
-                                    <h2>Our Success Story</h2>
-                                </div>
                                 <!--eco History Start-->
                                 <div class="eco-history">
                                     <div class="timeline-slider">
                                         <div class="timeline">
                                             <!--Timeline Text Start-->
-                                            <div>
-                                                <div class="row about-h">
-                                                    <div class=" col-4">
-                                                        <div class="timeline-box">
-                                                            <h6 align="center">Vision</h6>
-                                                            <ul>
-                                                                <li>To facilitate Telecom and Energy Based Companies by offering reliable network connectivity.</li>
-                                                                <li>To conserve natural resources through solutions from renewable energy sectors.</li>
-                                                                <li>To save energy, time and money by serving as sourcing hub besides providing best solutions for packaging and logistic needs of clients.</li>
-                                                            </ul>
-                                                        </div>
+                                            <div class="row about-h wow fadeInRight">
+                                                <div class=" col-12">
+                                                    <div class="timeline-box-1">
+                                                        <h6 align="center">Vision</h6>
+                                                        <ul class="data-list">
+                                                            <i class="fa fa-arrow-right" aria-hidden="true"></i><li>To facilitate Telecom and Energy Based Companies by offering reliable network connectivity.</li>
+                                                            <i class="fa fa-arrow-right" aria-hidden="true"></i><li>To conserve natural resources through solutions from renewable energy sectors.</li>
+                                                            <i class="fa fa-arrow-right" aria-hidden="true"></i><li>To save energy, time and money by serving as sourcing hub besides providing best solutions for packaging and logistic needs of clients.</li>
+                                                        </ul>
                                                     </div>
-                                                    <div class=" col-4">
-                                                        <div class="timeline-box">
-                                                            <h6 align="center">Mission</h6>
-                                                            <p> Yellow And Green aims to serve more than 50 telecom and energy companies worldwide.</p>
-                                                        </div>
+                                                </div>
+                                                <div class=" col-12">
+                                                    <div class="timeline-box-1">
+                                                        <h6 align="center">Mission</h6>
+                                                        <ul class="data-list">
+                                                            <i class="fa fa-arrow-right" aria-hidden="true"></i><li>VPN Commodities aims to serve more than 50 telecom and energy companies worldwide.</li>
+                                                        </ul>
                                                     </div>
-                                                    <div class=" col-4">
-                                                        <div class="timeline-box">
-                                                            <h6 align="center">Goal</h6>
-                                                            <ul>
-                                                                <li>To be the most successful professional operator for all stake holders within the industry.</li>
-                                                                <li>Yellow And Green has ambitions to introduce new products in the field of Sound, Image, Data, Connectivity, and Energy Flow.</li>
-                                                            </ul>
-                                                        </div>
+                                                </div>
+                                                <div class=" col-12">
+                                                    <div class="timeline-box-1">
+                                                        <h6 align="center">Goal</h6>
+                                                        <ul class="data-list">
+                                                            <i class="fa fa-arrow-right" aria-hidden="true"></i><li>To be the most successful professional operator for all stake holders within the industry.</li>
+                                                            <i class="fa fa-arrow-right" aria-hidden="true"></i><li>Yellow And Green has ambitions to introduce new products in the field of Sound, Image, Data, Connectivity, and Energy Flow.</li>
+                                                        </ul>
                                                     </div>
                                                 </div>
                                             </div>
@@ -107,51 +105,54 @@
                 </div>
                 <!--Our Success Story End--> 
                 <!--Why you Need to Choose Ecova Start-->
-                                <!--Why you Need to Choose Ecova End--> 
+                <!--Why you Need to Choose Ecova End--> 
                 <!--Testimonials Start-->
-                <section class="wf100 p80">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <h6 align="center">Solutions</h6>
-                                <ul>
-                                    <li>Providing Solutions for Communication and Energy Network for Reliable Connectivity</li>
-                                    <li>Renewable Energy Resource Equipment</li>
-                                    <li>Providing End to End Sourcing Solutions</li>
-                                    <li>Packaging and Logistic & Benefit Scheme Solutions</li>
-                                </ul>
-                            </div>
-                            
-                            <div class="col-lg-6">
-                                <h6 align="center">What Vpn Commodities Specializes in?</h6>
-                                <ul>
-                                    <li>Vpn Commodities are global players and their work involves management of data, image and flow distribution in cooperation with all the operators and suppliers of interconnected services globally.</li>
-                                    <li>We specialize in supply of quality products for copper outside Plant Networks, Fiber Optic outside plant networks, in building, civil engineering, LV products, Solar Products (PV Modules, Batteries, Inverters and etc…), for a clients from Telecom Operators, Distribution Networks, Power Utilities, Private Operators, Integrators, Distributors and Private Market.</li>
-                                </ul>
-                            </div>
-                            
-                        </div>
-                    </div>
-                </section>
+
                 <!--Testimonials End--> 
                 <!--Partner Logos Section Start-->
-                <div class="partner-logos wf100">
-                    <div class="container">
-                        <div id="partner-logos" class="owl-carousel owl-theme">
-                            <div class="item"><img src="images/plogo1.png" alt=""></div>
-                            <div class="item"><img src="images/plogo2.png" alt=""></div>
-                            <div class="item"><img src="images/plogo3.png" alt=""></div>
-                            <div class="item"><img src="images/plogo4.png" alt=""></div>
-                            <div class="item"><img src="images/plogo5.png" alt=""></div>
-                            <div class="item"><img src="images/plogo1.png" alt=""></div>
-                            <div class="item"><img src="images/plogo2.png" alt=""></div>
-                            <div class="item"><img src="images/plogo3.png" alt=""></div>
-                            <div class="item"><img src="images/plogo4.png" alt=""></div>
-                            <div class="item"><img src="images/plogo5.png" alt=""></div>
+                <!--                <div class="partner-logos wf100">
+                                    <div class="container">
+                                        <div id="partner-logos" class="owl-carousel owl-theme">
+                                            <div class="item"><img src="images/plogo1.png" alt=""></div>
+                                            <div class="item"><img src="images/plogo2.png" alt=""></div>
+                                            <div class="item"><img src="images/plogo3.png" alt=""></div>
+                                            <div class="item"><img src="images/plogo4.png" alt=""></div>
+                                            <div class="item"><img src="images/plogo5.png" alt=""></div>
+                                            <div class="item"><img src="images/plogo1.png" alt=""></div>
+                                            <div class="item"><img src="images/plogo2.png" alt=""></div>
+                                            <div class="item"><img src="images/plogo3.png" alt=""></div>
+                                            <div class="item"><img src="images/plogo4.png" alt=""></div>
+                                            <div class="item"><img src="images/plogo5.png" alt=""></div>
+                                        </div>
+                                    </div>
+                                </div>-->
+                <!--Partner Logos Section End--> 
+            </section>
+            <section class="wf100 our-success-story">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="box-1 wow fadeInLeft">
+                                <h3 align="center">Solutions</h3>
+                                <ul class="data-list">
+                                    <i class="fa fa-hand-o-right" aria-hidden="true"></i><li>Providing Solutions for Communication and Energy Network for Reliable Connectivity</li>
+                                    <i class="fa fa-hand-o-right" aria-hidden="true"></i><li>Renewable Energy Resource Equipment</li>
+                                    <i class="fa fa-hand-o-right" aria-hidden="true"></i><li>Providing End to End Sourcing Solutions</li>
+                                    <i class="fa fa-hand-o-right" aria-hidden="true"></i><li>Packaging and Logistic & Benefit Scheme Solutions</li>
+                                </ul>  
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="box-1 wow fadeInRight">
+                                <h3 align="center">What Vpn Commodities Specializes in?</h3>
+                                <ul class="data-list">
+                                    <i class="fa fa-hand-o-right" aria-hidden="true"></i><li>Vpn Commodities are global players and their work involves management of data, image and flow distribution in cooperation with all the operators and suppliers of interconnected services globally.</li>
+                                    <i class="fa fa-hand-o-right" aria-hidden="true"></i><li>We specialize in supply of quality products for copper outside Plant Networks, Fiber Optic outside plant networks, in building, civil engineering, LV products, Solar Products (PV Modules, Batteries, Inverters and etc…), for a clients from Telecom Operators, Distribution Networks, Power Utilities, Private Operators, Integrators, Distributors and Private Market.</li>
+                                </ul>  
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!--Partner Logos Section End--> 
             </section>
             <!--About End--> 
             <!--Footer Start-->
