8ff99ca891cd0a570868206202be392980c53b1e
Y&G <Y&G@YG00D6>
2019-08-28 13:22:44 +0530

    initial commit

---
+++ index-1.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/bootstrap-grid.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/common.css" rel="stylesheet" type="text/css"/>
+        <script src="js/bootstrap.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <header>
+            <section class="head">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="logo">
+                                <img src="img/logo.png" alt=""/>
+                            </div>
+                        </div>
+                        <div class="col-md-6 ">
+                            <div class="login">
+                                <img src="img/login.png" alt=""/>
+                                <h5>login</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>  
+        </header>  
+        <section>
+            <div class="container pad-tb-30">
+                <div class="row">
+                    <div class="col-md-6 img">
+                        <div class="img-container">
+                            <img src="img/001.jpg" alt="" class="img-responsive"/>
+                        </div>
+                        <div class="box">
+                            <svg class="tensorsite-content__clip-shape" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 409 132"><path d="M71.2,3.2,0,44v88H409L92.3,1.8A24.1,24.1,0,0,0,71.2,3.2Z" fill="#fff"></path></svg>
+                            <a href="#"><h3><span>4</span> WHEELS</h3></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 img">
+                        <div class="">
+                            <img src="img/002.jpg" alt="" class="img-responsive"/>
+                        </div>
+                        <div class="box">
+                            <svg class="tensorsite-content__clip-shape" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 409 132"><path d="M71.2,3.2,0,44v88H409L92.3,1.8A24.1,24.1,0,0,0,71.2,3.2Z" fill="#fff"></path></svg>
+                            <a href="#"><h3><span>2</span> WHEELS</h3></a>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="pad-t-10 about img">
+                            <img src="img/003.jpg" alt="" class="img-responsive"/>
+                            <img src="img/curve-001.png" alt="" class="curve"/>
+                            <a href="#"><h3>ABOUT TOOWHEEL</h3></a>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="pad-t-10 news img">
+                            <img src="img/004.jpg" alt="" class="img-responsive"/>
+                            <a href="#"><h3>NEWS & UPDATE</h3></a>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="pad-t-10 about img">
+                            <img src="img/006.jpg" alt="" class="img-responsive"/>
+                             <img src="img/curve-001.png" alt="" class="curve"/>
+                            <a href="#"><h3>SHOP NOW</h3></a>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="pad-t-10 about img">
+                            <img src="img/005.jpg" alt="" class="img-responsive"/>
+                             <img src="img/curve-001.png" alt="" class="curve"/>
+                            <a href="#"><h3>JOIN A CLUB</h3></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="pad-t-10 img">
+                            <img src="img/007.jpg" alt="" class="img-responsive"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <footer>
+            <p>&copy;TOOWHEEL All Rights Reserved.</p>
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,364 @@
+
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+    <head>
+        <title>Toowheel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="keywords" content="Revenue Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+              SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="img/favicon.png" rel="shortcut icon"/>
+        <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+        <link rel="stylesheet" href="css/smoothbox.css" type='text/css' media="all" />
+        <link rel="stylesheet" href="css/jquery.desoslide.css">
+        <link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+        <link href="css/font-awesome.css" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
+        <link href="css/common.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <header>
+            <section class="head">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="menu">
+                                <img src="img/toggle.png" alt=""/>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="logo">
+                                <img src="img/logo.png" alt=""/>
+                            </div>
+                        </div>
+                        <div class="col-md-4 ">
+                            <div class="search">
+
+                            </div>
+                            <div class="login">
+                                <img src="img/search.png" alt=""/>
+                                <img src="img/login.png" alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>  
+        </header>  
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 img">
+                        <video  autoplay="" loop="" muted>
+                            <source src="video/landing-4w.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 img">
+                        <h3>DISCOVER</h3>
+                    </div>
+                </div>
+                <div class="row cc">
+                    <div class="col-md-2">
+                        <div class="cc-bg">
+                            <i class="fa fa-motorcycle" aria-hidden="true"></i><p class="cc-curve">125CC</p>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="cc-bg">
+                            <i class="fa fa-motorcycle" aria-hidden="true"></i><p class="cc-curve">250CC</p>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="cc-bg">
+                            <i class="fa fa-motorcycle" aria-hidden="true"></i><p class="cc-curve">500CC</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row newses">
+                    <div class="col-md-3">
+                        <div class="box-1">
+                            <img src="img/club-001.jpg" alt="" class="img-responsive"/>
+                            <div class="club">
+                                <p> <a href="#">Club Name</a></p>
+                            </div>
+                            <div class="box-1-text">
+                                <h5>News Title</h5>
+                                <p>Lorem ipsum dolor sit amet,consetetur sadipscing elitr, seddiam nonumy eirmod temporinvidunt ut labore et dolore magnaaliquyam erat, sed diam voluptua.At vero eos et accusam et justoduo dolores et ea rebum</p>
+                            </div>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <div class="box-link">
+                                <p><a href="#">DISCOVER   <i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="box-1">
+                            <img src="img/club-002.jpg" alt="" class="img-responsive"/>
+                            <div class="club">
+                                <p> <a href="#">Club Name</a></p>
+                            </div>
+                            <div class="box-1-text">
+                                <h5>News Title</h5>
+                                <p>Lorem ipsum dolor sit amet,consetetur sadipscing elitr, seddiam nonumy eirmod temporinvidunt ut labore et dolore magnaaliquyam erat, sed diam voluptua.At vero eos et accusam et justoduo dolores et ea rebum</p>
+                            </div>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <div class="box-link">
+                                <p><a href="#">DISCOVER   <i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="box-1">
+                            <img src="img/club-003.jpg" alt="" class="img-responsive"/>
+                            <div class="club">
+                                <p> <a href="#">Club Name</a></p>
+                            </div>
+                            <div class="box-1-text">
+                                <h5>News Title</h5>
+                                <p>Lorem ipsum dolor sit amet,consetetur sadipscing elitr, seddiam nonumy eirmod temporinvidunt ut labore et dolore magnaaliquyam erat, sed diam voluptua.At vero eos et accusam et justoduo dolores et ea rebum</p>
+                            </div>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <div class="box-link">
+                                <p><a href="#">DISCOVER   <i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="box-1">
+                            <img src="img/club-004.jpg" alt="" class="img-responsive"/>
+                            <div class="club">
+                                <p> <a href="#">Club Name</a></p>
+                            </div>
+                            <div class="box-1-text">
+                                <h5>News Title</h5>
+                                <p>Lorem ipsum dolor sit amet,consetetur sadipscing elitr, seddiam nonumy eirmod temporinvidunt ut labore et dolore magnaaliquyam erat, sed diam voluptua.At vero eos et accusam et justoduo dolores et ea rebum</p>
+                            </div>
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <div class="box-link">
+                                <p><a href="#">DISCOVER   <i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row cc">
+                    <div class="col-md-12 img">
+                        <h3>MEDIA & PRESS RELEASE</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <img src="img/news.jpg" alt="" class="img-responsive"/>
+                        <div class="box-link-news">
+                            <p> <a href="#">ALL PRESS RELEASE <i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container ad">
+                <div class="row cc">
+                    <div class="col-md-12 img">
+                        <h3>Advertisement</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="box-2">
+                            <img src="img/news-001.jpg" alt="" class="img-responsive"/>
+                            <div class="box-link-news-1">
+                                <p><a href="#">Title about ads</a></p>
+                            </div>
+                            <div class="box-link-news">
+                                <p> <a href="#">See More<i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
+                            </div>  
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="box-2">
+                            <img src="img/news-002.jpg" alt="" class="img-responsive"/>
+                            <div class="box-link-news-1">
+                                <p><a href="#">Title about ads</a></p>
+                            </div>
+                            <div class="box-link-news">
+                                <p> <a href="#">See More<i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 upcoming">
+
+                        <div class="box-3">
+                            <h3>Upcoming Events</h3> 
+                            <div class="row img-adj">
+                                <div class="col-md-12">
+                                    <img src="img/events-001.jpg" alt=""/>
+                                    <div class="text-1">
+                                        <p>Date</p>
+                                        <h5>Location</h5>
+                                        <h3>Event Title</h3>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="row img-adj">
+                                <div class="col-md-12">
+                                    <img src="img/events-001.jpg" alt=""/>
+                                    <div class="text-1">
+                                        <p>Date</p>
+                                        <h5>Location</h5>
+                                        <h3>Event Title</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row img-adj">
+                                <div class="col-md-12">
+                                    <img src="img/events-001.jpg" alt=""/>
+                                    <div class="text-1">
+                                        <p>Date</p>
+                                        <h5>Location</h5>
+                                        <h3>Event Title</h3>
+                                    </div>
+                                </div>
+                            </div> 
+                            <div class="row img-adj">
+                                <div class="col-md-12">
+                                    <img src="img/events-001.jpg" alt=""/>
+                                    <div class="text-1">
+                                        <p>Date</p>
+                                        <h5>Location</h5>
+                                        <h3>Event Title</h3>
+                                    </div>
+                                </div>
+                            </div> 
+                            <div class="row ">
+                                <div class="col-md-12">
+                                    <div class="event">
+                                        <p> <a href="#">See More<i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
+                                    </div>
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container discover">
+                <div class="row">
+                    <div class="col-md-12">
+                        <img src="img/photo.jpg" alt="" class="img-responsive"/>
+                        <div class="box-link-news">
+                            <p> <a href="#">PHOTOS<i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container discover">
+                <div class="row cc">
+                    <div class="col-md-12 img">
+                        <h3>VIDEOS</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <iframe width="100%" height="auto" src="https://www.youtube.com/embed/xkO9mtXtp-A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div class="col-md-4">
+                                <iframe width="100%" height="auto" src="https://www.youtube.com/embed/LR623cfrI9k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div class="col-md-4">
+                                <iframe width="100%" height="auto" src="https://www.youtube.com/embed/xkO9mtXtp-A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div class="col-md-4">
+                                <iframe width="100%" height="auto" src="https://www.youtube.com/embed/LR623cfrI9k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div class="col-md-4">
+                                <iframe width="100%" height="auto" src="https://www.youtube.com/embed/xkO9mtXtp-A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                            <div class="col-md-4">
+                                <iframe width="100%" height="auto" src="https://www.youtube.com/embed/LR623cfrI9k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-1"></div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container discover">
+                <div class="row cc">
+                    <div class="col-md-12 img">
+                        <h3>PARTNERS</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="row partners">
+                            <img src="img/partner-001.png" alt=""/>
+                            <img src="img/partner-002.png" alt=""/>
+                            <img src="img/partner-004.png" alt=""/>
+                            <img src="img/partner-005.png" alt=""/>
+                            <img src="img/partner-006.png" alt=""/>
+                            <img src="img/partner-007.png" alt=""/>
+
+                        </div>
+                        <div class="row partners">
+                            <img src="img/partner-003.png" alt=""/>
+                            <img src="img/partner-008.png" alt=""/>
+                            <img src="img/partner-009.png" alt=""/>
+                            <img src="img/partner-010.png" alt=""/>
+                            <img src="img/partner-011.png" alt=""/>
+                            <img src="img/partner-012.png" alt=""/>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <p>&copy;TOOWHEEL All Rights Reserved.
+            <img src="img/logo-name-white.png" alt=""/></p>
+    </footer>
+    <script src="js/jquery-2.2.3.min.js"></script>
+    <script src="js/numscroller-1.0.js"></script>
+    <script src="js/responsiveslides.min.js"></script>
+    <script src="js/jquery.desoslide.js"></script>
+    <script src="js/jquery.flexisel.js"></script>
+    <script src="js/move-top.js"></script>
+    <script src="js/easing.js"></script>
+    <script src="js/SmoothScroll.min.js"></script>
+    <script src="js/smoothbox.jquery2.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+
+</html>
