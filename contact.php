4a03be9639144317aba13a34e12a9550b1552b12
Y&G <Y&G@YG00D6>
2019-08-29 12:23:23 +0530

    initial commit

---
+++ contact.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+    <?php
+    $page = 'home';
+    include 'head.php';
+    ?>
+    <body>
+        <div class="wrapper">
+            <!--Header Start-->
+            <?php include 'menu.php'; ?>
+            <div id="search">
+                <button type="button" class="close">×</button>
+                <form class="search-overlay-form">
+                    <input type="search" value="" placeholder="type keyword(s) here" />
+                    <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
+                </form>
+            </div>
+            <!--Header End-->
+            <!--Inner Header Start-->
+            <section class="wf100 p100 inner-header" style="background: url(images/sub-banner/003.jpg) no-repeat; background-size: cover;">
+                <div class="container">
+                    <h1>About Us</h1>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#"> About Us </a></li>
+
+                    </ul>
+                </div>
+            </section>
+            <!--Inner Header End--> 
+            <!--Contact Start-->
+            <section class="contact-page wf100 p80 pad-t-80">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="contact-form">
+                                <h3>Feel Free to Contact us</h3>
+                                <ul class="cform">
+                                    <li class="half pr-15">
+                                        <input type="text" class="form-control" placeholder="Full Name">
+                                    </li>
+                                    <li class="half pl-15">
+                                        <input type="text" class="form-control" placeholder="Email">
+                                    </li>
+                                    <li class="half pr-15">
+                                        <input type="text" class="form-control" placeholder="Contact">
+                                    </li>
+                                    <li class="half pl-15">
+                                        <input type="text" class="form-control" placeholder="Subject">
+                                    </li>
+                                    <li class="full">
+                                        <textarea class="textarea-control" placeholder="Message"></textarea>
+                                    </li>
+                                    <li class="full">
+                                        <input type="submit" value="Contact us" class="fsubmit">
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="google-map">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15555.994296236835!2d80.1675912197754!3d12.907812900000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x68c349a2637da354!2sYellow%20and%20Gray!5e0!3m2!1sen!2sin!4v1567058504566!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container contact-info">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3>Contact Information</h3>
+                        </div>
+                        <!--Contact Info Start-->
+                        <div class="col-md-4">
+                            <div class="c-info">
+                                <h6>Address:</h6>
+                                <p> 4700 Millenia Blvd # 175, Orlando, FL 32839, USA </p>
+                            </div>
+                        </div>
+                        <!--Contact Info End--> 
+                        <!--Contact Info Start-->
+                        <div class="col-md-4">
+                            <div class="c-info">
+                                <h6>Contact:</h6>
+                                <p><strong>Phone:</strong> +1 321 2345 678-9</p>
+                                <p><strong>Fax:</strong> +1 321 2345 876-7</p>
+                            </div>
+                        </div>
+                        <!--Contact Info End--> 
+                        <!--Contact Info Start-->
+                        <div class="col-md-4">
+                            <div class="c-info">
+                                <h6>For More Information:</h6>
+                                <p><strong>Email:</strong> info@ecova.com</p>
+                                <p>contact@ecova.com</p>
+                            </div>
+                        </div>
+                        <!--Contact Info End--> 
+                    </div>
+                </div>
+            </section>
+            <!--Contact End--> 
+            <!--Footer Start-->
+            <?php include'footer.php'; ?>
+            <!--Footer End--> 
+        </div>
+
+    </body>
+</html>
---
+++ partners.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+    <?php
+    $page = 'home';
+    include 'head.php';
+    ?>
+    <body>
+        <div class="wrapper">
+            <!--Header Start-->
+            <?php include 'menu.php'; ?>
+            <div id="search">
+                <button type="button" class="close">×</button>
+                <form class="search-overlay-form">
+                    <input type="search" value="" placeholder="type keyword(s) here" />
+                    <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
+                </form>
+            </div>
+            <!--Header End-->
+            <!--Inner Header Start-->
+            <section class="wf100 p100 inner-header" style="background: url(images/sub-banner/003.jpg) no-repeat; background-size: cover;">
+                <div class="container">
+                    <h1>About Us</h1>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#"> About Us </a></li>
+
+                    </ul>
+                </div>
+            </section>
+            <!--Inner Header End--> 
+            <!--Blog Start-->
+            <section class="wf100 p80 team pad-t-80 pad-b-80">
+                <div class="team-details">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="team-large-img"> <img src="images/partners/001.jpg" alt=""> </div>
+                            </div>
+                            <div class="col-md-7">
+                                <div class="team-details-txt">
+                                    <h2>SABIC</h2>
+                                    <strong class="trank">Project Coordinator</strong>
+                                    <p class="text-justify">SABIC is composed of four strategic business units – Petrochemicals, Specialties, Agri-Nutrients, and Metals – each headed by an Executive Vice President.</p>
+                                    <p class="text-justify">The Chairman of SABIC is Dr. Abdulaziz Saleh Aljarbou. Vice-Chairman and Chief Executive Officer, Yousef Abdullah Al-Benyan, leads the global management team. The company is overseen by a Board of Directors taken from government and the private sector.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+            <section class="agallery gallery wf100 p80">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <!--title start-->
+                            <div class="section-title">
+                                <h2>Our Partners</h2>
+                            </div>
+                            <!--title end--> 
+                        </div>
+                    </div>
+                    <div class="partner-logos wf100 mb80">
+                        <div class="container">
+                            <div id="partner-logos" class="owl-carousel owl-theme">
+                                <div class="item"><img src="images/plogo1.png" alt=""></div>
+                                <div class="item"><img src="images/plogo2.png" alt=""></div>
+                                <div class="item"><img src="images/plogo3.png" alt=""></div>
+                                <div class="item"><img src="images/plogo4.png" alt=""></div>
+                                <div class="item"><img src="images/plogo5.png" alt=""></div>
+                                <div class="item"><img src="images/plogo1.png" alt=""></div>
+                                <div class="item"><img src="images/plogo2.png" alt=""></div>
+                                <div class="item"><img src="images/plogo3.png" alt=""></div>
+                                <div class="item"><img src="images/plogo4.png" alt=""></div>
+                                <div class="item"><img src="images/plogo5.png" alt=""></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!--Blog End--> 
+            <!--Footer Start-->
+            <?php include'footer.php'; ?>
+            <!--Footer End--> 
+        </div>
+
+    </body>
+</html>
