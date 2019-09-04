cb7237beb44778218a92b609385ce56a5f3f33af
Y&G <Y&G@YG00D6>
2019-09-04 17:20:20 +0530

    Merge origin/vpnc-product into vpnc-product
    Conflicts:
    	css/common.css

---
+++ mega-menu.php
@@ -0,0 +1,3 @@
+<section id="mySidenav">
+   
+</section>
--- menu.php
+++ menu.php
@@ -1,24 +1,33 @@
 <section class="header">
     <div class="container">
         <div class="row">
-            <span class="toggle" style="font-size:30px;cursor:pointer" onclick="openNav()">
+            <span class="toggle" style="font-size:30px;cursor:pointer" id onclick="openNav()">
                 <img class="toggle" src='img/toggle.png' alt=''>
             </span>
             <div id="mySidenav" class="sidenav">
-                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                <ul>
-                    <li>
-                        <a href="#">About <span class="dropdown-toggle"></span></a>
-                        <div class="dropdown-menu">
-                            <ul>
-                                <li>Test</li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Clients</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
+                <div class="container">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                    <div class="tab">
+                        <button class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">London</button>
+                        <button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
+                        <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
+                    </div>
+
+                    <div id="London" class="tabcontent">
+                        <h3>London</h3>
+                        <p>London is the capital city of England.</p>
+                    </div>
+
+                    <div id="Paris" class="tabcontent">
+                        <h3>Paris</h3>
+                        <p>Paris is the capital of France.</p> 
+                    </div>
+
+                    <div id="Tokyo" class="tabcontent">
+                        <h3>Tokyo</h3>
+                        <p>Tokyo is the capital of Japan.</p>
+                    </div>
+                </div>
             </div>
             <a href="index.php" class="logo"><img src='img/logo.png' alt=''></a>
 
