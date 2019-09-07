65844a18cfe07a1d63f1f0eb02f23e3b07fe4b58
Y&G <Y&G@YG00D6>
2019-09-07 12:24:58 +0530

    Merge origin/twowheel into twowheel
    Conflicts:
    	menu.php

--- footer.php
+++ footer.php
@@ -167,5 +167,24 @@
 // Get the element with id="defaultOpen" and click on it
     document.getElementById("defaultOpen").click();
 </script>
+<script>
+    function openCit(evt, cityName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabconten");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += "active";
+    }
+
+// Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+</script>
+
 
 
