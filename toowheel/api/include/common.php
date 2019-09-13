4ed689c9f084b52825f2ee97fc6dceea8d89904d
User <mushaqdeen@yahoo.co.in>
2019-09-13 12:29:09 +0530

    Insert added

--- toowheel/api/include/common.php
+++ toowheel/api/include/common.php
@@ -32,4 +32,59 @@ class Common {
         return $data;
     }
 
+    public function updateRecordWithWhere($fields, $table, $where) {
+        $records = [];
+        foreach ($fields as $key => $val) {
+            array_push($records, $key . ' = \'' . mysqli_real_escape_string($this->conn, $val) . '\'');
+        }
+        $this->conn->query('UPDATE `' . $table . '` SET ' . implode(',', $records) . ' WHERE ' . $where);
+        return $this->conn->affected_rows;
+    }
+
+    public function insertRecord($fields, $table) {
+        $values = array_values($fields);
+        $this->conn->query('INSERT INTO `' . $table . '` (' . implode(", ", array_keys($fields)) . ') VALUES (\'' . implode("','", array_map(array($this, 'escapeString'), $values)) . '\')');
+        if ($this->conn->affected_rows > 0) {
+            return $this->conn->insert_id;
+        } else {
+            return 0;
+        }
+    }
+    
+    public function escapeString($val) {
+        return mysqli_real_escape_string($this->conn, $val);
+    }
+
+    public function saveFile($file, $path) {
+        $name = $file['name'];
+        $temp_name = $file['tmp_name'];
+        $extension = strtolower($this->getExtension($name));
+        $key = md5(uniqid());
+        $temp_file_name = $key . '.' . $extension;
+        $dir = $path;
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
+        $temp_file_path = $path . '/' . $temp_file_name;
+        move_uploaded_file($temp_name, $temp_file_path);
+        return $temp_file_path;
+    }
+
+    public function getExtension($str) {
+        $i = strrpos($str, ".");
+        if (!$i) {
+            return "";
+        }
+        $l = strlen($str) - $i;
+        $ext = substr($str, $i + 1, $l);
+        return $ext;
+    }
+
+    public function getReplacedString($str, $replaces) {
+        foreach ($replaces as $key => $val) {
+            $str = str_replace('[' . $key . ']', $val, $str);
+        }
+        return $str;
+    }
+
 }
