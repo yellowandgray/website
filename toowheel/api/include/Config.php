e052a1852dcba67df43de9d0558be9c06fb3a485
User <mushaqdeen@yahoo.co.in>
2019-09-13 12:17:46 +0530

    Config updated

--- toowheel/api/include/Config.php
+++ toowheel/api/include/Config.php
@@ -1,9 +1,6 @@
 <?php
 
-define('DB_USERNAME', 'beta_vaganam');
-define('DB_PASSWORD', 'admin@vaganam');
-define('DB_NAME', 'beta_vaganam');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'toowheel');
 define('DB_HOST', 'localhost');
-define('IMG_BASE_URL', 'https://vaganan.in/beta/v1/');
-define('FILES_PATH', 'https://vaganan.in/beta/');
-define('PDF_PATH', '/home/g3affpfv4f1h/public_html/v1/beta/');
