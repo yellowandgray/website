bd7ae1b7003b490071da7ead8742de366c721446
Yellowandgray <Yellowandgray@YG00D7>
2019-09-14 15:38:56 +0530

    coming soon updated

--- comming-soon/index.php
+++ comming-soon/index.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
 <html>
-    <head>Project Next Door</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Project Next Door</title>
+        <meta name="description" content="Free Web tutorials">
+        <meta name="keywords" content="Project Next Door">
+        <link rel="icon" href="img/favicon.png" type="image/gif">
+        <link href="css/css.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <section class="landing">
+            <div class="landing-inner">
+                <img src="img/logo.png" />
+                <p>Site is under processing...</p>
+                <h1>Coming Soon</h1>
+            </div>
+        </section>
+    </body>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
 </html>
