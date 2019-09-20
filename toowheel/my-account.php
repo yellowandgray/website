69aaf3f8da6fa37b31d4f515a4d1fad41290ffda
Yellowandgray <Yellowandgray@YG00D7>
2019-09-20 19:34:06 +0530

    my account page updated

--- toowheel/my-account.php
+++ toowheel/my-account.php
@@ -4,73 +4,85 @@
     <body>
         <?php include 'menu.php'; ?>
         <div class="padding-top-108"></div>
-        <div class="accordion-container">
-            <h2>My Account</h2>
-            <div class="set">
-                <a href="#">
-                    Profile
-                    <i class="fa fa-plus"></i>
-                </a>
-                <div class="content">
-                    <table class="rwd-table">
-<!--                        <tr>
-                            <th>Movie Title</th>
-                            <th>Genre</th>
-                            <th>Year</th>
-                            <th>Gross</th>
-                        </tr>-->
-                        <tr>
-                            <th>PROFILE</th>
-                            <td><img src="img/person.png"> <input type="file" /></td>
-                            <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
-                        </tr>
-                        <tr>
-                            <th>NAME</th>
-                            <td>Member Name</td>
-                            <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
-                        </tr>
-                        <tr>
-                            <th>BIRTHDAY</th>
-                            <td>January 1, 1990</td>
-                            <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
-                        </tr>
-                        <tr>
-                            <th>GENDER</th>
-                            <td>Male</td>
-                            <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
-                        </tr>
-                        <tr>
-                            <th>PASSWORD</th>
-                            <td>********</td>
-                            <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
-                        </tr>
-                    </table>
+        <div class="my-account-section">
+            <div class="container">
+                <div class="display-flex">
+                    <div class="flex-icon">
+                        <div><img src="img/my-account/edit.png" alt="" /></div>
+                        <div><img src="img/my-account/setting.png" alt="" /></div>
+                        <div><img src="img/my-account/bell.png" alt="" /></div>
+                    </div>
                 </div>
-            </div>
-            <div class="set">
-                <a href="#">
-                    Contact Info 
-                    <i class="fa fa-plus"></i>
-                </a>
-                <div class="content">
-                    <table class="rwd-table">
-                    <!--                        <tr>
-                                                <th>Movie Title</th>
-                                                <th>Genre</th>
-                                                <th>Year</th>
-                                                <th>Gross</th>
-                                            </tr>-->
-                        <tr>
-                            <th>EMAIL</th>
-                            <td>member@email.com</td>
-                            <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
-                        </tr>
-                        <tr>
-                            <th>PHONE</th>
-                            <td>123456789</td>
-                            <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
-                        </tr>
-                    </table>
+                <div class="profile-section">
+                    <div class="profile-picture-section">
+                        <div class="">
+                            <img src="img/my-account/person.png" alt="" />
+                        </div>
+                    </div>
+                    <div class="profile-details-section">
+                        <div class="profile-club-details">
+                            <span>2 WHEELS</span>
+                            <span>250CC</span>
+                        </div>
+                        <span>RTM BIKERS CLUB</span>
+                        <h2>MUHAMAD HAFIZI</h2>
+                        <div class="profile-table">
+                            <table>
+                                <tr>
+                                    <th>Age</th>
+                                    <td>:</td>
+                                    <td>34</td>
+                                </tr>
+                                <tr>
+                                    <th>Gender</th>
+                                    <td>:</td>
+                                    <td>Male</td>
+                                </tr>
+                                <tr>
+                                    <th>Date of Birth</th>
+                                    <td>:</td>
+                                    <td>November 14 1985</td>
+                                </tr>
+                                <tr>
+                                    <th>Email Address</th>
+                                    <td>:</td>
+                                    <td>Muhamadhafizi@gmail.com</td>
+                                </tr>
+                                <tr>
+                                    <th>Contact No</th>
+                                    <td>:</td>
+                                    <td>+3011 245 24077</td>
+                                </tr>
+                                <tr>
+                                    <th>Address</th>
+                                    <td>:</td>
+                                    <td>Jalan SS 6/3, Ss 6, 47301<br/> Petaling Jaya, Selangor.</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="profile-details-section-1">
+                        <div class="profile-table">
+                            <h4>COVERAGE INFORMATION (NEXT OF KIN)</h4>
+                            <table>
+                                <tr>
+                                    <th>Full Name</th>
+                                    <td>:</td>
+                                    <td>Muhamad Farhan</td>
+                                </tr>
+                                <tr>
+                                    <th>Contact No.</th>
+                                    <td>:</td>
+                                    <td>+3011 245 24077</td>
+                                </tr>
+                                <tr>
+                                    <th>Address</th>
+                                    <td>:</td>
+                                    <td>Jalan SS 6/3, Ss 6, 47301<br/> Petaling Jaya, Selangor.</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
