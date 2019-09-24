38d5b29351effd1cbbec64eb3098accb012f4811
Yellowandgray <Yellowandgray@YG00D7>
2019-09-24 13:38:54 +0530

    pages updated

--- toowheel/news.php
+++ toowheel/news.php
@@ -17,7 +17,12 @@
                     <div class="col-md-8">
                         <p class="clb-bg clr-white">WFC</p>
                         <div class="events-main-content">
-                            <h2>NEWS TITLE COMES HERE</h2>
+                            <span>Media | Author Name | 24-09-2019</span>
+                            <div class="row">
+                                <h2>NEWS TITLE COMES HERE</h2>
+                                <span class="twitter-share" data-js="twitter-share">Twitter</span>
+                                <span class="facebook-share" data-js="facebook-share">Facebook</span>
+                            </div>
                             <strong>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </strong><br/><br/>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                             <img src="img/attachment-bg.jpg" alt="" style="width: 100%" /><br/><br/>
@@ -122,5 +127,28 @@
             </div>
         </div>
         <?php include 'footer.php'; ?>
+        <script type="text/javascript">
+            var twitterShare = document.querySelector('[data-js="twitter-share"]');
+
+            twitterShare.onclick = function (e) {
+                e.preventDefault();
+                var twitterWindow = window.open('https://twitter.com/share?url=' + document.URL, 'twitter-popup', 'height=350,width=600');
+                if (twitterWindow.focus) {
+                    twitterWindow.focus();
+                }
+                return false;
+            }
+
+            var facebookShare = document.querySelector('[data-js="facebook-share"]');
+
+            facebookShare.onclick = function (e) {
+                e.preventDefault();
+                var facebookWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=' + document.URL, 'facebook-popup', 'height=350,width=600');
+                if (facebookWindow.focus) {
+                    facebookWindow.focus();
+                }
+                return false;
+            }
+        </script>
     </body>
 </html>
