31e4c1a7e12e791d55eb4a5266f31e8c1b82cb30
User <mushaqdeen@yahoo.co.in>
2019-09-30 14:15:31 +0530

    Event added

--- toowheel/admin/src/app/clubevent/clubevent.component.ts
+++ toowheel/admin/src/app/clubevent/clubevent.component.ts
@@ -40,7 +40,7 @@ image_url: string = 'http://ec2-13-233-145-114.ap-south-1.compute.amazonaws.com/
 
     dialogRef.afterClosed().subscribe(result => {
         if(result !== false && result !== 'false') {
-            this.getClubEvent();
+            this.getEvent();
         }
     });
 }
@@ -54,33 +54,118 @@ image_url: string = 'http://ec2-13-233-145-114.ap-south-1.compute.amazonaws.com/
 export class ClubEventForm {
     clubeventForm: FormGroup;
     loading = false;
+    categories:any[];
+    clubs:any[];
+    cover_image: string = 'Choose Event Picture';
+    thumb_image: string = 'Choose Thumb Image';
+    cover_image_path: string;
+    thumb_image_path: string;
     constructor(
     public dialogRef: MatDialogRef<ClubEventForm>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    @Inject(MAT_DIALOG_DATA) public data: any,
     private _snackBar: MatSnackBar,
     private httpClient: HttpClient) {}
 
   ngOnInit() {
       this.clubeventForm = new FormGroup({
-      'club_type': new FormControl('', Validators.required),
-      'club_category': new FormControl('', Validators.required),
-      'club_name': new FormControl('', Validators.required),
+      'type': new FormControl('', Validators.required),
+      'category_id': new FormControl('', Validators.required),
+      'club_id': new FormControl('', Validators.required),
       'title': new FormControl('', Validators.required),
       'date': new FormControl('', Validators.required),
       'location': new FormControl('', Validators.required),
-      'address': new FormControl('', Validators.required)
+      'description': new FormControl('', Validators.required)
         });
     }
-
+    getCategory(): void {
+       this.loading = true;
+          this.httpClient.get('http://ec2-13-233-145-114.ap-south-1.compute.amazonaws.com/toowheel/api/v1/get_'+this.clubeventForm.value.type+'_category').subscribe(
+              (res)=>{
+                this.loading = false;
+                if(res["result"]["error"] === false) {
+                    this.categories = res["result"]["data"];
+                }else{
+    this._snackBar.open(res["result"]["message"], '', {
+          duration: 2000,
+        });
+                }
+            },
+            (error)=>{
+                this.loading = false;
+                this._snackBar.open(error["statusText"], '', {
+          duration: 2000,
+            });
+        });
+    }
+    getClub(): void {
+       this.loading = true;
+          this.httpClient.get('http://ec2-13-233-145-114.ap-south-1.compute.amazonaws.com/toowheel/api/v1/get_club_by_category/'+this.clubeventForm.value.category_id).subscribe(
+              (res)=>{
+                this.loading = false;
+                if(res["result"]["error"] === false) {
+                    this.clubs = res["result"]["data"];
+                }else{
+    this._snackBar.open(res["result"]["message"], '', {
+          duration: 2000,
+        });
+                }
+            },
+            (error)=>{
+                this.loading = false;
+                this._snackBar.open(error["statusText"], '', {
+          duration: 2000,
+            });
+        });
+    }
+    fileProgress(fileInput: any, name:string, field: string) {
+        var fileData = <File>fileInput.target.files[0];
+        this[name] = fileData.name;
+        this.loading = true;
+          var formData = new FormData();
+          formData.append('file', fileData);
+          this.httpClient.post('http://ec2-13-233-145-114.ap-south-1.compute.amazonaws.com/toowheel/api/v1/upload_file', formData).subscribe(
+              (res)=>{
+                this.loading = false;
+                if(res["result"]["error"] === false) {
+                    this[field] = res["result"]["data"];
+                }else{
+    this._snackBar.open(res["result"]["message"], '', {
+          duration: 2000,
+        });
+                }
+            },
+            (error)=>{
+                this.loading = false;
+                this._snackBar.open(error["statusText"], '', {
+          duration: 2000,
+        });
+            });
+    }
     onSubmit() {
           if (this.clubeventForm.invalid) {
                 return;
           }
           this.loading = true;
-          this.httpClient.post('http://localhost:3000/clubevent', {club_type: this.clubeventForm.value.name, club_category: this.clubeventForm.value.name, club_name: this.clubeventForm.value.name, event_banner: this.clubeventForm.value.name, title: this.clubeventForm.value.name, date: this.clubeventForm.value.name, location: this.clubeventForm.value.name, address: this.clubeventForm.value.name, created_by: 'Admin', updated_by: 'Admin'}).subscribe(
+          var formData = new FormData();
+          formData.append('type', this.clubeventForm.value.type);
+          formData.append('category_id', this.clubeventForm.value.category_id);
+          formData.append('club_id', this.clubeventForm.value.club_id);
+          formData.append('cover_image', this.cover_image_path);
+          formData.append('thumb_image', this.thumb_image_path);
+          formData.append('title', this.clubeventForm.value.title);
+          formData.append('event_date', this.clubeventForm.value.date);
+          formData.append('location', this.clubeventForm.value.location);
+          formData.append('description', this.clubeventForm.value.description);
+          this.httpClient.post('http://ec2-13-233-145-114.ap-south-1.compute.amazonaws.com/toowheel/api/v1/insert_event', formData).subscribe(
               (res)=>{
                 this.loading = false;
-                this.dialogRef.close(true);
+                if(res["result"]["error"] === false) {
+                    this.dialogRef.close(true);
+                }else{
+            this._snackBar.open(res["result"]["message"], '', {
+              duration: 2000,
+            });
+            }
             },
             (error)=>{
                 this.loading = false;
