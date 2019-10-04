f8d79e99b0b5f4e3afe48bb498303487591dfc05
Yellowandgray <Yellowandgray@YG00D7>
2019-10-04 12:59:12 +0530

    css updated

--- toowheel/press-release.php
+++ toowheel/press-release.php
@@ -8,17 +8,19 @@
             <div class="container">
                 <h3>PRESS RELEASE</h3>
                 <div class="press-release-content">
-                    <div class="row">
-                        <div class="col-md-2">
-                            <img src="img/press-release/mic.png" alt="">
-                        </div>
-                        <div class="col-md-8">
-                            <h4>PRESS RELEASE EVENTS TITLE COMES HERE </h4>
-                            <span>Media | Author Name | 23 December 2019</span>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        </div>
-                        <div class="col-md-2">
-                            <a href="" class="press-download-button"> Read More</a>
+                    <div class="border-bottom">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <img src="img/login-pic.jpg" alt="">
+                            </div>
+                            <div class="col-md-10">
+                                <h4>PRESS RELEASE EVENTS TITLE COMES HERE </h4>
+                                <span>Media | Author Name | 23 December 2019</span>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <a href="#" class="press-download-button"> Read More</a>
+                                <br/>
+                                <br/>
+                            </div>
                         </div>
                     </div>
                 </div>
