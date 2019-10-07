c76248b25eb33ebf217e176b8e5a61d85d537deb
Yellowandgray <Yellowandgray@YG00D7>
2019-10-07 18:42:50 +0530

    landing page color code added

--- index.php
+++ index.php
@@ -110,137 +110,143 @@ $configs = $obj->getLandingDetails();
                 </div>
             </div>
         </section>
-        <!--        <footer class="footer slide slide-four" data-background="#111111">
-                    <div class="container footer-sec inside">
-                        <div class="footer-section fooetr-1">
-                            <img src="img/footer-logo.png" alt=""/>
-                        </div>
-                        <div class="footer-section fooetr-2">
-                            <ul class="footer__nav">
-                                <li class="nav__item">
-                                    <h2 class="footer-text">2 Wheel</h2>
-                                    <h2 class="footer-text">4 Wheel</h2>
-                                    <h2 class="footer-text">NEWS & MEDIA</h2>
-                                    <h2 class="footer-text">UPCOMING EVENTS</h2>
-                                    <h2 class="footer-text">PRESS RELEASE</h2>
-                                                        <ul class="nav__ul">
-                                                            <li>
-                                                                <a href="#">Home</a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#">News & Media</a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#">Press Release</a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#">Gallery</a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#">Join A Club</a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#">Partners</a>
-                                                            </li>
-                                                        </ul>
+        <footer class="footer">
+            <div class="container footer-sec">
+                <div class="footer-section fooetr-1">
+                    <img src="img/footer-logo.png" alt=""/>
+                </div>
+                <div class="footer-section fooetr-2">
+                    <ul class="footer__nav">
+                        <li class="nav__item">
+                            <a href="toowheel/index.php" class="footer-text">2 Wheel</a>
+                            <br/>
+                            <a href="toowheel/index.php" class="footer-text">4 Wheel</a>
+                            <br/>
+                            <a href="toowheel/news.php" class="footer-text">NEWS & MEDIA</a>
+                            <br/>
+                            <a href="toowheel/events.php" class="footer-text">UPCOMING EVENTS</a>
+                            <br/>
+                            <a href="toowheel/press-release.php" class="footer-text">PRESS RELEASE</a>
+                            <!--                                        <ul class="nav__ul">
+                                                                        <li>
+                                                                            <a href="#">Home</a>
+                                                                        </li>
+                                                                        <li>
+                                                                            <a href="#">News & Media</a>
+                                                                        </li>
+                                                                        <li>
+                                                                            <a href="#">Press Release</a>
+                                                                        </li>
+                                                                        <li>
+                                                                            <a href="#">Gallery</a>
+                                                                        </li>
+                                                                        <li>
+                                                                            <a href="#">Join A Club</a>
+                                                                        </li>
+                                                                        <li>
+                                                                            <a href="#">Partners</a>
+                                                                        </li>
+                                                                    </ul>-->
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-section  fooetr-3">
+                    <ul class="footer__nav">
+                        <li class="nav__item">
+                            <h2 class="nav__title">MEMBERS</h2>
+                            <ul class="nav__ul">
+                                <li>
+                                    <a href="toowheel/login.php">Login</a>
+                                </li>
+                                <li>
+                                    <a href="toowheel/member-register.php">Be A Member</a>
+                                </li>
+                                <li>
+                                    <a href="toowheel/member-benefits.php">Member Benefits</a>
                                 </li>
                             </ul>
-                        </div>
-                        <div class="footer-section  fooetr-3">
-                            <ul class="footer__nav">
-                                <li class="nav__item">
-                                    <h2 class="nav__title">MEMBERS</h2>
-                                    <ul class="nav__ul">
-                                        <li>
-                                            <a href="toowheel/login">Login</a>
-                                        </li>
-                                        <li>
-                                            <a href="toowheel/member-register">Be A Member</a>
-                                        </li>
-                                        <li>
-                                            <a href="toowheel/member">Member Benefits</a>
-                                        </li>
-                                    </ul>
+                        </li>
+                    </ul>
+                    <ul class="footer__nav">
+                        <li class="nav__item">
+                            <h2 class="nav__title">CLUB</h2>
+                            <ul class="nav__ul">
+                                <li>
+                                    <a href="toowheel/find-a-club.php">Find A Club</a>
+                                </li>
+                                <li>
+                                    <a href="toowheel/club-register.php">Register My Club</a>
                                 </li>
                             </ul>
-                            <ul class="footer__nav">
-                                <li class="nav__item">
-                                    <h2 class="nav__title">CLUB</h2>
-                                    <ul class="nav__ul">
-                                        <li>
-                                            <a href="toowheel/find-a-club">Find A Club</a>
-                                        </li>
-                                        <li>
-                                            <a href="toowheel/club-register">Register My Club</a>
-                                        </li>
-                                    </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-section fooetr-4">
+                    <ul class="footer__nav">
+                        <li class="nav__item">
+                            <h2 class="nav__title">TOOWHEEL</h2>
+                            <ul class="nav__ul">
+                                <li>
+                                    <a href="toowheel/about.php">About us</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contact us</a>
                                 </li>
                             </ul>
-                        </div>
-                        <div class="footer-section fooetr-4">
-                            <ul class="footer__nav">
-                                <li class="nav__item">
-                                    <h2 class="nav__title">TOOWHEEL</h2>
-                                    <ul class="nav__ul">
-                                        <li>
-                                            <a href="toowheel/about">About us</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Contact us</a>
-                                        </li>
-                                    </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-section fooetr-5">
+                    <ul class="footer__nav">
+                        <li class="nav__item">
+                            <h2 class="nav__title color-w">ALWAYS GET IN TOUCH</h2>
+                            <ul class="nav__ul" style="margin-top: 10px;">
+                                <li>
+                                    <a href="#">Receive updates on our Upcoming Events</a>
+                                </li>
+                                <li>
+                                    <form>
+                                        <input type="email" placeholder="Email Address" required="">
+                                        <button type="submit">submit</button>
+                                    </form>
+                                </li>
+                                <li class="i-con text-center">
+                                    <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
                                 </li>
                             </ul>
-                        </div>
-                        <div class="footer-section fooetr-5">
-                            <ul class="footer__nav">
-                                <li class="nav__item">
-                                    <h2 class="nav__title color-w">ALWAYS GET IN TOUCH</h2>
-                                    <ul class="nav__ul" style="margin-top: 10px;">
-                                        <li>
-                                            <a href="#">Receive updates on our Upcoming Events</a>
-                                        </li>
-                                        <li>
-                                            <form>
-                                                <input type="email" placeholder="Email Address" required="">
-                                                <button type="submit">submit</button>
-                                            </form>
-                                        </li>
-                                        <li class="i-con text-center">
-                                            <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-                                            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                            <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-        
-                        <div class="legal">
-                            <p>&copy; 2019 TOOWHEEl. All Right Reserved</p>
-                            <span class="legal__links">
-                                <a href="#">Privacy Policy</a>
-                                <span></span>
-                                <a href="#">Terms of Use</a>
-                            </span>
-                        </div>
-                </footer>-->
-        <?php include 'footer.php'; ?>
-<!--        <script type="text/javascript">
-        $(window).ready(function () {
-            var wHeight = $(window).height();
-            $('.slide')
-                    .height(wHeight)
-                    .scrollie({
-                        scrollOffset: -50,
-                        scrollingInView: function (elem) {
-                            var bgColor = elem.data('background');
-                            $('body').css('background-color', bgColor);
-                        }
-                    });
-        });
-        </script>-->
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <!--        <div class="legal">
+                        <p>&copy; 2019 TOOWHEEl. All Right Reserved</p>
+                        <span class="legal__links">
+                            <a href="#">Privacy Policy</a>
+                            <span></span>
+                            <a href="#">Terms of Use</a>
+                        </span>
+                    </div>-->
+        </footer>
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.js" type="text/javascript"></script>
+        <script src="js/jquery.scrollie.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            $(window).ready(function () {
+                var wHeight = $(window).height();
+                $('.slide')
+                        .height(wHeight)
+                        .scrollie({
+                            scrollOffset: -50,
+                            scrollingInView: function (elem) {
+                                var bgColor = elem.data('background');
+                                $('body').css('background-color', bgColor);
+                            }
+                        });
+            });
+        </script>
     </body>
 </html>
