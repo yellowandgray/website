2bbcb1dca43f5e24c220118f9bea8bc1879570e4
User <mushaqdeen@yahoo.co.in>
2019-10-08 19:06:00 +0530

    News category filter added

--- toowheel/news.php
+++ toowheel/news.php
@@ -28,7 +28,7 @@ $news_gallery = $obj->selectAll('*', 'news_gallery', 'news_id = ' . $nid);
             <div class="container">
                 <div class="row events-content">
                     <div class="col-md-8">
-                        <p class="clb-bg clr-white"><?php echo $obj->charLimit($news['category'], 17); ?></p>
+                        <p class="clb-bg clr-white"><?php echo $news['club_id'] != 0 ? $obj->charLimit($news['category'], 17) : $obj->charLimit($news['sponsor'], 14); ?></p>
                         <div class="events-main-content">
                             <span><?php echo $news['media']; ?> | <?php echo $news['author_name']; ?> | <?php echo date('M d, Y', strtotime($news['news_date'])); ?></span>
                             <div class="middle">
