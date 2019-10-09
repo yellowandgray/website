e957b456ec0c1af794588908b29c0e2b87e60368
Yellowandgray <Yellowandgray@YG00D7>
2019-10-09 13:10:18 +0530

    shop now page updated

--- toowheel/shop-now.php
+++ toowheel/shop-now.php
@@ -9,16 +9,7 @@
                 <img src="img/e-kedai.jpg" alt="" class="img-responsive"/>
             </div>
         </section>
-
         <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.js" type="text/javascript"></script>
-        <script src="js/slick-slider.js" type="text/javascript"></script>
-        <script src="js/rangeslider.min.js" type="text/javascript"></script>
-        <script src="js/jquery.easing.min.js" type="text/javascript"></script>
-        <script src="js/jquery.smartWizard_step_by.min.js" type="text/javascript"></script>
-        <script src="js/script.js" type="text/javascript"></script>
-        <script type="text/javascript">
-                    </body>
-
-
-                    </html>
+    </body>
+</html>
