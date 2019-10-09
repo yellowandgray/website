712abea114329858713ea9060f7069b624de20fc
Mushaqdeen <mushaqdeen@yahoo.co.in>
2019-10-09 23:47:27 +0530

    Member retrieve

--- toowheel/admin/src/app/member/member.component.ts
+++ toowheel/admin/src/app/member/member.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
-export interface DialogData {
-  animal: string;
-  name: string;
-}
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-member',
@@ -12,19 +11,79 @@ export interface DialogData {
   styleUrls: ['./member.component.css']
 })
 export class MemberComponent implements OnInit {
-
-  constructor(public dialog: MatDialog) { }
+  result = [];
+  result_fw = [];  
+  constructor(public dialog: MatDialog, private _snackBar: MatSnackBar, private httpClient: HttpClient) { }
 
   ngOnInit() {
+      this.getCategory();
+      this.getFourWheelCategory();
   }
-  openDialog(): void  {
+  getCategory(): void {
+  this.httpClient.get<any>('../toowheel/api/v1/get_two_wheel_category')
+  .subscribe(
+          (res)=>{
+              this.result = res["result"]["data"];
+        },
+        (error)=>{
+            this._snackBar.open(error["statusText"], '', {
+      duration: 2000,
+    });
+        }
+        );
+  }
+  getFourWheelCategory(): void {
+  this.httpClient.get<any>('../toowheel/api/v1/get_four_wheel_category')
+  .subscribe(
+          (res)=>{
+              this.result_fw = res["result"]["data"];
+        },
+        (error)=>{
+            this._snackBar.open(error["statusText"], '', {
+      duration: 2000,
+    });
+        }
+        );
+  }
+  openDialog(id, res): void  {
+      var data = null;
+      if(id != 0) { 
+      this[res].forEach(val=> {
+           if(parseInt(val.category_id) === parseInt(id)) {
+                data = val;
+                return false;
+           }
+         });
+      }
     const dialogRef = this.dialog.open(MemberForm, {
         minWidth: "80%",
-        maxWidth: "80%"
+        maxWidth: "80%",
+        data: data
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
+        if(result !== false && result !== 'false') {
+            this.getCategory();
+            this.getFourWheelCategory();
+       }
+    });
+}
+confirmDelete(id): void  {
+    var data = null;
+      if(id != 0) { 
+        data = id;
+      }
+    const dialogRef = this.dialog.open(CategoryDelete, {
+        minWidth: "40%",
+        maxWidth: "40%",
+        data: data
+    });
+
+   dialogRef.afterClosed().subscribe(result => {
+       if(result !== false && result !== 'false') {
+      this.getCategory();
+      this.getFourWheelCategory();
+       }
     });
 }
 
@@ -35,11 +94,98 @@ export class MemberComponent implements OnInit {
   templateUrl: 'member-form.html',
 })
 export class MemberForm {
+    categoryForm: FormGroup;
     constructor(
     public dialogRef: MatDialogRef<MemberForm>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: any,private _snackBar: MatSnackBar,
+    private httpClient: HttpClient) {
+        this.categoryForm = new FormGroup({
+            'name': new FormControl('', Validators.required)
+        });
+        if(this.data.data != null) { 
+            this.categoryForm.patchValue({ 
+                name: this.data.data.name
+        })
+        this.category_id = this.data.data.category_id;
+    }
+    }
 
-  onNoClick(): void {
-    this.dialogRef.close();
+  onSubmit() {
+      if (this.categoryForm.invalid) {
+            return;
+      }
+      var formData = new FormData();
+      var url = '';
+      if(this.category_id != 0) {
+        formData.append('name', this.categoryForm.value.name);
+        formData.append('type', this.category_type);  
+        url = 'update_record/category/category_id = '+this.category_id;
+      } else {
+        formData.append('name', this.categoryForm.value.name);
+        formData.append('type', this.category_type);          
+        url = 'insert_category';
+      }
+      this.loading = true;
+      this.httpClient.post('../toowheel/api/v1/'+url, formData).subscribe(
+          (res)=>{
+                this.loading = false;
+                if(res["result"]["error"] === false) {
+                    this.dialogRef.close(true);
+                }else{
+this._snackBar.open(res["result"]["message"], '', {
+          duration: 2000,
+        });
+                }
+            },
+            (error)=>{
+                this.loading = false;
+                this._snackBar.open(error["statusText"], '', {
+          duration: 2000,
+        });
+            }
+        );
+  }
+}
+
+@Component({
+  selector: 'member-delete-confirmation',
+  templateUrl: 'member-delete-confirmation.html',
+})
+export class MemberDelete {
+    loading = false;
+    category_id = 0;
+    constructor(
+    public dialogRef: MatDialogRef<MemberDelete>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _snackBar: MatSnackBar,
+    private httpClient: HttpClient) {
+        if(this.data != null) { 
+            this.category_id = this.data;
+    }
+}
+
+  confirmDelete() {
+      if (this.category_id == null || this.category_id == 0) {
+            return;
+      }
+      this.loading = true;
+      this.httpClient.get('../toowheel/api/v1/delete_record/category/category_id='+this.category_id).subscribe(
+          (res)=>{
+                this.loading = false;
+                if(res["result"]["error"] === false) {
+                    this.dialogRef.close(true);
+                }else{
+this._snackBar.open(res["result"]["message"], '', {
+          duration: 2000,
+        });
+                }
+            },
+            (error)=>{
+                this.loading = false;
+                this._snackBar.open(error["statusText"], '', {
+          duration: 2000,
+        });
+            }
+        );
   }
 }
