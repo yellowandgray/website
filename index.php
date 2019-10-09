ad38a7e30a4aa11e5cb8e0c6a59c231c8dfb5d1f
Yellowandgray <Yellowandgray@YG00D7>
2019-10-09 13:02:33 +0530

    Merge origin/twowheel into twowheel
    Conflicts:
    	toowheel/css/common.css

--- index.php
+++ index.php
@@ -89,8 +89,8 @@ $configs = $obj->getLandingDetails();
                         <a href="toowheel/find-a-club.php?type=two_wheel" class="cross-btn"><span>JOIN A CLUB</span></a>
                     </div>
                     <div class="col-md-3">
-                        <a href="#"><img src="<?php echo BASE_URL . $configs['landing_shop_now_image']; ?>" alt="" /></a>
-                        <a href="toowheel/shop-now.php" class="cross-btn"><span>SHOP NOW!</span></a>
+                        <a href="toowheel/shop-now.php?type=two_wheel"><img src="<?php echo BASE_URL . $configs['landing_shop_now_image']; ?>" alt="" /></a>
+                        <a href="toowheel/shop-now.php?type=two_wheel" class="cross-btn"><span>SHOP NOW!</span></a>
                     </div>
                 </div>
             </div>
--- toowheel/shop-now.php
+++ toowheel/shop-now.php
@@ -1,25 +1,14 @@
-
 <!DOCTYPE html>
 <html>
     <?php include 'head.php'; ?>
     <body>
+        <?php include 'menu.php'; ?>
         <section class="ekedai">
             <div>
-                <i  onclick="window.history.back();" class="fa fa-arrow-left" aria-hidden="true"></i>
+                <i onclick="window.history.back();" class="fa fa-arrow-left" aria-hidden="true"></i>
                 <img src="img/e-kedai.jpg" alt="" class="img-responsive"/>
             </div>
-
         </section>
-
-
-        <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
-        <script src="js/bootstrap.js" type="text/javascript"></script>
-        <script src="js/slick-slider.js" type="text/javascript"></script>
-        <script src="js/rangeslider.min.js" type="text/javascript"></script>
-        <script src="js/jquery.easing.min.js" type="text/javascript"></script>
-        <script src="js/jquery.smartWizard_step_by.min.js" type="text/javascript"></script>
-        <script src="js/script.js" type="text/javascript"></script>
-        <script type="text/javascript">
-
-        </body>
-        </html>
+        <?php include 'footer.php'; ?>
+    </body>
+</html>
