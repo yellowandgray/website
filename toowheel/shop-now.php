fda8edce6fc9576778bfe5a275100ae61897b10e
Yellowandgray <Yellowandgray@YG00D7>
2019-10-09 13:11:30 +0530

    shop now page updated

--- toowheel/shop-now.php
+++ toowheel/shop-now.php
@@ -2,7 +2,6 @@
 <html>
     <?php include 'head.php'; ?>
     <body>
-        <?php include 'menu.php'; ?>
         <section class="ekedai">
             <div>
                 <i onclick="window.history.back();" class="fa fa-arrow-left" aria-hidden="true"></i>
