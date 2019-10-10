bf3e04f09845e6a191ad65637f4e9c6c51998e88
Yellowandgray <Yellowandgray@YG00D7>
2019-10-10 12:23:27 +0530

    club register and common css updated

--- toowheel/club-register.php
+++ toowheel/club-register.php
@@ -22,14 +22,14 @@ $obj = new Common();
                                 <input placeholder="Your club name" name="club_name" type="text" tabindex="1" required autofocus>
                             </fieldset>
                             <fieldset>
-                                <select name="type">
+                                <select name="type" tabindex="2" required>
                                     <option>Type</option>
                                     <option>2 Wheel</option>
                                     <option>4 Wheel</option>
                                 </select>
                             </fieldset>
                             <fieldset>
-                                <select name="category">
+                                <select name="category" tabindex="3" required>
                                     <option>Category</option>
                                     <option>125 cc</option>
                                     <option>250 cc</option>
@@ -39,20 +39,34 @@ $obj = new Common();
                             <br/>
                             <fieldset>
                                 <label>Cover Image :</label>
-                                <input type="file" name="cover_image" placeholder="" />
+                                <input type="file" name="cover_image" tabindex="4" required placeholder="" />
                             </fieldset>
                             <br/>
                             <fieldset>
                                 <label>Logo Image :</label>
-                                <input type="file" name="logo" placeholder="" />
+                                <input type="file" name="logo" placeholder="" tabindex="5" required />
                             </fieldset>
                             <fieldset>
-                                <select name="country">
+                                <input type="text" name="rank" placeholder="Rank" tabindex="6" required />
+                            </fieldset>
+                            <br/>
+                            <fieldset>
+                                <label>Club Video :</label>
+                                <input type="file" name="video" placeholder="" tabindex="7" required />
+                            </fieldset>
+                            <fieldset>
+                                <input placeholder="Email Address" name="email" type="email" tabindex="8" required>
+                            </fieldset>
+                            <fieldset>
+                                <input placeholder="Contact No." name="contact" type="text" tabindex="9" required>
+                            </fieldset>
+                            <fieldset>
+                                <select name="country" tabindex="10" required>
                                     <option value='Malaysia'>Malaysia</option>
                                 </select>
                             </fieldset>
                             <fieldset>
-                                <select name="state">
+                                <select name="state" tabindex="11" required>
                                     <option value="0">State</option>
                                     <option value='Kuala_Lumpur'>Kuala Lumpur</option>
                                     <option value='Labuan'>Labuan</option>
@@ -73,40 +87,37 @@ $obj = new Common();
                                 </select>
                             </fieldset>
                             <fieldset>
-                                <input placeholder="City" name="city" type="text" tabindex="4" required>
+                                <input placeholder="City" name="city" type="text" tabindex="12" required>
                             </fieldset>
                             <fieldset>
-                                <input placeholder="Zip" name="zip" type="text" tabindex="4" required>
+                                <input placeholder="Zip" name="zip" type="text" tabindex="13" required>
                             </fieldset>
                             <fieldset>
-                                <textarea placeholder="Address" name="address" type="text" tabindex="5" required></textarea>
+                                <input placeholder="Landmark" name="landmark" type="text" tabindex="14" required>
                             </fieldset>
                             <fieldset>
-                                <input placeholder="Club Leader name (Full Name)" name="leader_name" type="text" tabindex="6" required>
+                                <textarea placeholder="Address" name="address" type="text" tabindex="15" required></textarea>
                             </fieldset>
                             <fieldset>
-                                <input placeholder="No. of Members" name="no_of_members" type="text" tabindex="7" required>
+                                <input placeholder="Club Leader name (Full Name)" name="leader_name" type="text" tabindex="16" required>
                             </fieldset>
                             <fieldset>
-                                <input placeholder="Email Address" name="email" type="email" tabindex="8" required>
+                                <input placeholder="No. of Members" name="no_of_members" type="text" tabindex="17" required>
                             </fieldset>
                             <fieldset>
-                                <input placeholder="Contact No." name="contact" type="text" tabindex="9" required>
+                                <input placeholder="Facebook Link" name="fb_link" type="text" tabindex="18">
                             </fieldset>
                             <fieldset>
-                                <input placeholder="Facebook Link" name="fb_link" type="text" tabindex="10">
+                                <input placeholder="Youtube Link" name="yt_link" type="text" tabindex="19">
                             </fieldset>
                             <fieldset>
-                                <input placeholder="Youtube Link" name="yt_link" type="text" tabindex="11">
+                                <input placeholder="Twitter Link" name="tw_link" type="text" tabindex="20">
                             </fieldset>
                             <fieldset>
-                                <input placeholder="Twitter Link" name="tw_link" type="text" tabindex="12">
+                                <input placeholder="Instagram Link" name="insta_link" type="text" tabindex="21">
                             </fieldset>
                             <fieldset>
-                                <input placeholder="Instagram Link" name="insta_link" type="text" tabindex="13">
-                            </fieldset>
-                            <fieldset>
-                                <textarea placeholder="About Club" name="about_club" type="text" tabindex="14" required></textarea>
+                                <textarea placeholder="About Club" name="about_club" type="text" tabindex="22" required></textarea>
                             </fieldset>
                             <fieldset>
                                 <br/>
