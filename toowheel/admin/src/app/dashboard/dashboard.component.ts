5fb8f59f3255e4f9157955d0be5c9c865d3f6f35
Y&G <Y&G@YG00D6>
2019-10-16 19:43:34 +0530

    Merge origin/twowheel into twowheel

--- toowheel/admin/src/app/dashboard/dashboard.component.ts
+++ toowheel/admin/src/app/dashboard/dashboard.component.ts
@@ -12,13 +12,42 @@ public pieChart: GoogleChartInterface = {
     chartType: 'PieChart',
     dataTable: [
       ['Parameters', 'Hours per Day'],
-      ['Club',     11],
-      ['Member',      2],
-      ['News',  2],
-      ['Event', 2],
-      ['Advertisment',    7]
+      ['Registered Member',     11],
+      ['Registered Club',      2],
+      ['Pending Members for Approval',  2],
+      ['Number of Membership that will Expire', 2],
+      ['Number of Pending T Shirt Delivery',    7],
+      ['Website Earnings',    7],
+      ['Website Traffic',    7],
+      ['Top 10 Clubs',    7]
     ],
-   options: {'title': 'Parameters',
+   options: {'title': 'Toowheel',
  pieHole: 0.4,},
   };
+  
+  public columnChart: GoogleChartInterface = { 
+      chartType: 'ColumnChart',
+      dataTable: [
+        ['Country', 'Performance', 'Profits'],
+        ['Registered Member',     200, 900],
+      ['Registered Club', 100, 500 ],
+      ['Pending Members for Approval',  150, 400],
+      ['Number of Membership that will Expire', 600, 300],
+      ['Number of Pending T Shirt Delivery',  500, 800],
+      ['Website Earnings',   200, 600],
+      ['Website Traffic',    600, 900],
+      ['Top 10 Clubs',    1000, 700]
+      ],
+      options: {title: 'Toowheel',
+                       }
+  };
+
+  myfunction() {
+    let ccComponent = this.columnChart.component;
+    let ccWrapper = ccComponent.wrapper;
+
+   
+    ccComponent.draw();
+  }
+
 }
