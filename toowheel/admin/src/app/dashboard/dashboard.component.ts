5a055b4e3c1f9b3100c93b579e561d142642f234
Y&G <Y&G@YG00D6>
2019-10-16 17:11:05 +0530

    Merge origin/twowheel into twowheel

--- toowheel/admin/src/app/dashboard/dashboard.component.ts
+++ toowheel/admin/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+selector: 'app-dashboard',
+templateUrl: './dashboard.component.html',
+styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-  constructor() { }
+export class DashboardComponent  {
 
-  ngOnInit() {
-  }
-
+public pieChart: GoogleChartInterface = {
+    chartType: 'PieChart',
+    dataTable: [
+      ['Parameters', 'Hours per Day'],
+      ['Club',     11],
+      ['Member',      2],
+      ['News',  2],
+      ['Event', 2],
+      ['Advertisment',    7]
+    ],
+   options: {'title': 'Parameters',
+ pieHole: 0.4,},
+  };
 }
