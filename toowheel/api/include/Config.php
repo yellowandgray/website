e743b75bad55f37ec55c93dc3e50ebb63a4095b5
Yellowandgray <Yellowandgray@YG00D7>
2019-10-21 19:06:44 +0530

    config file updated

--- toowheel/api/include/Config.php
+++ toowheel/api/include/Config.php
@@ -3,5 +3,5 @@
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'toowheel');
-define('DB_HOST', 'http://localhost/');
-define('BASE_URL', 'http://ec2-13-233-145-114.ap-south-1.compute.amazonaws.com/toowheel/api/v1/');
+define('DB_HOST', 'localhost');
+define('BASE_URL', 'http://localhost/twowheel-frontend/toowheel/api/v1');
