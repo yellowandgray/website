af1edbaaec8bca7ec6c89efc66f89f44b4bfbb8f
Mushaqdeen <mushaqdeen@yahoo.co.in>
2019-10-25 21:27:55 +0530

    Member activated added - attempt2

--- toowheel/admin/src/app/login/login.component.ts
+++ toowheel/admin/src/app/login/login.component.ts
@@ -24,12 +24,17 @@ export class LoginComponent implements OnInit {
     });
   }
     onSubmit(): void {
+        if (this.loginForm.invalid) {
+            return;
+        }
+        this.loading = true;
 var formData = new FormData();
 formData.append('email', this.loginForm.value.username);
 formData.append('password', this.loginForm.value.password);
     this.httpClient.post('https://www.toowheel.com/beta/toowheel/api/v1/loginadmin', formData)
     .subscribe(
             (res)=>{
+                this.loading = false;
                  if(res["result"]["error"] === false) {
                      (res["result"]["data"]).forEach((val, key)=> {
                          sessionStorage.setItem("toowheel_"+key, val);
@@ -42,6 +47,7 @@ formData.append('password', this.loginForm.value.password);
             }
           },
           (error)=>{
+              this.loading = false;
               this._snackBar.open(error["statusText"], '', {
         duration: 2000,
       });
