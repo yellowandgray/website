76556e7d1f07aa125c1810af2abb66d0b746e0a0
Mushaqdeen <mushaqdeen@yahoo.co.in>
2019-10-26 11:50:40 +0530

    Email added

--- toowheel/api/include/Config.php
+++ toowheel/api/include/Config.php
@@ -5,3 +5,4 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'toowheel');
 define('DB_HOST', 'localhost');
 define('BASE_URL', 'http://ec2-13-233-145-114.ap-south-1.compute.amazonaws.com/toowheel/api/v1/');
+define('FILES_PATH', 'https://www.toowheel.com/toowheel/');
