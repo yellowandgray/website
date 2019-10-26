e5b8f70ed019540e16d9ecf7844e2a58013f7e7f
Mushaqdeen <mushaqdeen@yahoo.co.in>
2019-10-26 14:24:06 +0530

    Email notification updates

--- toowheel/api/include/Config.php
+++ toowheel/api/include/Config.php
@@ -6,3 +6,4 @@ define('DB_NAME', 'toowheel');
 define('DB_HOST', 'localhost');
 define('BASE_URL', 'http://ec2-13-233-145-114.ap-south-1.compute.amazonaws.com/toowheel/api/v1/');
 define('FILES_PATH', 'https://www.toowheel.com/beta/toowheel/');
+define('LOGIN_PATH', 'https://www.toowheel.com/beta/toowheel/login.php?type=two_wheel');
