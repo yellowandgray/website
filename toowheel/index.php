68e01912bd633127a6cee5058ba6009dcb781424
User <mushaqdeen@yahoo.co.in>
2019-11-09 16:43:51 +0530

    Flag news

--- toowheel/index.php
+++ toowheel/index.php
@@ -11,6 +11,8 @@ $card_add_two = $obj->selectRow('url, image', 'advertisement', 'advertisement_id
 $banner_add = $obj->selectRow('url, image', 'advertisement', 'advertisement_id = ' . $configs['home_banner_ad']);
 $categories = $obj->selectAll('*', 'category', 'category_id > 0 AND type = \'' . $type . '\'');
 $news = $obj->selectAll('n.*, m.name AS media, c.name AS club, ca.name AS category', 'news AS n LEFT JOIN media AS m ON m.media_id = n.media_id LEFT JOIN club AS c ON c.club_id = n.club_id LEFT JOIN category AS ca ON ca.category_id = n.category_id AND ca.category_id = c.category_id', 'n.news_id > 0 AND n.type = \'' . $type . '\' ORDER BY n.news_id DESC');
+$news_star = $obj->selectRow('n.*, m.name AS media, c.name AS club, ca.name AS category', 'news AS n LEFT JOIN media AS m ON m.media_id = n.media_id LEFT JOIN club AS c ON c.club_id = n.club_id LEFT JOIN category AS ca ON ca.category_id = n.category_id AND ca.category_id = c.category_id', 'n.news_id > 0 AND n.type = \'' . $type . '\' AND n.isstar = 1 ORDER BY n.news_id DESC');
+$news_flag = $obj->selectAll('n.*, m.name AS media, c.name AS club, ca.name AS category', 'news AS n LEFT JOIN media AS m ON m.media_id = n.media_id LEFT JOIN club AS c ON c.club_id = n.club_id LEFT JOIN category AS ca ON ca.category_id = n.category_id AND ca.category_id = c.category_id', 'n.news_id > 0 AND n.type = \'' . $type . '\' AND n.isflag = 1 ORDER BY n.news_id DESC');
 $events = $obj->selectAll('e.*, c.name AS club, ca.name AS category', 'event AS e LEFT JOIN club AS c ON c.club_id = e.club_id LEFT JOIN category AS ca ON ca.category_id = e.category_id', 'e.event_id > 0 AND e.type = \'' . $type . '\' ORDER BY e.event_id DESC LIMIT 4');
 $images = $obj->selectAll('*', 'gallery', 'gallery_id > 0 AND media_type = \'image\' AND type = \'' . $type . '\' ORDER BY gallery_id DESC');
 $videos = $obj->selectAll('*', 'gallery', 'gallery_id > 0 AND media_type = \'video\' AND type = \'' . $type . '\' ORDER BY gallery_id DESC');
@@ -68,17 +70,17 @@ $videos = $obj->selectAll('*', 'gallery', 'gallery_id > 0 AND media_type = \'vid
                 </div>
             </div>
         </section>
-<!--        <section class="media-press-release pad-t-80">
+        <section class="media-press-release pad-t-80">
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 padding-lr-5">
-                        <div class="news-img-01 news-01 height-one" onClick="document.location.href = 'news?nid=<?php echo $row['news_id']; ?>'">
-                            <img src="img/video/003.jpg" alt="" class="img-responsive-01">
+                        <div class="news-img-01 news-01 height-one" onClick="document.location.href = 'news?nid=<?php echo $news_star['news_id']; ?>'">
+                            <img src="<?php echo BASE_URL . $news_star['cover_image']; ?>" alt="" class="img-responsive-01">
                             <div class="news-01-cont">
                                 <div class="position-ad">
-                                    <p class="clb-bg"><?php echo $row['club_id'] != 0 ? $obj->charLimit($row['club'], 14) : $obj->charLimit($row['sponsor'], 14) ?></p>
-                                    <h2><?php echo $row['title']; ?></h2>
-                                    <p><?php echo $row['media']; ?> | <?php echo $row['author_name']; ?> | <?php echo date('M d, Y', strtotime($row['news_date'])); ?></p>
+                                    <p class="clb-bg"><?php echo $news_star['club_id'] != 0 ? $obj->charLimit($news_star['club'], 14) : $obj->charLimit($news_star['sponsor'], 14) ?></p>
+                                    <h2><?php echo $news_star['title']; ?></h2>
+                                    <p><?php echo $news_star['media']; ?> | <?php echo $news_star['author_name']; ?> | <?php echo date('M d, Y', strtotime($news_star['news_date'])); ?></p>
 
                                 </div>
                             </div>
@@ -86,61 +88,25 @@ $videos = $obj->selectAll('*', 'gallery', 'gallery_id > 0 AND media_type = \'vid
                     </div>
                     <div class="col-md-6">
                         <div class="row" style="margin-bottom: 12px;">
-                            <div class="col-md-6 col-sm-6 col-xs-6 padding-lr-5">
-                                <div class="news-img-02 news-01 height-two" onClick="document.location.href = 'news?nid=<?php echo $row['news_id']; ?>'">
-                                    <img src="img/events/001.jpg" alt="" class="img-responsive-h"/>
-                                    <div class=" news-02-cont">
-                                        <div class="position-ad-01">
-                                            <p class="clb-bg"><?php echo $row['club_id'] != 0 ? $obj->charLimit($row['club'], 14) : $obj->charLimit($row['sponsor'], 14) ?></p>
-                                            <h2><?php echo $row['title']; ?></h2>
+                            <?php foreach ($news_flag as $row) { ?>
+                                <div class="col-md-6 col-sm-6 col-xs-6 padding-lr-5">
+                                    <div class="news-img-02 news-01 height-two" onClick="document.location.href = 'news?nid=<?php echo $row['news_id']; ?>'">
+                                        <img src="img/events/001.jpg" alt="" class="img-responsive-h"/>
+                                        <div class=" news-02-cont">
+                                            <div class="position-ad-01">
+                                                <p class="clb-bg"><?php echo $row['club_id'] != 0 ? $obj->charLimit($row['club'], 14) : $obj->charLimit($row['sponsor'], 14) ?></p>
+                                                <h2><?php echo $row['title']; ?></h2>
 
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6 col-sm-6 col-xs-6 padding-lr-5">
-                                <div class="news-img-03 news-01 height-two" onClick="document.location.href = 'news?nid=<?php echo $row['news_id']; ?>'">
-                                    <img src="img/events/003.jpg" alt="" class="img-responsive-h"/>
-                                    <div class=" news-02-cont">
-                                        <div class="position-ad-01">
-                                            <p class="clb-bg"><?php echo $row['club_id'] != 0 ? $obj->charLimit($row['club'], 14) : $obj->charLimit($row['sponsor'], 14) ?></p>
-                                            <h2><?php echo $row['title']; ?></h2>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row"> 
-                            <div class="col-md-6 col-sm-6 padding-lr-5">
-                                <div class="news-img-04 news-01 height-two" onClick="document.location.href = 'news?nid=<?php echo $row['news_id']; ?>'">
-                                    <img src="img/events/005.jpg" alt="" class="img-responsive-h"/>
-                                    <div class=" news-02-cont">
-                                        <div class="position-ad-01">
-                                            <p class="clb-bg"><?php echo $row['club_id'] != 0 ? $obj->charLimit($row['club'], 14) : $obj->charLimit($row['sponsor'], 14) ?></p>
-                                            <h2><?php echo $row['title']; ?></h2>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-sm-6 padding-lr-5">
-                                <div class="news-img-05 news-01 height-two" onClick="document.location.href = 'news?nid=<?php echo $row['news_id']; ?>'">
-                                    <img src="img/events/002.jpg" alt="" class="img-responsive-h"/>
-                                    <div class=" news-02-cont">
-                                        <div class="position-ad-01">
-                                            <p class="clb-bg"><?php echo $row['club_id'] != 0 ? $obj->charLimit($row['club'], 14) : $obj->charLimit($row['sponsor'], 14) ?></p>
-                                            <h2><?php echo $row['title']; ?></h2>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>-->
+        </section>
         <section class="media-press-release" onClick="document.location.href = 'press-release?type=<?php echo $type; ?>'">
             <div class="container">
                 <div class="col-md-12">
