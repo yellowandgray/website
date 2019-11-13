608e2669a3463f3758ba80311c94e6badb6007a7
Y&G <Y&G@YG00D6>
2019-11-13 14:56:53 +0530

    video gallery edited

--- toowheel/index.php
+++ toowheel/index.php
@@ -65,7 +65,7 @@ $press_release_menu = $obj->selectAll('p.*, m.name AS media', 'press_release AS 
                                 <div class="discover-slider-content">
                                     <p class="clb-bg"><?php echo $row['club_id'] != 0 ? $obj->charLimit($row['club'], 14) : $obj->charLimit($row['sponsor'], 14) ?></p>
                                     <h2><?php echo $row['title']; ?></h2>
-    <!--                                    <p><?php //echo $obj->charLimit($row['moto_text'], 60);              ?></p>-->
+    <!--                                    <p><?php //echo $obj->charLimit($row['moto_text'], 60);                ?></p>-->
 
                                     <div class="discover-btn">
                                         <a href="news?nid=<?php echo $row['news_id']; ?>" class="discover-btn-home">DISCOVER</a>
@@ -125,7 +125,7 @@ $press_release_menu = $obj->selectAll('p.*, m.name AS media', 'press_release AS 
                             <?php foreach ($press_release_menu as $row) { ?>
                                 <div class="row margin-b-20 ">
                                     <!--                                    <div class="col-md-2">
-                                                                            <div class="home-press-release-bg" style="background: url(<?php //echo BASE_URL . $row['thumb_image'];         ?>)no-repeat;background-position: center;background-size: cover;cursor: pointer;width: 100%;height: 150px;"></div>
+                                                                            <div class="home-press-release-bg" style="background: url(<?php //echo BASE_URL . $row['thumb_image'];           ?>)no-repeat;background-position: center;background-size: cover;cursor: pointer;width: 100%;height: 150px;"></div>
                                                                         </div>-->
                                     <div class="col-md-12">
                                         <div class="press-width float-right">
@@ -188,7 +188,7 @@ $press_release_menu = $obj->selectAll('p.*, m.name AS media', 'press_release AS 
                 <div class="row">
                     <div class="col-md-12 col-sm-12 col-xs-12">
                         <h1>PHOTOS</h1>
-                        <!--<div class="s-photo"><a href="gallery?type=<?php //echo $type;              ?>" class="btn-secondary">SEE MORE PHOTOS</a></div>-->
+                        <!--<div class="s-photo"><a href="gallery?type=<?php //echo $type;                ?>" class="btn-secondary">SEE MORE PHOTOS</a></div>-->
                     </div>
                 </div>
             </div>
@@ -232,12 +232,12 @@ $press_release_menu = $obj->selectAll('p.*, m.name AS media', 'press_release AS 
             <div class="container">
                 <div class="flex-row">
                     <h1>VIDEOS</h1>
-                    <!--<div class="s-photo"><a href="gallery?type=<?php //echo $type;             ?>" class="btn-secondary">SEE MORE VIDEOS</a></div>-->
+                    <!--<div class="s-photo"><a href="gallery?type=<?php //echo $type;               ?>" class="btn-secondary">SEE MORE VIDEOS</a></div>-->
                     <!--                    <div class="home-gallery video-section">
                     <?php if ($type == 'two_wheel') { ?>
-                                                    <a href="<?php echo BASE_URL . $videos[0]['media_path']; ?>" class="html5lightbox" title="<?php echo $videos[0]['title']; ?> <?php echo $videos[0]['description']; ?>" data-group="videogroup"><img src="<?php echo BASE_URL . $configs['two_wheel_videos']; ?>" class="popup-img" alt="" /></a>
+                                                            <a href="<?php echo BASE_URL . $videos[0]['media_path']; ?>" class="html5lightbox" title="<?php echo $videos[0]['title']; ?> <?php echo $videos[0]['description']; ?>" data-group="videogroup"><img src="<?php echo BASE_URL . $configs['two_wheel_videos']; ?>" class="popup-img" alt="" /></a>
                     <?php } else { ?>
-                                                    <a href="<?php echo BASE_URL . $videos[0]['media_path']; ?>" class="html5lightbox" title="<?php echo $videos[0]['title']; ?> <?php echo $videos[0]['description']; ?>" data-group="videogroup"><img src="<?php echo BASE_URL . $configs['four_wheel_videos']; ?>" class="popup-img" alt="" /></a>
+                                                            <a href="<?php echo BASE_URL . $videos[0]['media_path']; ?>" class="html5lightbox" title="<?php echo $videos[0]['title']; ?> <?php echo $videos[0]['description']; ?>" data-group="videogroup"><img src="<?php echo BASE_URL . $configs['four_wheel_videos']; ?>" class="popup-img" alt="" /></a>
                     <?php } ?>
                                         </div>-->
                 </div>
@@ -248,10 +248,10 @@ $press_release_menu = $obj->selectAll('p.*, m.name AS media', 'press_release AS 
                         <div class="column-1">
                             <div class="video-gal">
                                 <img src="img/photo/007.jpg" alt="" class="img-responsive"/>
-                                <video id="myVideo1" width="100%" height="500"  muted="">
+                                <video onmouseover="this.play()" onmouseout="this.pause();this.currentTime = 0;" id="myVideo1" width="100%" height="500"  muted="">
                                     <source src="video/002.mp4" type="video/mp4">
                                 </video>
-                                <img  id="vimg01" src="img/photo/007.jpg" alt="" class="img-responsive popup-img"/>
+                                <!--<img  id="vimg01" src="img/photo/007.jpg" alt="" class="img-responsive popup-img"/>-->
                                 <a href="video/002.mp4" class="html5lightbox" title="" data-group="videogroup"> <i class="fa fa-play-circle-o" aria-hidden="true"></i></a>
                                 <div class="video-t-1" id="text-v">
                                     <h4>Toowheel</h4>
@@ -261,20 +261,22 @@ $press_release_menu = $obj->selectAll('p.*, m.name AS media', 'press_release AS 
                         <div class="column-1">
 
                             <div class="video-gal">
-                                <video id="myVideo3" width="100%" height="250" muted="" >
+                                <img id="vimg02" src="img/photo/009.jpg" alt="" class="img-responsive"/>
+                                <video onmouseover="this.play()" onmouseout="this.pause();this.currentTime = 0;" id="myVideo3" width="100%" height="250" muted="" >
                                     <source src="video/004.mp4" type="video/mp4">
                                 </video>
-                                <img id="vimg02" src="img/photo/009.jpg" alt="" class="img-responsive"/>
+
                                 <a href="video/004.mp4" class="html5lightbox" title="1st convoy arrival , stay tuned for official video . Thx to fan cam" data-group="videogroup"> <i  class="fa fa-play-circle-o" aria-hidden="true"></i></a>
                                 <div class="video-t-1" id="text-v">
                                     <h4>1st convoy arrival , stay tuned for official video . Thx to fan cam </h4>
                                 </div>
                             </div>
                             <div class="video-gal">
-                                <video id="myVideo2" width="100%" height="250" muted="" >
+                                <img id="vimg03" src="img/photo/008.jpg" alt="" class="img-responsive"/>
+                                <video onmouseover="this.play()" onmouseout="this.pause();this.currentTime = 0;" id="myVideo2" width="100%" height="250" muted="" >
                                     <source src="video/003.mp4" type="video/mp4">
                                 </video>
-                                <img id="vimg03" src="img/photo/008.jpg" alt="" class="img-responsive"/>
+
                                 <a href="video/003.mp4" class="html5lightbox" title="2nd convoy , stay tuned for official video." data-group="videogroup"> <i  class="fa fa-play-circle-o" aria-hidden="true"></i></a>
                                 <div class="video-t-1" id="text-v">
                                     <h4>2nd convoy , stay tuned for official video.</h4>
@@ -283,10 +285,11 @@ $press_release_menu = $obj->selectAll('p.*, m.name AS media', 'press_release AS 
                         </div>
                         <div class="column-1">
                             <div class="video-gal">
-                                <video id="myVideo4" width="100%" height="250" muted="">
+                                <img  id="vimg04" src="img/photo/010.jpg" alt="" class="img-responsive"/>
+                                <video onmouseover="this.play()" onmouseout="this.pause();this.currentTime = 0;" id="myVideo4" width="100%" height="250" muted="">
                                     <source src="video/005.mp4" type="video/mp4">
                                 </video>
-                                <img id="vimg04" src="img/photo/010.jpg" alt="" class="img-responsive"/>
+
                                 <a href="video/005.mp4" class="html5lightbox" title="3rd convoy in coming." data-group="videogroup"> <i  class="fa fa-play-circle-o" aria-hidden="true"></i></a>
                                 <div class="video-t-1" id="text-v">
                                     <h4>3rd convoy in coming.</h4>
@@ -294,10 +297,11 @@ $press_release_menu = $obj->selectAll('p.*, m.name AS media', 'press_release AS 
                                 </div>
                             </div>
                             <div class="video-gal">
-                                <video id="myVideo5" width="100%" height="250" muted="" >
+                                <img id="vimg05" src="img/photo/011.jpg" alt="" class="img-responsive"/>
+                                <video onmouseover="this.play()" onmouseout="this.pause();this.currentTime = 0;" id="myVideo5" width="100%" height="250" muted="" >
                                     <source src="video/006.mp4" type="video/mp4">
                                 </video>
-                                <img id="vimg05" src="img/photo/011.jpg" alt="" class="img-responsive"/>
+
                                 <a href="video/006.mp4" class="html5lightbox" title="Soft Launching Toowheel Group" data-group="videogroup"> <i onclick="playVid('myVideo1', 'vimg01')"  class="fa fa-play-circle-o" aria-hidden="true"></i></a>
                                 <div class="video-t-1" id="text-v">
                                     <h4>Soft Launching Toowheel Group</h4>
@@ -454,7 +458,21 @@ $press_release_menu = $obj->selectAll('p.*, m.name AS media', 'press_release AS 
                                     }
         </script>
         <script>
+            var figure = $(".video");
+            var vid = $("video");
 
+            [].forEach.call(figure, function (item) {
+                item.addEventListener('mouseover', hoverVideo, false);
+                item.addEventListener('mouseout', hideVideo, false);
+            });
+
+            function hoverVideo(e) {
+                $('.thevideo')[0].play();
+            }
+
+            function hideVideo(e) {
+                $('.thevideo')[0].pause();
+            }
         </script>
     </body>
 </html>
