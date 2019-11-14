eddb10f1ca651900203558184539617a353b2ed1
Yellowandgray <Yellowandgray@YG00D7>
2019-11-14 14:21:30 +0530

    style and responsive css updated

--- toowheel/workshop-landing.php
+++ toowheel/workshop-landing.php
@@ -19,24 +19,64 @@ $obj = new Common();
                         <h3>WORKSHOP</h3>
                     </div>
                 </div>
-                <div class="row border-bottom">
-                    <div class="col-md-2">
-                        <img src="img/slider/slider-002.png" alt="" class="image-border-width">
-                    </div>
-                    <div class="col-md-10">
-                        <h4>Workshop Title</h4>
-                        <span>Location | Expertise(Category)</span>
-                        <div class="rating">
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star checked"></span> 
+                <div class="border-bottom" style="padding-bottom: 40px;padding-top: 40px;">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <img src="img/slider/slider-002.png" alt="" class="image-border-width">
                         </div>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <a href="workshop?type=<?php echo $type; ?>" class="workshop-btn"> Read More</a>
-                        <br/>
-                        <br/>
+                        <div class="col-md-10">
+                            <h4>Workshop Title</h4>
+                            <span>Location | Expertise(Category)</span>
+                            <div class="rating">
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span> 
+                            </div>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <a href="workshop?type=<?php echo $type; ?>" class="workshop-btn"> Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="border-bottom" style="padding-bottom: 40px;padding-top: 40px;">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <img src="img/slider/slider-002.png" alt="" class="image-border-width">
+                        </div>
+                        <div class="col-md-10">
+                            <h4>Workshop Title</h4>
+                            <span>Location | Expertise(Category)</span>
+                            <div class="rating">
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span> 
+                            </div>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <a href="workshop?type=<?php echo $type; ?>" class="workshop-btn"> Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="border-bottom" style="padding-bottom: 40px;padding-top: 40px;">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <img src="img/slider/slider-002.png" alt="" class="image-border-width">
+                        </div>
+                        <div class="col-md-10">
+                            <h4>Workshop Title</h4>
+                            <span>Location | Expertise(Category)</span>
+                            <div class="rating">
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span> 
+                            </div>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <a href="workshop?type=<?php echo $type; ?>" class="workshop-btn"> Read More</a>
+                        </div>
                     </div>
                 </div>
             </div>
