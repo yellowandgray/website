906a4b60dba0f4f9cbafd51aab6b1e4915fc315c
Y&G <Y&G@YG00D6>
2019-12-10 19:12:35 +0530

    environmental policy page edit

---
+++ hospitol.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+    include 'head.php';
+    ?>
+    <body class="goto-here">
+
+
+        <!-- END nav -->
+        <?php include 'enquiry.php'; ?>
+        <section>
+            <?php include 'menu.php'; ?>
+        </section>
+        <div class="hero-wrap hero-bread" style="background-image: url('images/bg_03.jpg'); background-size: cover;">
+
+            <div class="container">
+                <div class="row no-gutters slider-text align-items-center justify-content-center">
+                    <div class="col-md-9 ftco-animate text-center">
+                        <h1 class="mb-0 bread">Fresche Website content for Hospital Industry</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <section class="ftco-section" >
+            <div class="pad-lr-80">
+                <div class="row box-4">
+                    <div class=" col-lg-8 product-details pl-md-5 ftco-animate">
+                        <p>“A recent Indian Intensive Care Case Mix and Practice Patterns (INDICAPS) study with a sample size of 4,209 patients admitted in 124 ICUs across 17 states  found that <span class="font-weight-bold">one out of every eight patients in India die  from infections contracted in ICUs.</span>”</p>
+                        <p class="font-weight-bold">“10-30% of the patients admitted  to hospitals and nursing homes In India acquires nosocomial infection as against  an impressive 5% in the west.”</p>
+                        <p class="font-weight-bold" style="text-align: right"><bold>-Hospital Infection Society –India</bold></p>
+                                                                        <!--<button class="button-03"><a href="#"><i class="fa fa-cart-plus" aria-hidden="true"></i>  Add To Cart</a></button>-->
+                    </div>
+                    <div class=" text-center col-lg-4 product-details pl-md-5 ftco-animate">
+
+                        <img src="images/applications/app/005.jpg" class="product-img-responsive" alt=""/>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="ftco-section">
+            <div class="pad-lr-80">
+                <div class="row">
+                    <div class="col-lg-4 mb-5  app-img ftco-animate">
+                        <div class="">
+                        <!--<a href="images/product-01.png" class="image-popup"><img src="images/product-01.png" class="img-fluid" alt="Colorlib Template"></a>-->
+                            <img src="images/applications/app/001.jpg" class="product-img-responsive" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-8 product-details app-con pl-md-5 ftco-animate">
+                        <h3>Delivering Powerful Outcomes</h3>
+                        <h5></h5>
+                        <!--<button class="button-03"><a href="#"><i class="fa fa-cart-plus" aria-hidden="true"></i>  Add To Cart</a></button>-->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="ftco-section" style="background-image: url('images/bg_02.png'); background-size: cover;">
+            <div class="pad-lr-80">
+                <div class="row">
+                    <div class="col-lg-8 product-details  app-con pl-md-5 ftco-animate">
+                        <h3>Footwear</h3>
+                        <h5>Did you know that sweat from your feet has no odour?</h5>
+                        <p>It’s the bacteria that grows on our feet or in the shoes we wear that actually cause odour.</p>
+                        <p>These bacteria convert the sweat and oil from skin into a complex mixture of chemical compounds that lead to unpleasant odours. The truth is that your shoes DO NOTHING to stop your sweat turning into odour!</p>
+                        <p>Sure, many brands might argue their shoes have better ventilation & odour absorbing chemicals. But, unless that sweat “magically disappears” from your shoes, the bacteria are going to accumulate and YOUR feet is going to produce unpleasant odours!</p>
+                        <!--<button class="button-03"><a href="#"><i class="fa fa-cart-plus" aria-hidden="true"></i>  Add To Cart</a></button>-->
+                    </div>
+                    <div class="col-lg-4 mb-5 app-img ftco-animate">
+                        <div class="">
+                       <!--<a href="images/product-01.png" class="image-popup"><img src="images/product-01.png" class="img-fluid" alt="Colorlib Template"></a>-->
+                            <img src="images/applications/app/002.jpg" class="product-img-responsive" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="ftco-section ">
+            <div class="pad-lr-80">
+                <div class="row">
+                    <div class="col-lg-4 mb-5 app-img  ftco-animate">
+                        <div class="">
+                        <!--<a href="images/product-01.png" class="image-popup"><img src="images/product-01.png" class="img-fluid" alt="Colorlib Template"></a>-->
+                            <img src="images/applications/app/003.jpg" class="product-img-responsive" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-8 product-details pl-md-5  app-con ftco-animate">
+                        <h3>Surface</h3>
+                        <h5>DOES YOUR COMMERCIAL DISINFECTION PRODUCT PROVIDE YOU WITH UP TO 7 DAYS OF BROAD SPECTRUM MICROBIAL CONTROL & PROTECTION?</h5>
+                        <p>Many disinfectant products are inefficient. They destroy resident germs on the surface being treated, but last only a short time before new microorganisms arrive to recolonise and recontaminate the surface.</p>
+                        <p>FRESCHE EF 3851 is a dual action, surface bonding, commercial grade disinfectant, which delivers long term, broad spectrum microbial control and protection on treated surfaces. It not only sanitises the surface, but continues to protect the surface from bacteria, fungus and molds, for upto 7 days.</p>
+                        <p>FRESCHE EF 3851 is non-corrosive, non staining, stable and safe for people, pets, plants and the environment. FRESCHE EF 3851 is redefining conventional performance and durability paradigms of safe microbial control and protection.</p>
+                        <!--<button class="button-03"><a href="#"><i class="fa fa-cart-plus" aria-hidden="true"></i>  Add To Cart</a></button>-->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="ftco-section pad-b-50" style="background-image: url('images/bg_02.png'); background-size: cover;">
+            <div class="pad-lr-80">
+                <div class="row">
+                    <div class="col-lg-12 product-details pl-md-5  app-con ftco-animate">
+                        <h3>Hospital</h3>
+                        <div class="box-3 text-for-hai">
+                            <h5 class="text-center">Hospital Acquired Infections [HAI]</h5>
+                            <p class="text-center">The World Health Organization ranks nosocomial or hospital acquired infections [HAI’s] as the third most significant threat to the human race.</p>
+                            <p class="text-center">“10-30% of the patients admitted to hospitals and nursing homes In India acquires nosocomial infection as against an impressive 5% in the west.” -Hospital Infection Society –India</p>
+                        </div> 
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 product-details pl-md-5  app-con ftco-animate">
+
+                        <h5>Does your commercial disinfection product provide you with up to 7 days of broadspectrum microbial control & protection?</h5>
+                        <p>Many disinfectant products are inefficient. They destroy germs on the surface being treated,but last only a short time before new microorganisms arrive to recolonise and recontaminate the surface.</p>
+                        <p>Fresche EF3851 is a dual action, surface bonding commercial grade disinfectant, which delivers long term, broadspectrum microbial controland protection on treated surfaces.It not only sanitises the surface, but continue to protect the surface from bacteria, fungus, and molds for up to 7 days.</p>
+
+                        <h5>YOUR FRESCHE APPLICATION</h5>
+                        <p>Operation thatre, ICU, Cath lab, Emergency room, Lobby area, Stretchers, ECG Monitor, Oprating table, Instrument Cabinet, Trolley, Surgeon’s tools, Hospital linen, Surgical drapes, OT floors and walls, ICU bed, Spotlight, Floors, Walls, Ceilings, Bedpan, Kidney dish, Bedrails, Air filters, Evaporative coolers, Rubbish and Recycling areas.</p>
+                        <!--<button class="button-03"><a href="#"><i class="fa fa-cart-plus" aria-hidden="true"></i>  Add To Cart</a></button>-->
+                    </div>
+                    <div class="col-lg-4 mb-5 app-img ftco-animate">
+                        <div class="">
+                       <!--<a href="images/product-01.png" class="image-popup"><img src="images/product-01.png" class="img-fluid" alt="Colorlib Template"></a>-->
+                            <img src="images/applications/app/004.jpg" class="product-img-responsive" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php include 'footer.php'; ?>
+    </body>
+</html>
