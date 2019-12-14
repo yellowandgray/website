3ecb37bb42321ad9cb52e5d4d3c2c82ab297f0b1
Mushaqdeen <mushaqdeen@yahoo.co.in>
2019-12-14 07:05:26 +0530

    Cart validation

--- js/common.js
+++ js/common.js
@@ -25,12 +25,12 @@ function MemberInsert() {
         data: {fname: $('#fname').val(), lname: $('#lname').val(), mobile: $('#mobile').val(), address: $('#address').val(), state: $('#state').val(), city: $('#city').val(), pincode: $('#pincode').val(), email: $('#email').val(), altmobile: $('#altmobile').val()},
         success: function (data) {
             if (data.result.error === false) {
-                $('#name').val(''); 
-                $('#mobile').val(''); 
-                $('#address').val(''); 
-                $('#state').val(''); 
-                $('#city').val(''); 
-                $('#pincode').val(''); 
+                $('#name').val('');
+                $('#mobile').val('');
+                $('#address').val('');
+                $('#state').val('');
+                $('#city').val('');
+                $('#pincode').val('');
                 $('#email').val('');
                 $('#altmobile').val('');
                 swal("Thanks for the registration", "", "success");
@@ -43,4 +43,60 @@ function MemberInsert() {
         }
     });
     return false;
+}
+
+
+function buyProduct() {
+    var rzpOptions = {
+        key: "rzp_test_zksOkaS0IXezA2",
+        amount: ((amt + (amt * 0.01)) * 100),
+        name: name,
+        description: "Account Recharge",
+        image: "https://phalamrutha.com/images/razorpay.png",
+        handler: function (response) {
+            var data = {action: 'credit', amount: amt, user_code: code, remark: 'RazorpayTransaction', identity: 'Transaction id: ' + response.razorpay_payment_id};
+            $.isLoading({text: 'Loading...'});
+            var api = new $.RestClient('v1/');
+            api.add('user', {url: 'transaction', stripTrailingSlash: true});
+            var result = api.user.create(data);
+            result.done(function (rs) {
+                if (rs.result.error === false) {
+                    if (type === 'checkout') {
+                        window.location = 'checkout.php';
+                    }
+                    if (type === 'payment') {
+                        window.location = 'mypayments.php';
+                    }
+                } else {
+                    swal('Error', rs.result.message, 'error');
+                    $.isLoading('hide');
+                }
+            });
+            result.fail(function (err) {
+                $.isLoading('hide');
+                swal('Error', 'Process failed!!', 'error');
+            });
+        },
+        prefill: {
+            name: name,
+            email: email,
+            contact: mobile
+        },
+        notes: {
+            address: "RazorpayTransaction"
+        },
+        theme: {
+            color: "#086f31"
+        }
+    };
+    var rzp1 = new Razorpay(rzpOptions);
+    rzp1.open();
+}
+
+function goToCheckout() {
+    if (shoppingCart.totalCount() > 0) {
+        window.location = 'checkout.php';
+    } else {
+        swal('Information', 'Your cart is empty', 'info')
+    }
 }
