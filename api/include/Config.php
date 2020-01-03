3b0b40fde93705d47365c69183f00270f0969be0
Yellowandgray <Yellowandgray@YG00D7>
2020-01-03 12:39:45 +0530

    pages updated

--- api/include/Config.php
+++ api/include/Config.php
@@ -2,7 +2,7 @@
 
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'megana');
+define('DB_NAME', 'mekana');
 define('DB_HOST', 'localhost');
-define('BASE_URL', 'http://localhost/microview/megana/api/v1/');
+define('BASE_URL', 'http://localhost/project/mekana/api/v1/');
 
