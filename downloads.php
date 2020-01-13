5b17dcf73f11cdbf3903c723ff6b827068066c6e
Y&G <Y&G@192.168.0.11>
2020-01-13 11:37:37 +0530

    contact form

--- downloads.php
+++ downloads.php
@@ -54,7 +54,7 @@
                             </a>
                         </div>
                         <div class="col-md-6 text-center">
-                            <a href="images/download/ENPEE-Brochure-2019-EDITED.pdf" target="blank">
+                            <a href="images/download/brochure.pdf" target="blank">
                                 <h5 title="ENPEE-Brochure-2019">ENPEE-Brochure-2019</h5>
                                 <img src="images/ENPEE-Brochure-2019.jpg" class="img-width download-box" alt="ENPEE-Brochure-2019" title="ENPEE-Brochure-2019">
                             </a>
