19740952f3bed16b083cd4aac0204971d3b1f1cc
Yellowandgray <Yellowandgray@YG00D7>
2020-01-25 12:09:52 +0530

    question page updated

--- admin/src/app/question/question.component.ts
+++ admin/src/app/question/question.component.ts
@@ -101,7 +101,7 @@ export class QuestionComponent implements OnInit {
 
         dialogRef.afterClosed().subscribe(result => {
             if (result !== false && result !== 'false') {
-                //this.getQuestion();
+                //this.getQuestionsByTopic();
             }
         });
     }
@@ -119,7 +119,7 @@ export class QuestionComponent implements OnInit {
         });
         dialogRef.afterClosed().subscribe(result => {
             if (result !== false && result !== 'false') {
-                //this.getQuestion();
+                //this.getQuestionsByTopic();
             }
         });
     }
