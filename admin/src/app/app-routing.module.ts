d645115ea4fe713deb26ef5ec82db3e5d655ed5c
Yellowandgray <Yellowandgray@YG00D7>
2020-01-29 17:34:02 +0530

    pages link changed

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ import { DifficultyLevelComponent } from './difficulty-level/difficulty-level.co
 
 
 const routes: Routes = [
-        {path: '', component: LoginComponent},
+        //{path: '', component: LoginComponent},
+        {path: '', component: SubjectComponent},
         {path: 'subject', component: SubjectComponent},
         {path: 'topic', component: TopicComponent},
         {path: 'question', component: QuestionComponent},
