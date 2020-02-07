09211e427d91eb742221cf1f8c75cdf83624ab1b
Yellowandgray <Yellowandgray@YG00D7>
2020-02-07 19:13:34 +0530

    home subject page and inquiry form updated

--- home_subject.php
+++ home_subject.php
@@ -2,7 +2,7 @@
 session_start();
 require_once 'api/include/common.php';
 $obj = new Common();
-$subjects = $obj->selectAll('*', 'subject', 'subject_id > 0');
+$subjects = $obj->selectAll('*', 'subject', 'subject_id > 0 AND status = 1');
 ?>
 <html lang="en">
     <?php include 'head.php'; ?>
@@ -15,21 +15,19 @@ $subjects = $obj->selectAll('*', 'subject', 'subject_id > 0');
                         <strong>10<sup>th</sup> STD State Board</strong>
                     </h4>
                     <div class="row margin-auto">
-                        <?php //if ($subjects) { ?>
-                            <?php foreach ($subjects as $row) { ?>
-                                <div class="span3 subject-section" onclick="window.location = 'difficult_level?sub=<?php echo $row['name']; ?>'">
-                                    <div class="price">Feringo</div>
-                                    <div class="subject-1">
-                                        <div class="subject-1-img" style="background: url(<?php echo BASE_URL . $row['image_path']; ?>)no-repeat;"></div>
-                                    </div>
-                                    <div class="subject-1-text">
-                                        <h2><?php echo $row['name']; ?></h2>
-                                        <p><?php echo $obj->charLimit($row['description'], 200); ?></p>
-                                        <button class="btn btn-theme margintop10" onclick="window.location = 'select_chapter?sub=<?php echo $row['name']; ?>'">Start Quiz...</button>
-                                    </div>
+                        <?php foreach ($subjects as $row) { ?>
+                            <div class="span3 subject-section" onclick="window.location = 'difficult_level?sub=<?php echo $row['name']; ?>'">
+                                <div class="price">Feringo</div>
+                                <div class="subject-1">
+                                    <div class="subject-1-img" style="background: url(<?php echo BASE_URL . $row['image_path']; ?>)no-repeat;"></div>
                                 </div>
-                            <?php } ?>
-                        <?php //} ?>
+                                <div class="subject-1-text">
+                                    <h2><?php echo $row['name']; ?></h2>
+                                    <p><?php echo $obj->charLimit($row['description'], 200); ?></p>
+                                    <button class="btn btn-theme margintop10" onclick="window.location = 'select_chapter?sub=<?php echo $row['name']; ?>'">Start Quiz...</button>
+                                </div>
+                            </div>
+                        <?php } ?>
                     </div>
                 </div>
             </section>
