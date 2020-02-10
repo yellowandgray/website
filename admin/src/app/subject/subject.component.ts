f77dc2ec6dc2dc69ae1333c46d93964cea8f01b4
User <mushaqdeen@yahoo.co.in>
2020-02-10 12:25:25 +0530

    Search issue fixed

--- admin/src/app/subject/subject.component.ts
+++ admin/src/app/subject/subject.component.ts
@@ -23,9 +23,9 @@ export class SubjectComponent implements OnInit {
   ngOnInit() {
     this.getsubject();
   }
-  image_url: string = '../api/v1/';
+  image_url: string = 'http://localhost/mushak/feringo/api/v1/';
   getsubject(): void {
-    this.httpClient.get<any>('../api/v1/get_subject')
+    this.httpClient.get<any>('http://localhost/mushak/feringo/api/v1/get_subject')
       .subscribe(
         (res) => {
           this.subject = res["result"]["data"];
@@ -81,7 +81,7 @@ export class SubjectComponent implements OnInit {
   templateUrl: 'subject-form.html',
 })
 export class SubjectForm {
-  image_url: string = '../api/v1/';
+  image_url: string = 'http://localhost/mushak/feringo/api/v1/';
   subjectForm: FormGroup;
   loading = false;
   subject_id = 0;
@@ -128,7 +128,7 @@ export class SubjectForm {
       formData.append('subject_image', this.image_path);
       url = 'insert_subject';
     }
-    this.httpClient.post('../api/v1/' + url, formData).subscribe(
+    this.httpClient.post('http://localhost/mushak/feringo/api/v1/' + url, formData).subscribe(
       (res) => {
         this.loading = false;
         if (res["result"]["error"] === false) {
@@ -155,7 +155,7 @@ export class SubjectForm {
     this.loading = true;
     var formData = new FormData();
     formData.append('file', fileData);
-    this.httpClient.post('../api/v1/upload_file', formData).subscribe(
+    this.httpClient.post('http://localhost/mushak/feringo/api/v1/upload_file', formData).subscribe(
       (res) => {
         this.loading = false;
         if (res["result"]["error"] === false) {
@@ -245,7 +245,7 @@ export class SubjectDelete {
       return;
     }
     this.loading = true;
-    this.httpClient.get('../api/v1/delete_record/subject/subject_id=' + this.subject_id).subscribe(
+    this.httpClient.get('http://localhost/mushak/feringo/api/v1/delete_record/subject/subject_id=' + this.subject_id).subscribe(
       (res) => {
         this.loading = false;
         if (res["result"]["error"] === false) {
