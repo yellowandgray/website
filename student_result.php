885eff1e2f30024bc0fdf2441a11dbafe43d59a7
mushaqdeen <mushaqdeen@192.168.0.104>
2020-02-12 22:34:57 +0530

    Full result view added

--- student_result.php
+++ student_result.php
@@ -63,27 +63,27 @@ if (isset($_SESSION['student_register_id'])) {
                                                             <div id = 'collapseOne' class = 'accordion-body collapse in'>
                                                                 <div class = 'accordion-inner'>
                         <?php foreach ($student_logs as $row) { ?>
-                                                                            <h2 class = 'titleContainer title'> <i class="font-icon-arrow-simple-right"></i> <?php echo $row['subject_name']; ?><i class="font-icon-arrow-simple-right"></i> <?php echo $row['chapter_name']; ?> <i class="font-icon-arrow-simple-right"></i><?php echo $row['topic_name']; ?><span>Date: <?php echo date('d/M/Y h:iA', strtotime($row['created_at'])); ?></span></h2>
-                                                                            <table class = 'table table-striped result_table'>
-                                                                                <thead>
-                                                                                    <tr>
-                                                                                        <th class="text-center">Category</th>
-                                                                                        <th class="text-center">Total</th>
-                                                                                            <th class="text-center">Attand Questions</th>
-                                                                                        <th class="text-center"><i class="icon-ok-sign"></i></th>
-                                                                                        <th class="text-center"><i class="font-icon-remove-circle"></i></th>
-                                                                                    </tr>
-                                                                                </thead>
-                                                                                <tbody>
-                                                                                    <tr>
-                                                                                        <td><?php echo $row['difficult_name'] ?> Marks</td>
-                                                                                        <td><?php echo $row['total_questions'] ?></td>
-                                                                                            <td><?php //echo $row['attended']           ?></td>
-                                                                                        <td><?php echo $row['correct_answers'] ?></td>
-                                                                                        <td><?php echo ($row['attended'] - $row['correct_answers']) ?></td>
-                                                                                    </tr>
-                                                                                </tbody>
-                                                                            </table>
+                                                                                        <h2 class = 'titleContainer title'> <i class="font-icon-arrow-simple-right"></i> <?php echo $row['subject_name']; ?><i class="font-icon-arrow-simple-right"></i> <?php echo $row['chapter_name']; ?> <i class="font-icon-arrow-simple-right"></i><?php echo $row['topic_name']; ?><span>Date: <?php echo date('d/M/Y h:iA', strtotime($row['created_at'])); ?></span></h2>
+                                                                                        <table class = 'table table-striped result_table'>
+                                                                                            <thead>
+                                                                                                <tr>
+                                                                                                    <th class="text-center">Category</th>
+                                                                                                    <th class="text-center">Total</th>
+                                                                                                        <th class="text-center">Attand Questions</th>
+                                                                                                    <th class="text-center"><i class="icon-ok-sign"></i></th>
+                                                                                                    <th class="text-center"><i class="font-icon-remove-circle"></i></th>
+                                                                                                </tr>
+                                                                                            </thead>
+                                                                                            <tbody>
+                                                                                                <tr>
+                                                                                                    <td><?php echo $row['difficult_name'] ?> Marks</td>
+                                                                                                    <td><?php echo $row['total_questions'] ?></td>
+                                                                                                        <td><?php //echo $row['attended']              ?></td>
+                                                                                                    <td><?php echo $row['correct_answers'] ?></td>
+                                                                                                    <td><?php echo ($row['attended'] - $row['correct_answers']) ?></td>
+                                                                                                </tr>
+                                                                                            </tbody>
+                                                                                        </table>
                         <?php } ?>
                                                                 </div>
                                                             </div>
@@ -118,39 +118,14 @@ if (isset($_SESSION['student_register_id'])) {
                                             <tr>
                                                 <td><?php echo $row['difficult_name'] ?> Marks</td>
                                                 <td><?php echo $row['total_questions'] ?></td>
-    <!--                                                            <td><?php //echo $row['attended']           ?></td>-->
+    <!--                                                            <td><?php //echo $row['attended']              ?></td>-->
                                                 <td><?php echo $row['correct_answers'] ?></td>
                                                 <td><?php echo ($row['attended'] - $row['correct_answers']) ?></td>
                                             </tr>
                                         </tbody>
                                     </table>
-                                    <center><button id="result-view-btn" class="btn btn-brown">Show Result</button></center>
-                                    <div id="result-view" class="full-result">
-                                        <div class="question-title">
-                                            <h6>1. Push or pull is called as ………………………………..</h6>
-                                            <div class="result-option crt_clr">
-                                                <div class="option">A. motion</div>
-                                            </div>
-                                            <div class="result-option">
-                                                <div class="option">B. force</div>
-                                            </div>
-                                            <div class="result-option wrng_clr">
-                                                <div class="option">C. momentum</div>
-                                            </div>
-                                        </div>
-                                        <div class="question-title">
-                                            <h6>2. …………………….is the branch of physics that deals with the effect of force on bodies.</h6>
-                                            <div class="result-option">
-                                                <div class="option">A. Gravitation</div>
-                                            </div>
-                                            <div class="result-option wrng_clr">
-                                                <div class="option">B. Acceleration</div>
-                                            </div>
-                                            <div class="result-option crt_clr">
-                                                <div class="option">C. Mechanics</div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <center><button id="result-view-btn" class="btn btn-brown" onclick="showFullResult(<?php echo $row['student_log_id']; ?>);">Show Result</button></center>
+                                    <div id="result_view_<?php echo $row['student_log_id']; ?>" class="full-result"></div>
                                     <hr/>
                                     <br/>
                                 <?php } ?>
@@ -170,6 +145,73 @@ if (isset($_SESSION['student_register_id'])) {
                     $("#result-view").fadeToggle();
                 });
             });
+            function showFullResult(slid) {
+                $.ajax({
+                    type: "GET",
+                    url: 'api/v1/get_result_detail/' + slid,
+                    success: function (data) {
+                        if (data.result.error === false) {
+                            var qlist = '';
+                            var correct_ans = '';
+                            var student_ans = '';
+                            $.each(data.result.data, function (key, val) {
+                                qlist = qlist + '<div class="question-title"><h6>' + (key + 1) + '. ' + val.name + '</h6>';
+                                if (val.a !== '') {
+                                    correct_ans = '';
+                                    student_ans = '';
+                                    if ((val.answer).toUpperCase() === 'A') {
+                                        correct_ans = 'crt_clr';
+                                    }
+                                    if ((val.student_answer).toUpperCase() === 'A' && (val.answer).toUpperCase() !== 'A') {
+                                        student_ans = 'wrng_clr';
+                                    }
+                                    qlist = qlist + '<div class="result-option ' + correct_ans + ' ' + student_ans + '"><div class="option">A. ' + val.a + '</div></div>';
+                                }
+                                if (val.b !== '') {
+                                    correct_ans = '';
+                                    student_ans = '';
+                                    if ((val.answer).toUpperCase() === 'B') {
+                                        correct_ans = 'crt_clr';
+                                    }
+                                    if ((val.student_answer).toUpperCase() === 'B' && (val.answer).toUpperCase() !== 'B') {
+                                        student_ans = 'wrng_clr';
+                                    }
+                                    qlist = qlist + '<div class="result-option ' + correct_ans + ' ' + student_ans + '"><div class="option">B. ' + val.b + '</div></div>';
+                                }
+                                if (val.c !== '') {
+                                    correct_ans = '';
+                                    student_ans = '';
+                                    if ((val.answer).toUpperCase() === 'C') {
+                                        correct_ans = 'crt_clr';
+                                    }
+                                    if ((val.student_answer).toUpperCase() === 'C' && (val.answer).toUpperCase() !== 'C') {
+                                        student_ans = 'wrng_clr';
+                                    }
+                                    qlist = qlist + '<div class="result-option ' + correct_ans + ' ' + student_ans + '"><div class="option">C. ' + val.c + '</div></div>';
+                                }
+                                if (val.d !== '') {
+                                    correct_ans = '';
+                                    student_ans = '';
+                                    if ((val.answer).toUpperCase() === 'D') {
+                                        correct_ans = 'crt_clr';
+                                    }
+                                    if ((val.student_answer).toUpperCase() === 'D' && (val.answer).toUpperCase() !== 'D') {
+                                        student_ans = 'wrng_clr';
+                                    }
+                                    qlist = qlist + '<div class="result-option ' + correct_ans + ' ' + student_ans + '"><div class="option">D. ' + val.d + '</div></div>';
+                                }
+                                qlist = qlist + '</div>';
+                            });
+                            $('#result_view_' + slid).html(qlist);
+                        } else {
+                            swal('Information', data.result.message, 'info');
+                        }
+                    },
+                    error: function (err) {
+                        swal('Error', err.statusText, 'error');
+                    }
+                });
+            }
         </script>
     </body>
 </html>
