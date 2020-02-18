ca3085c33521ed4a191fd4751f7455c2e7d3b3e9
User <mushaqdeen@yahoo.co.in>
2020-02-18 14:39:53 +0530

    API files added

---
+++ api/include/Config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_USERNAME', 'electromech');
+define('DB_PASSWORD', 'Emech@123');
+define('DB_NAME', 'electromech');
+define('DB_HOST', '166.62.8.52');
+define('BASE_URL', 'http://www.lemonandshadow.com/scaffolding/api/v1/');
