11b51e44790ff7ac4c13cfce91e9177c9aba9313
Yellowandgray <Yellowandgray@YG00D7>
2020-02-24 15:37:32 +0530

    pages updated

--- admission-content.php
+++ admission-content.php
@@ -4,23 +4,25 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="row">
-                    <h5>Admissions Open</h5>
-                    <div class="col-md-4">
+                    <div class="col-md-2">
                         <img src="images/enpeekkl-admission-open.jpg" alt="Enpee International School Admission Opens"
                              title="Enpee International School Admission Opens" />
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-8">
+                        <h5>Admissions Open</h5>
                         <p>Admissions for academic year 2020-21 open for Pre-KG to STD VIII. Contact Address: ENPEE
                             International School, Château Français, ENPEE Enclave, 241/2, Puthakudy Village, Vadamattam
                             Main Road, Nedungadu Panchayat, Karaikal – 609609. U.T of Puducherry.</p>
                         <a type="button" href="contact.php" class="btn-custom">Enquire Now</a>
                     </div>
                     <div class="col-md-2">
-                        <a href="images/first-annual-day-celebration.jpeg" target="blank" class="letter-position">
-                            <img src="images/first-annual-day-celebration.jpg"
-                                 alt="Enpee International School First Annual Day Celebration"
-                                 title="Enpee International School First Annual Day Celebration">
-                        </a>
+                        <div class="annual-day-section">
+                            <a href="images/first-annual-day-celebration.jpeg" target="blank" class="letter-position">
+                                <img src="images/first-annual-day-celebration.jpg"
+                                     alt="Enpee International School First Annual Day Celebration"
+                                     title="Enpee International School First Annual Day Celebration">
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
