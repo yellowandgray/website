450d47293816ab872fcd2162a7c95c6ab3a44bf1
Yellowandgray <Yellowandgray@YG00D7>
2020-02-27 10:36:49 +0530

    pages updated

--- admin/src/app/user/user.component.ts
+++ admin/src/app/user/user.component.ts
@@ -16,7 +16,7 @@ export class UserComponent implements OnInit {
   searchTerm: string = '';
   student = [];
   student_count = 0;
-  image_url: string = '../api/v1/';
+  image_url: string = 'http://localhost/project/feringo/api/v1/';
 
   constructor(public dialog: MatDialog, private httpClient: HttpClient, private _snackBar: MatSnackBar) { }
 
@@ -24,7 +24,7 @@ export class UserComponent implements OnInit {
     this.getuser();
   }
   getuser(): void {
-    this.httpClient.get<any>('../api/v1/get_student')
+    this.httpClient.get<any>('http://localhost/project/feringo/api/v1/get_student')
       .subscribe(
         (res) => {
           this.student = res["result"]["data"];
@@ -92,7 +92,7 @@ export class UserComponent implements OnInit {
     });
   }
   openResult(sid, name): void {
-      this.httpClient.get<any>('../api/v1/get_student_result/'+sid)
+      this.httpClient.get<any>('http://localhost/project/feringo/api/v1/get_student_result/'+sid)
       .subscribe(
         (res) => {
             if(res["result"]["error"] == false) {
@@ -168,7 +168,7 @@ export class UserComponent implements OnInit {
   templateUrl: 'user-form.html',
 })
 export class UserForm {
-  image_url: string = '../api/v1/';
+  image_url: string = 'http://localhost/project/feringo/api/v1/';
   userForm: FormGroup;
   loading = false;
   student_register_id = 0;
@@ -251,7 +251,7 @@ export class UserForm {
       formData.append('email', this.userForm.value.email);
       url = 'insert_student';
     }
-    this.httpClient.post('../api/v1/' + url, formData).subscribe(
+    this.httpClient.post('http://localhost/project/feringo/api/v1/' + url, formData).subscribe(
       (res) => {
         this.loading = false;
         if (res["result"]["error"] === false) {
@@ -277,7 +277,7 @@ export class UserForm {
     this.loading = true;
     var formData = new FormData();
     formData.append('file', fileData);
-    this.httpClient.post('../api/v1/upload_file', formData).subscribe(
+    this.httpClient.post('http://localhost/project/feringo/api/v1/upload_file', formData).subscribe(
       (res) => {
         this.loading = false;
         if (res["result"]["error"] === false) {
@@ -327,7 +327,7 @@ export class UserDelete {
       return;
     }
     this.loading = true;
-    this.httpClient.get('../api/v1/delete_record/student_register/student_register_id=' + this.student_register_id).subscribe(
+    this.httpClient.get('http://localhost/project/feringo/api/v1/delete_record/student_register/student_register_id=' + this.student_register_id).subscribe(
       (res) => {
         this.loading = false;
         if (res["result"]["error"] === false) {
@@ -410,7 +410,7 @@ export class UserFullResultForm {
 })
 
 export class PictureViewUser {
-  image_url: string = '../api/v1/';
+  image_url: string = 'http://localhost/project/feringo/api/v1/';
   action: string = '';
   loading = false;
   student_register_id = 0;
@@ -436,7 +436,7 @@ export class PictureViewUser {
 })
 
 export class UserViewForm {
-  image_url: string = '../api/v1/';
+  image_url: string = 'http://localhost/project/feringo/api/v1/';
   loading = false;
   student = [];
   student_register_id = 0;
