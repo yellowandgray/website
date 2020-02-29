ee61809b9d528d582e62fa2608394a478f8ecf2a
mushaqdeen <mushaqdeen@192.168.0.104>
2020-02-29 11:50:10 +0530

    API calls added

---
+++ api/include/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_USERNAME', 'threelevel');
+define('DB_PASSWORD', 'Panipuri@123');
+define('DB_NAME', 'threelevel');
+define('DB_HOST', '166.62.8.3');
+define('BASE_URL', 'http://www.lemonandshadow.com/threelevel/api/v1/');
+define('FILES_PATH', 'http://www.lemonandshadow.com/threelevel/');
