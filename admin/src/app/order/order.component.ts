93bb2f0210820212756f292312a38b54bd1f787d
Yellowandgray <Yellowandgray@YG00D7>
2020-02-29 15:09:36 +0530

    delivery boy page and order view form updated

--- admin/src/app/order/order.component.ts
+++ admin/src/app/order/order.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -30,5 +30,36 @@ export class OrderComponent implements OnInit {
         }
       );
   }
+  
+  OrderView(): void {
+    
+    const dialogRef = this.dialog.open(OrderView, {
+      minWidth: "40%",
+      maxWidth: "40%",
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== false && result !== 'false') {
+        
+      }
+    });
+  }
+}
+
+@Component({
+  selector: 'order-view',
+  templateUrl: 'order-view.html',
+})
+
+export class OrderView {
+  image_url: string = 'http://www.lemonandshadow.com/threelevel/api/v1/';
+  loading = false;
+  data: any;
+  constructor(
+    public dialogRef: MatDialogRef<OrderView>,
+    @Inject(MAT_DIALOG_DATA) public datapopup: any,
+    private _snackBar: MatSnackBar,
+    private httpClient: HttpClient) { 
+        this.data = this.datapopup;
+    }
 }
