dc7bd4e9f845c1d90729d56ab52ab16a1aea6a23
Yellowandgray <Yellowandgray@YG00D7>
2020-03-09 13:10:19 +0530

    tc format updated

--- include/Config.php
+++ include/Config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('DB_USERNAME', 'mypetcab_sankara');
-define('DB_PASSWORD', 'mypetcab_sankara');
-define('DB_NAME', 'mypetcab_sankara-school');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'sankara-school');
 define('DB_HOST', 'localhost');
-define('IMG_BASE_URL', 'http://bluehost.com/sankara-school/v1/');
+define('IMG_BASE_URL', 'http://localhost/project/sankara-school/v1/');
