7c1b964ad4431311fc2f019b0b17cb67c1dc2df8
Yellowandgray <Yellowandgray@YG00D7>
2020-03-12 19:55:04 +0530

    quiz page updated

--- quiz_page.php
+++ quiz_page.php
@@ -54,35 +54,34 @@ if (count($questions) > 0) {
     }
 }
 
-$subj_topic = $obj->selectAll('t.*,s.name As subject',' topic AS t LEFT JOIN subject AS s ON s.subject_id = t.subject_id',' t.topic_id IN (' . $_SESSION['student_selected_topics_id'] . ')  ORDER BY subject_id, topic_id ASC');
+$subj_topic = $obj->selectAll('t.*,s.name As subject', ' topic AS t LEFT JOIN subject AS s ON s.subject_id = t.subject_id', ' t.topic_id IN (' . $_SESSION['student_selected_topics_id'] . ')  ORDER BY subject_id, topic_id ASC');
 $sub_topic_val = '';
-if(count($subj_topic)>0) {
-   $sub_topic_arr = array();
-    foreach($subj_topic as $val) {
-            $sub_topic_arr[$val['subject']][] = $val['name'];
+if (count($subj_topic) > 0) {
+    $sub_topic_arr = array();
+    foreach ($subj_topic as $val) {
+        $sub_topic_arr[$val['subject']][] = $val['name'];
     }
-    
-    foreach($sub_topic_arr as $stak=>$stav) {
-        if($sub_topic_val != '') {
+
+    foreach ($sub_topic_arr as $stak => $stav) {
+        if ($sub_topic_val != '') {
             $sub_topic_val .= ', ';
         }
         $sub_topic_val .= $stak;
-        $sub_topic_val .= ' ('.implode(', ',$stav).') ';
+        $sub_topic_val .= ' (' . implode(', ', $stav) . ') ';
     }
- }
- 
- $sel_year_val = '';
- if(isset($_SESSION['student_selected_years_id']) && ($_SESSION['student_selected_years_id']!='')) {
-   $yearres = $obj->selectAll('year','year',' year_id IN ('.$_SESSION['student_selected_years_id'].')');
-   if(count($yearres)>0) {
-        $selyearr = array();   
-        foreach($yearres as $yval) {
-            $selyearr[] = $yval['year']; 
-        }
-        $sel_year_val = implode(', ',$selyearr);
-   }    
- }        
+}
 
+$sel_year_val = '';
+if (isset($_SESSION['student_selected_years_id']) && ($_SESSION['student_selected_years_id'] != '')) {
+    $yearres = $obj->selectAll('year', 'year', ' year_id IN (' . $_SESSION['student_selected_years_id'] . ')');
+    if (count($yearres) > 0) {
+        $selyearr = array();
+        foreach ($yearres as $yval) {
+            $selyearr[] = $yval['year'];
+        }
+        $sel_year_val = implode(', ', $selyearr);
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -147,7 +146,7 @@ if(count($subj_topic)>0) {
                                 <div v-if="quiz.questions[questionIndex].show_image" class="text-center">
                                     <img style="width: 50%" v-if="quiz.questions[questionIndex].direction == 'top'" v-bind:src="'api/v1/'+quiz.questions[questionIndex].image_path" alt="image" class="qes-img" />
                                 </div>
-                                <h2 class="titleContainer title">{{questionIndex + 1}} <span v-html="quiz.questions[questionIndex].text"></span></h2>
+                                <h2 class="titleContainer title">{{questionIndex + 1}}. <span v-html="quiz.questions[questionIndex].text"></span></h2>
                                 <div v-if="quiz.questions[questionIndex].show_image" class="text-center">
                                     <img style="width: 50%" v-if="quiz.questions[questionIndex].direction == 'bottom'" v-bind:src="'api/v1/'+quiz.questions[questionIndex].image_path" alt="image" class="qes-img" />
                                 </div>
@@ -155,119 +154,119 @@ if(count($subj_topic)>0) {
                                 <div class="optionContainer">
                                     <div id="two" class="option" v-for="(response, index) in quiz.questions[questionIndex].responses" @click="selectOption(index)" :class="{ 'is-selected': userResponses[questionIndex] == index}" :key="index">
                                          <span class="q-option">{{ index | charIndex }}.</span> {{ response.text }} {{response.show_image}}
+                                    </div>
+                                </div>
+                                <!--                            <footer class="questionFooter">
+                                                                <nav class="pagination" role="navigation" aria-label="pagination">
+                                                                                                        <a class="button" v-on:click="prev();" :disabled="questionIndex < 1">
+                                                                                                           Back
+                                                                                                    </a>
+                                                                    <a class="btn btn-green" href="select_language">
+                                                                        Home
+                                                                    </a>
+                                                                    <a class="button" :class="(userResponses[questionIndex]==null)?'':'is-active'" v-on:click="next();" :disabled="questionIndex>=quiz.questions.length">
+                                                                        {{ (userResponses[questionIndex]==null)?'Skip':'Next' }}
+                                                                    </a>
+                                                                </nav>
+                                                            </footer>-->
+                            </div>
+                            <!--quizCompletedResult-->
+                            <div v-if="questionIndex >= quiz.questions.length" v-bind:key="questionIndex" class="quizCompleted has-text-centered">
+
+                                <!-- quizCompletedIcon: Achievement Icon -->
+                                <span class="icon">
+                                    <i class="fa" :class="score()>3?'fa-check-circle-o is-active':'fa-times-circle'"></i>
+                                </span>
+
+                                <!--resultTitleBlock-->
+                                <h2 class="complete-title">
+                                    You did {{ (score()>7?'an amazing':(score()<4?'improve':'a good')) }} knowledge!
+                                </h2>
+                                <p class="subtitle">
+                                    Total score: {{ score() }} / {{ quiz.questions.length }}
+                                </p>
+                                <div class="">
+                                    <a class="btn btn-theme btn-rounded" @click="restart()">Restart <i class="fa fa-refresh"></i></a>
+                                    <a class="btn btn-theme btn-rounded" onclick="window.location = 'select_language'">Home <i class="fa fa-refresh"></i></a>
+                                    <a class="btn btn-theme btn-rounded" onclick="window.location = 'student_result'">Full Result <i class="fa fa-refresh"></i></a>
+                                    <!--/resultTitleBlock-->
+
                                 </div>
                             </div>
-                            <!--                            <footer class="questionFooter">
-                                                            <nav class="pagination" role="navigation" aria-label="pagination">
-                                                                                                    <a class="button" v-on:click="prev();" :disabled="questionIndex < 1">
-                                                                                                       Back
-                                                                                                </a>
-                                                                <a class="btn btn-green" href="select_language">
-                                                                    Home
-                                                                </a>
-                                                                <a class="button" :class="(userResponses[questionIndex]==null)?'':'is-active'" v-on:click="next();" :disabled="questionIndex>=quiz.questions.length">
-                                                                    {{ (userResponses[questionIndex]==null)?'Skip':'Next' }}
-                                                                </a>
-                                                            </nav>
-                                                        </footer>-->
+                            <!--/quizCompetedResult-->
+                            <!-- 		</transition> -->
                         </div>
-                        <!--quizCompletedResult-->
-                        <div v-if="questionIndex >= quiz.questions.length" v-bind:key="questionIndex" class="quizCompleted has-text-centered">
-
-                            <!-- quizCompletedIcon: Achievement Icon -->
-                            <span class="icon">
-                                <i class="fa" :class="score()>3?'fa-check-circle-o is-active':'fa-times-circle'"></i>
-                            </span>
-
-                            <!--resultTitleBlock-->
-                            <h2 class="complete-title">
-                                You did {{ (score()>7?'an amazing':(score()<4?'improve':'a good')) }} knowledge!
-                            </h2>
-                            <p class="subtitle">
-                                Total score: {{ score() }} / {{ quiz.questions.length }}
-                            </p>
-                            <div class="">
-                                <a class="btn btn-theme btn-rounded" @click="restart()">Restart <i class="fa fa-refresh"></i></a>
-                                <a class="btn btn-theme btn-rounded" onclick="window.location = 'select_language'">Home <i class="fa fa-refresh"></i></a>
-                                <a class="btn btn-theme btn-rounded" onclick="window.location = 'student_result'">Full Result <i class="fa fa-refresh"></i></a>
-                                <!--/resultTitleBlock-->
-
-                            </div>
-                        </div>
-                        <!--/quizCompetedResult-->
-                        <!-- 		</transition> -->
+                        <!-- question Box -->
                     </div>
-                    <!-- question Box -->
                 </div>
-            </div>
-        </section>
-    </div>
-    <!--/container-->
-    <?php include 'footer.php'; ?>
-    <?php include 'script.php'; ?>
-    <script>
-        console.log(<?php echo json_encode($questions_list); ?>);
-        var quiz = {
-            user: "<?php echo $student['student_name']; ?>",
-            questions: <?php echo json_encode($questions_list); ?>
-        },
-                userResponseSkelaton = Array(quiz.questions.length).fill(null);
-        var app = new Vue({
-            el: "#app",
-            data: {
-                quiz: quiz,
-                questionIndex: 0,
-                userResponses: userResponseSkelaton,
-                isActive: false
+            </section>
+        </div>
+        <!--/container-->
+        <?php include 'footer.php'; ?>
+        <?php include 'script.php'; ?>
+        <script>
+            console.log(<?php echo json_encode($questions_list); ?>);
+            var quiz = {
+                user: "<?php echo $student['student_name']; ?>",
+                questions: <?php echo json_encode($questions_list); ?>
             },
-            filters: {
-                charIndex: function (i) {
-                    return String.fromCharCode(97 + i);
-                }
-            },
-            methods: {
-                restart: function () {
-                    this.questionIndex = 0;
-                    this.userResponses = Array(this.quiz.questions.length).fill(null);
+                    userResponseSkelaton = Array(quiz.questions.length).fill(null);
+            var app = new Vue({
+                el: "#app",
+                data: {
+                    quiz: quiz,
+                    questionIndex: 0,
+                    userResponses: userResponseSkelaton,
+                    isActive: false
                 },
-                selectOption: function (index) {
-                    setTimeout(() => {
-                        Vue.set(this.userResponses, this.questionIndex, index);
-                        if (this.questionIndex < this.quiz.questions.length) {
-                            this.questionIndex++;
-                        }
-                    }, 500);
-                    setTimeout(() => {
-                        test();
-                    }, 600);
-                },
-                next: function () {
-                    if (this.questionIndex < this.quiz.questions.length)
-                        this.questionIndex++;
-                },
-                prev: function () {
-                    if (this.quiz.questions.length > 0)
-                        this.questionIndex--;
-                },
-                // Return "true" count in userResponses
-                score: function () {
-                    var score = 0;
-                    for (let i = 0; i < this.userResponses.length; i++) {
-                        if (
-                                typeof this.quiz.questions[i].responses[
-                                this.userResponses[i]
-                        ] !== "undefined" &&
-                                this.quiz.questions[i].responses[this.userResponses[i]].correct
-                                ) {
-                            score = score + 1;
-                        }
+                filters: {
+                    charIndex: function (i) {
+                        return String.fromCharCode(97 + i);
                     }
-                    return score;
+                },
+                methods: {
+                    restart: function () {
+                        this.questionIndex = 0;
+                        this.userResponses = Array(this.quiz.questions.length).fill(null);
+                    },
+                    selectOption: function (index) {
+                        setTimeout(() => {
+                            Vue.set(this.userResponses, this.questionIndex, index);
+                            if (this.questionIndex < this.quiz.questions.length) {
+                                this.questionIndex++;
+                            }
+                        }, 500);
+                        setTimeout(() => {
+                            test();
+                        }, 600);
+                    },
+                    next: function () {
+                        if (this.questionIndex < this.quiz.questions.length)
+                            this.questionIndex++;
+                    },
+                    prev: function () {
+                        if (this.quiz.questions.length > 0)
+                            this.questionIndex--;
+                    },
+                    // Return "true" count in userResponses
+                    score: function () {
+                        var score = 0;
+                        for (let i = 0; i < this.userResponses.length; i++) {
+                            if (
+                                    typeof this.quiz.questions[i].responses[
+                                    this.userResponses[i]
+                            ] !== "undefined" &&
+                                    this.quiz.questions[i].responses[this.userResponses[i]].correct
+                                    ) {
+                                score = score + 1;
+                            }
+                        }
+                        return score;
 
-                    //return this.userResponses.filter(function(val) { return val }).length;
+                        //return this.userResponses.filter(function(val) { return val }).length;
+                    }
                 }
-            }
-        });
-    </script>
-</body>
+            });
+        </script>
+    </body>
 </html>
