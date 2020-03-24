c60a7855042d78cc625aade1e8f961d94bb5670c
Umasekar <Umasekar@DESKTOP-E20NLT8>
2020-03-24 18:40:23 +0530

    pages updated

--- product.php
+++ product.php
@@ -70,13 +70,18 @@
                                             <p>Fresche EF 3851– 1 Litre</p>
                                             <p>Bz 1 Binder – 1.5 Litre </p>
                                             <p><span><i class="fa fa-inr" aria-hidden="true"></i></span><span>8000/-</span></p>
-                                            <a href="#" data-name="combo" data-price="8000" class="add-to-cart">
-                                                                                         <button class="button-03"><i class="fa fa-cart-plus" aria-hidden="true"></i>  Add To Cart<!-- ( <i class="fa fa-inr" aria-hidden="true"></i> 8000 )--></button>
+                                            <a href="tel:+91 8409 012345" data-name="combo" data-price="8000" class="add-to-cart">
+<!--                                                <button class="button-03">
+                                                        <i class="fa fa-cart-plus" aria-hidden="true"></i>  Add To Cart ( <i class="fa fa-inr" aria-hidden="true"></i> 8000 )
+                                                </button>-->
+                                                <button class="button-03">
+                                                        <i class="fa fa-cart-plus" aria-hidden="true"></i>  Call For Order
+                                                </button>
                                             </a>
-                                            <a href="javascript: goToCheckout();" class="go-cart" data-target="#cart">
+<!--                                            <a href="javascript: goToCheckout();" class="go-cart" data-target="#cart">
                                                 <span class="icon-shopping_cart"></span>
                                                 <span class="total-count"></span>
-                                            </a>
+                                            </a>-->
                                         </div>
                                     </div>
                                 </div>
