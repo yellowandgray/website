60266afc960899d87d3aa54401a6982a6b954d48
Y&G <Y&G@YG00D6>
2020-05-25 20:43:47 +0530

    pause quiz

--- select_language.php
+++ select_language.php
@@ -85,7 +85,7 @@ if(count($student_log_pause)>0) {
                                 
                                 
                                 $student_log_year  = $obj->selectRow('sly.*,y.year As year','student_log_year As sly LEFT JOIN year As y ON y.year_id=sly.year_id',' sly.student_log_id='.$student_pause_log_id);
-                                $student_log = $obj->selectRow('sl.*,l.name As language_name','student_log sl LEFT JOIN language l ON sl.language_id=l.language_id','student_log_id='.$student_pause_log_id);
+                                $student_log = $obj->selectRow('sl.*','student_log sl LEFT JOIN language l ON sl.language_id=l.language_id','student_log_id='.$student_pause_log_id);
                                 
                                 $student_log_detail             = $obj->selectRow('COUNT(student_log_detail_id) AS attended, IFNULL((SELECT COUNT(student_log_detail_id) FROM student_log_detail INNER JOIN question ON student_log_detail.question_id=question.question_id WHERE student_log_id=' . $student_pause_log_id . ' AND UPPER(answer) = UPPER(student_answer)), 0) AS correct_answers', 'student_log_detail', 'student_log_id=' . $student_pause_log_id);
                                 $student_log_attended           = $student_log_detail['attended'];
@@ -94,9 +94,13 @@ if(count($student_log_pause)>0) {
                             
                             <div class="span2">
                                     <div class="language-box" onClick="selpausequiz();">
+                                        <?php /*
                                         <div class="language-img-zoom">
                                             <div class="language-img" style="background: url(<?php echo BASE_URL . $val['imageurl']; ?>)no-repeat;"></div>
                                         </div>
+                                         * 
+                                         */
+                                        ?>
                                         <div class="language-title">
                                             <h3>
                                                 <a href="#" onClick="selpausequiz();">Your Last Session</a>
@@ -124,7 +128,7 @@ if(count($student_log_pause)>0) {
                                                 <tbody>
 
                                                     <tr>
-                                                       <td class="text-center"><?php echo date('d/m/Y',strtotime($student_log_pause['pause_date'])); ?></td>
+                                                        <td class="text-center"><?php echo date('d/m/Y',strtotime($student_log_pause['pause_date'])); ?></td>
                                                         <td class="text-center"><?php echo $student_log_year['year']; ?></td>
                                                         <td class="text-center"><?php echo $student_log['total_questions'] ?></td>
                                                         <td class="text-center"><?php echo $student_log_attended; ?></td>
@@ -136,7 +140,7 @@ if(count($student_log_pause)>0) {
                                             
                                             
                                             
-                                            <a class="btn btn-theme margintop10 difficult-button" onClick="selpausequiz();">START QUIZ</a>
+                                            <a class="btn btn-theme margintop10 difficult-button" onClick="selpausequiz();">RESUME QUIZ</a>
                                         </div>
                                     </div>
                                 </div>
