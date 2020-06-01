a54b7028a15a6b0cb611ce5b6da306f1d01340c9
Umasekar <Umasekar@DESKTOP-E20NLT8>
2020-06-01 12:03:58 +0530

    pages updated

--- api/include/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'mekana');
-define('DB_HOST', 'localhost');
-define('BASE_URL', 'http://localhost/project/mekana/api/v1/');
-
