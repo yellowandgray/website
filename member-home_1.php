7a874c1e0aec9b9732deb1af4780067be8b8207b
Umasekar <Umasekar@DESKTOP-E20NLT8>
2020-06-02 16:51:54 +0530

    pages updated

--- member-home_1.php
+++ member-home_1.php
@@ -27,7 +27,7 @@ if (isset($_SESSION['student_register_id'])) {
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
                             </div>
                             <div class="start-quiz-btn">
-                                <a href="select_language" class="btn btn-green">START QUIZ</a>
+                                <a href="#" class="btn btn-green">START QUIZ</a>
                             </div>
                             <table class="full-member-table">
                                 <tbody>
