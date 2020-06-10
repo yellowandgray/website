47dca2dd74cc4471b52839908c8751e189ea5575
Umasekar <Umasekar@DESKTOP-E20NLT8>
2020-06-10 19:49:05 +0530

    pages updated

--- reset-password.php
+++ reset-password.php
@@ -10,31 +10,38 @@ $languages = $obj->selectAll('*', 'language', 'status = 1');
         <?php include 'menu.php'; ?>
         <div class="member-full-version-secttion">
             <div class="container">
-                <form class = 'form-horizontal' onsubmit="return changePassword();">
-                    <input type = 'text' id = 'user_name' placeholder = 'Enter Register User Name'>
-                    <input type = 'password' id = 'password' placeholder = ''>
-                    <input type = 'password' id = 'confirm_password' placeholder = ''>
-                    <button type = 'submit' class = 'btn'>Reset password</button>
-        <!--                <p class = 'aligncenter margintop20'>
-                    Please put your registered user name. you have reset the your password...
-                </p>-->
+                <form onsubmit="return changePassword();" class="reset-form">
+                    <div class="form-group">
+                        <input type = 'text' id = 'user_name' placeholder = 'Enter Register User Name' required>
+                    </div>
+                    <div class="form-group">
+                        <input type = 'password' id = 'password' placeholder = 'New Password' required>
+                    </div>
+                    <div class="form-group">
+                        <input type = 'password' id = 'confirm_password' placeholder = 'Confirm Password' required>
+                    </div>
+                    <div class="form-group">
+                        <button type = 'submit' class = 'btn'>Reset password</button>
+            <!--                <p class = 'aligncenter margintop20'>
+                        Please put your registered user name. you have reset the your password...
+                    </p>-->
+                    </div>
+                </form>
             </div>
-        </form>
-    </div>
-</div>
-<?php include 'footer.php'; ?>
-<?php include 'script.php'; ?>
-<script>
-    $("#open-logout").click(function (e) {
-        console.log("test-1");
-        e.stopPropagation();
-        $(".logout_position").show("fast");
-    });
-    $(document).click(function (e) {
-        if (!(e.target.class === 'logout_position')) {
-            $(".logout_position").hide("fast");
-        }
-    });
-</script>
-</body>
+        </div>
+        <?php include 'footer.php'; ?>
+        <?php include 'script.php'; ?>
+        <script>
+            $("#open-logout").click(function (e) {
+                console.log("test-1");
+                e.stopPropagation();
+                $(".logout_position").show("fast");
+            });
+            $(document).click(function (e) {
+                if (!(e.target.class === 'logout_position')) {
+                    $(".logout_position").hide("fast");
+                }
+            });
+        </script>
+    </body>
 </html>
