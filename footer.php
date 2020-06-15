90c227aaa0236ed1688f6ad8cb75c48ebc217c86
Umasekar <Umasekar@DESKTOP-E20NLT8>
2020-06-15 16:58:41 +0530

    pages updated

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
                             <span class="text-left">&copy; Copyright 2020 - Exam Horse - All Rights Reserved.</span>
                         </p>
                         <p>
-                            <span class="text-right" style="font-size: 13px;color: #484848;"><a href="terms" target="blank">Terms & Conditions</a> / <a href="privacy_policy" target="blank">Privacy Policy</a></span>
+                            <span class="text-right" style="font-size: 13px;color: #484848;"><a href="terms-and-conditions" target="blank">Terms & Conditions</a> / <a href="privacy-policy" target="blank">Privacy Policy</a></span>
                         </p>
                     </div>
                 </div>
