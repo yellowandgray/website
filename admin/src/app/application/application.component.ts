c5c6e79ed5ceebbae5474a7bdcea98aa7c5aa4ef
mushaqdeen <mushaqdeen@yahoo.co.in>
2020-10-27 15:33:52 +0530

    Adding multiple images

--- admin/src/app/application/application.component.ts
+++ admin/src/app/application/application.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import * as moment from 'moment';
 
@@ -221,44 +222,37 @@ export class ApplicationForm {
 })
 export class ApplicationGalleryForm {
     image_url: string = 'http://localhost/microview/fresche/api/v1/';
-    applicationForm: FormGroup;
+    applicationGalleryForm: FormGroup;
     loading = false;
     application_id = 0;
-    application_thumb_image: string = 'Select Application Thumb Image';
-    image_path_thumb: string = '';
     constructor(
         public dialogRef: MatDialogRef<ApplicationGalleryForm>,
+        private sanitizer: DomSanitizer,
         @Inject(MAT_DIALOG_DATA) public data: any,
         private _snackBar: MatSnackBar,
         private httpClient: HttpClient) {
-        this.applicationForm = new FormGroup({
-            'title': new FormControl('', Validators.required),
+        this.applicationGalleryForm = new FormGroup({
+            'youtube_id': new FormControl('', Validators.required),
         });
 
         if (this.data != null) {
-            this.applicationForm.patchValue({
-                title: this.data.title,
+            this.applicationGalleryForm.patchValue({
+                youtube_id: this.data.youtube_id,
             });
             this.application_id = this.data.application_id;
-            this.image_path_thumb = this.data.image_path_thumb;
         }
     }
 
     onSubmit() {
-        if (this.applicationForm.invalid) {
+        if (this.applicationGalleryForm.invalid) {
             return;
         }
         this.loading = true;
         var formData = new FormData();
         var url = '';
         if (this.application_id != 0) {
-            formData.append('title', this.applicationForm.value.title);
-            formData.append('image_path_thumb', this.image_path_thumb);
+            formData.append('youtube_id', this.applicationGalleryForm.value.youtube_id);
             url = 'update_record/application/application_id = ' + this.application_id;
-        } else {
-            formData.append('title', this.applicationForm.value.title);
-            formData.append('application_thumb_image', this.image_path_thumb);
-            url = 'insert_application';
         }
         this.httpClient.post('http://localhost/microview/fresche/api/v1/' + url, formData).subscribe(
             (res) => {
@@ -280,17 +274,24 @@ export class ApplicationGalleryForm {
         );
     }
 
-    fileProgress(fileInput: any, name: string, path: string) {
-        var fileData = <File>fileInput.target.files[0];
-        this[name] = fileData.name;
+    getTrustURL(id) {
+        return this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" + id);
+    }
+
+    fileProgress(fileInput: any) {
+        console.log(fileInput.target.files);
         this.loading = true;
         var formData = new FormData();
-        formData.append('file', fileData);
-        this.httpClient.post('http://localhost/microview/fresche/api/v1/upload_file', formData).subscribe(
+        var filedata = [];
+        for (var i = 0; i < fileInput.target.files.length; i++) {
+            formData.append('file[]', <File>fileInput.target.files[i]);
+        }
+        formData.append('application_id', (this.application_id).toString());
+        this.httpClient.post('http://localhost/microview/fresche/api/v1/upload_files', formData).subscribe(
             (res) => {
                 this.loading = false;
                 if (res["result"]["error"] === false) {
-                    this[path] = res["result"]["data"];
+
                 } else {
                     this._snackBar.open(res["result"]["message"], '', {
                         duration: 2000,
@@ -305,11 +306,27 @@ export class ApplicationGalleryForm {
             });
     }
 
-    removeMedia(url) {
-        this[url] = '';
-        if (url === 'image_path_thumb') {
-            this.application_thumb_image = 'Select Application Thumb Image';
-        }
+    removeMedia(id) {
+        this.loading = true;
+        var formData = new FormData();
+        formData.append('gallery_id', id);
+        this.httpClient.post('http://localhost/microview/fresche/api/v1/delete_file', formData).subscribe(
+            (res) => {
+                this.loading = false;
+                if (res["result"]["error"] === false) {
+
+                } else {
+                    this._snackBar.open(res["result"]["message"], '', {
+                        duration: 2000,
+                    });
+                }
+            },
+            (error) => {
+                this.loading = false;
+                this._snackBar.open(error["statusText"], '', {
+                    duration: 2000,
+                });
+            });
     }
 
 }
